7e0504c1afb9302b02c96d2d4ba36183db659d75
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-02-18 10:00:00 -0800

    :sparkles: change app title to EVMts playground
    Fix e2e test
    
    Update pure.spec.ts
    
    :sparkles: change title

--- apps/e2e/src/pure.spec.ts
+++ apps/e2e/src/pure.spec.ts
@@ -3,9 +3,6 @@ import { test, expect } from "@playwright/test";
 test("should run a pure test", async ({ page }) => {
 	await page.goto("http://localhost:5173");
 
-	// Expect a title "to contain" a substring.
-	await expect(page).toHaveTitle(/wagmi/);
-
 	await page.getByRole("spinbutton").first().click();
 	await page.getByRole("spinbutton").first().fill("05");
 	await page.getByRole("spinbutton").nth(1).click();
