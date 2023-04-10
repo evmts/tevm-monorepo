6c8309678d6a21146260f23fd5ac5e75746fd622
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-04-09 19:39:09 -0700

    :memo: feat(docs): Update sidebar

--- apps/docs/.vitepress/config.ts
+++ apps/docs/.vitepress/config.ts
@@ -13,17 +13,34 @@ export default defineConfig({
 
     sidebar: [
       {
-        text: "Examples",
+        text: "Introduction",
         items: [
           { text: "Home", link: "/" },
           { text: "Get started", link: "/get-started" },
           { text: "Api reference", link: "/api" },
         ],
       },
+      {
+        text: "Guides",
+        items: [
+          { text: "Cheat codes", link: "/guide/cheat-codes" },
+          { text: "Scripting", link: "/guides/scripting" },
+        ],
+      },
+      {
+        text: "reference",
+        items: [
+          { text: "Usage with viem contracts", link: "/reference/contract" },
+          { text: "execute", link: "/reference/contract" },
+          { text: "http-fork", link: "/reference/http-fork" },
+          { text: "public-client", link: "/reference/public-client" },
+          { text: "wallet-client", link: "/reference/wallet-client" },
+        ],
+      },
     ],
-
     socialLinks: [
       { icon: "github", link: "https://github.com/evmts/evmts-monorepo" },
+      { icon: "twitter", link: "https://twitter.com/FUCORY" },
     ],
   },
 });
