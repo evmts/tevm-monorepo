c1016005ffda6ee4a084a84259e70e6a0d41b886
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-04-20 07:30:07 -0700

    :green_heart: fix: Fix typechecker for ts plugin

--- packages/plugin-ts/src/index.ts
+++ packages/plugin-ts/src/index.ts
@@ -1,7 +1,9 @@
+import type typescript from 'typescript/lib/tsserverlibrary'
+
 function init(modules: { typescript: typeof import("typescript/lib/tsserverlibrary") }) {
     const ts = modules.typescript;
-  
-    function create(info: ts.server.PluginCreateInfo) {
+
+    function create(info: typescript.server.PluginCreateInfo) {
       // Get a list of things to remove from the completion list from the config object.
       // If nothing was specified, we'll just remove 'caller'
       const whatToRemove: string[] = info.config.remove || ["caller"];
@@ -10,27 +12,27 @@ function init(modules: { typescript: typeof import("typescript/lib/tsserverlibra
       info.project.projectService.logger.info(
         "I'm getting set up now! Check the log for this message."
       );
-  
+
       // Set up decorator object
-      const proxy: ts.LanguageService = Object.create(null);
-      for (let k of Object.keys(info.languageService) as Array<keyof ts.LanguageService>) {
+      const proxy: typescript.LanguageService = Object.create(null);
+      for (let k of Object.keys(info.languageService) as Array<keyof typescript.LanguageService>) {
         const x = info.languageService[k]!;
         // @ts-expect-error - JS runtime trickery which is tricky to type tersely
         proxy[k] = (...args: Array<{}>) => x.apply(info.languageService, args);
       }
-  
+
       // Remove specified entries from completion list
       proxy.getCompletionsAtPosition = (fileName, position, options) => {
         // This is just to let you hook into something to
         // see the debugger working
-        debugger        
+        debugger
 
         const prior = info.languageService.getCompletionsAtPosition(fileName, position, options);
         if (!prior) return
 
         const oldLength = prior.entries.length;
         prior.entries = prior.entries.filter(e => whatToRemove.indexOf(e.name) < 0);
-  
+
         // Sample logging for diagnostic purposes
         if (oldLength !== prior.entries.length) {
           const entriesRemoved = oldLength - prior.entries.length;
@@ -38,15 +40,15 @@ function init(modules: { typescript: typeof import("typescript/lib/tsserverlibra
             `Removed ${entriesRemoved} entries from the completion list`
           );
         }
-  
+
         return prior;
       };
-  
+
       return proxy;
     }
-  
+
     return { create };
   }
-  
+
   export = init;
-  
+
