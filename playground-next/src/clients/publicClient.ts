a1ab45e9be71fee9124b2af268b90d3026a27654
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-04-20 01:15:29 -0700

    :rotating_light: fix: Fix lint

--- playground-next/src/clients/publicClient.ts
+++ playground-next/src/clients/publicClient.ts
@@ -3,6 +3,6 @@ import { forkUrl, createPublicClient, optimism } from "@evmts/core";
 export const publicClient = createPublicClient({
   chain: optimism,
   transport: forkUrl({
-    url: `https://mainnet.optimism.io`,
+    url: "https://mainnet.optimism.io",
   }),
 })
