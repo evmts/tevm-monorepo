6916ad5d9bed39f9d55823a7f9f4779096a40e90
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-04-30 00:55:01 -0700

    :bug: fix(e2e): Fix e2e nx serve command

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,28 +1,28 @@
-import { PlaywrightTestConfig, devices } from '@playwright/test'
+import { PlaywrightTestConfig, devices } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
-  testDir: 'src',
-  retries: 3,
-  webServer: {
-    command: 'pnpm nx serve @evmts/playground',
-    port: 5173,
-    reuseExistingServer: true,
-    timeout: 180000,
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: { browserName: 'chromium' },
-    },
-    {
-      name: 'firefox',
-      use: { browserName: 'firefox' },
-    },
-    {
-      name: 'mobile-chromium',
-      use: devices['Pixel 5'],
-    },
-  ],
-}
+	testDir: "src",
+	retries: 3,
+	webServer: {
+		command: "pnpm nx serve playground",
+		port: 5173,
+		reuseExistingServer: true,
+		timeout: 180000,
+	},
+	projects: [
+		{
+			name: "chromium",
+			use: { browserName: "chromium" },
+		},
+		{
+			name: "firefox",
+			use: { browserName: "firefox" },
+		},
+		{
+			name: "mobile-chromium",
+			use: devices["Pixel 5"],
+		},
+	],
+};
 
-export default config
+export default config;
