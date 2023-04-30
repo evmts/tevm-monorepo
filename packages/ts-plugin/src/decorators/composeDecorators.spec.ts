ccca5e35f9ca89b0ec2071a7f6b83847e4e37374
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-04-30 00:44:52 -0700

    :white_check_mark: fix(ts-plugin): Fix test

--- packages/ts-plugin/src/decorators/composeDecorators.spec.ts
+++ packages/ts-plugin/src/decorators/composeDecorators.spec.ts
@@ -1,106 +1,70 @@
 import { describe, expect, it, vi } from "vitest";
 import { Decorator, composeDecorators } from ".";
+import typescript from "typescript/lib/tsserverlibrary";
+import { createProxy } from "../factories";
 
 // rome-ignore lint/suspicious/noExplicitAny: <explanation>
 type TestAny = any;
 
 describe(composeDecorators.name, () => {
-	it("should return the instance if there are no decorators", () => {
-		const instance = {} as TestAny;
-		const createInfo = {} as TestAny;
-		const ts = {} as TestAny;
-		const logger = {} as TestAny;
-		const decorators = [] as TestAny[];
+	it("composes decorators into a single decorator", () => {
+		const decorator1 = vi.fn();
+		const decorator2 = vi.fn();
+		const decorator3 = vi.fn();
 
-		const result = composeDecorators(...decorators)(
-			instance,
-			createInfo,
-			ts,
+		const mockDecoratorImplementation =
+			(i: number): Decorator =>
+			(host, createInfo, ts, logger) => {
+				return createProxy(host, { [i]: "decorated" });
+			};
+
+		decorator1.mockImplementation(mockDecoratorImplementation(1));
+		decorator2.mockImplementation(mockDecoratorImplementation(2));
+		decorator3.mockImplementation(mockDecoratorImplementation(3));
+
+		const composedDecorator = composeDecorators(
+			decorator1,
+			decorator2,
+			decorator3,
+		);
+
+		const host = { isHost: true };
+		const createInfo = { isCreateInfo: true };
+		const logger = {
+			log: vi.fn(),
+			info: vi.fn(),
+			warn: vi.fn(),
+			error: vi.fn(),
+		};
+
+		const decoratedHost = composedDecorator(
+			host as TestAny,
+			createInfo as TestAny,
+			typescript,
 			logger,
 		);
 
-		expect(result).toBe(instance);
-	});
+		expect((decoratedHost as TestAny)[1]).toBe("decorated");
+		expect((decoratedHost as TestAny)[2]).toBe("decorated");
+		expect((decoratedHost as TestAny)[3]).toBe("decorated");
 
-	it("should return a Proxy", () => {
-		const originalMethod = "originalMethod";
-		const instance = {
-			originalMethod: () => originalMethod,
-			decoratedMethod: () => {
-				throw new Error("This should not be called");
-			},
-		} as TestAny;
-		const createInfo = {} as TestAny;
-		const ts = {} as TestAny;
-		const logger = {} as TestAny;
-		const expected = "expected";
-		const decorators: TestAny = [() => ({ decoratedMethod: () => expected })];
-
-		const result = composeDecorators(...decorators)(
-			instance,
+		expect(decorator1).toHaveBeenCalledWith(
+			host,
 			createInfo,
-			ts,
+			typescript,
 			logger,
 		);
-
-		expect((result as TestAny).decoratedMethod()).toBe(expected);
-		expect((result as TestAny).originalMethod()).toBe(originalMethod);
-	});
-
-	it("should work with multiple decorators", () => {
-		const originalMethod = "originalMethod";
-		const instance = {
-			originalMethod: () => originalMethod,
-			decoratedMethod: () => {
-				throw new Error("This should not be called");
-			},
-		} as TestAny;
-		const createInfo = {} as TestAny;
-		const ts = {} as TestAny;
-		const logger = {} as TestAny;
-		const expected = "expected";
-		const expected2 = "expected2";
-		const decorators: TestAny = [
-			() => ({ decoratedMethod: () => expected }),
-			() => ({ decoratedMethod2: () => expected2 }),
-		];
-
-		const result = composeDecorators(...decorators)(
-			instance,
+		expect(decorator2).toHaveBeenCalledWith(
+			host,
 			createInfo,
-			ts,
+			typescript,
 			logger,
 		);
-
-		expect((result as TestAny).decoratedMethod()).toBe(expected);
-		expect((result as TestAny).decoratedMethod2()).toBe(expected2);
-		expect((result as TestAny).originalMethod()).toBe(originalMethod);
-	});
-
-	it("should pass in createInfo, ts, and logger into the decorators", () => {
-		const instance = {} as TestAny;
-		const createInfo = {} as TestAny;
-		const ts = {} as TestAny;
-		const logger = {} as TestAny;
-		const expected = "expected";
-		const decoreator = vi.fn();
-		decoreator.mockImplementationOnce((createInfoArg, tsArg, loggerArg) => {
-			expect(createInfoArg).toBe(createInfo);
-			expect(tsArg).toBe(ts);
-			expect(loggerArg).toBe(logger);
-			return { decoratedMethod: () => expected } as TestAny;
-		});
-		const decorators: Decorator[] = [decoreator];
-
-		const result = composeDecorators(...decorators)(
-			instance,
+		expect(decorator3).toHaveBeenCalledWith(
+			host,
 			createInfo,
-			ts,
+			typescript,
 			logger,
 		);
-
-		expect((result as TestAny).decoratedMethod()).toBe(expected);
-		expect(decoreator).toHaveBeenCalledTimes(1);
-		expect(decoreator).toBeCalledWith(createInfo, ts, logger);
 	});
 });
