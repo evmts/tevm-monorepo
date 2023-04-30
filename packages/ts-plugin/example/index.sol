3bc860fc4c1b213384a9d0612094853d5c8a6968
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-04-30 09:06:47 -0700

    :construction: feat(ts-plugin): Update ts-plugin example harness

---
+++ packages/ts-plugin/example/index.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract HelloWorld {
+    function greet() public pure returns (string memory) {
+        return "Hello World";
+    }
+}
--- packages/ts-plugin/example/index.ts
+++ packages/ts-plugin/example/index.ts
@@ -1,4 +1,7 @@
 // Edit this file to trigger the TSServer commands.
 
-const anExampleVariable = 'Hello World'
-console.log(anExampleVariable)
+import { HelloWorld } from './HelloWorld.sol'
+
+export type Abi = typeof HelloWorld.abi
+
+console.log(HelloWorld.abi)
