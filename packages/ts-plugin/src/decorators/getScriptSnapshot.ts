e749d0f86f333657cc64b6ec9f844628dc536f39
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-05-01 14:49:38 -0700

    :fire: chore(ts-plugin): Dead code

--- packages/ts-plugin/src/decorators/getScriptSnapshot.ts
+++ packages/ts-plugin/src/decorators/getScriptSnapshot.ts
@@ -2,32 +2,7 @@ import { createDecorator } from '../factories'
 import { isSolidity } from '../utils'
 import { getArtifactPathSync } from '../utils/getArtifactPathSync'
 import { existsSync, readFileSync } from 'fs'
-import { globSync } from 'glob'
-import { join, resolve } from 'path'
 
-/**
- * @see https://github.com/wagmi-dev/wagmi/blob/main/packages/cli/src/plugins/foundry.ts#LL15C1-L32C2
- */
-
-const defaultIncludes = ['*.json']
-const defaultExcludes = [
-  'Common.sol/**',
-  'Components.sol/**',
-  'Script.sol/**',
-  'StdAssertions.sol/**',
-  'StdError.sol/**',
-  'StdCheats.sol/**',
-  'StdMath.sol/**',
-  'StdJson.sol/**',
-  'StdStorage.sol/**',
-  'StdUtils.sol/**',
-  'Vm.sol/**',
-  'console.sol/**',
-  'console2.sol/**',
-  'test.sol/**',
-  '**.s.sol/*.json',
-  '**.t.sol/*.json',
-]
 /**
  * Decorate `LangaugeServerHost.getScriptSnapshot` to return generated `.d.ts` file for `.sol` files
  * This will allow the language server to provide intellisense for `.sol` files
@@ -36,11 +11,6 @@ const defaultExcludes = [
  */
 export const getScriptSnapshotDecorator = createDecorator(
   ({ languageServiceHost, project }, ts, logger, config) => {
-    // return getArtifactPathSync(
-    //  include[0],
-    //         project.getCurrentDirectory(),
-    //       config
-    //   )
     const getAbiSync = (contractName?: string) => {
       if (!contractName) {
         logger.error(`no contract with name ${contractName} found`)
