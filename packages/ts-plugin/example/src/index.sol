45f944f46ef045777b66862611134b0d72968e3a
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-04-30 19:51:38 -0700

    :fire: chore(ts-plugin): Nuke the example

--- packages/ts-plugin/example/src/index.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-contract HelloWorld {
-    function greet() public pure returns (string memory) {
-        return "Hello World";
-    }
-}
--- packages/ts-plugin/example/src/index.ts
+++
@@ -1,7 +0,0 @@
-// Edit this file to trigger the TSServer commands.
-
-import { HelloWorld } from './index.sol'
-
-export type Abi = typeof HelloWorld.abi
-
-console.log(HelloWorld.abi)
