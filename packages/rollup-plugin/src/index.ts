2016d5dbb0650637712958252f2d0c50a3a23c12
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-05-06 15:53:19 -0700

    :package: feat(monorepo): Add back a blank new rollup-plugin package

---
+++ packages/rollup-plugin/src/index.ts
@@ -0,0 +1,66 @@
+// @ts-ignore - TODO figure out why these types don't work
+import type { Plugin } from 'rollup'
+import { z } from 'zod'
+
+/**
+ * Expected shape of the forge artifacts
+ */
+export const forgeArtifactsValidator = z.object({
+  abi: z.array(
+    z.object({
+      inputs: z.array(
+        z.object({
+          internalType: z.string(),
+          name: z.string(),
+          type: z.string(),
+        }),
+      ),
+      name: z.string(),
+      outputs: z
+        .array(
+          z.object({
+            internalType: z.string(),
+            name: z.string(),
+            type: z.string(),
+          }),
+        )
+        .optional(),
+      stateMutability: z.string().optional(),
+      type: z.string(),
+    }),
+  ),
+  bytecode: z.object({
+    object: z.string(),
+    sourceMap: z.string(),
+  }),
+})
+
+type FoundryOptions = {}
+
+export const foundry = (options: FoundryOptions = {}): Plugin => {
+  return {
+    name: '@evmts/rollup-plugin',
+    version: '0.0.0',
+    buildStart: async () => {},
+    load(id) {
+      if (!id.endsWith('.sol')) {
+        return
+      }
+    },
+  }
+}
+
+type HardhatOptions = {}
+
+export const hardhat = (options: HardhatOptions = {}): Plugin => {
+  return {
+    name: '@evmts/rollup-plugin',
+    version: '0.0.0',
+    buildStart: async () => {},
+    load(id) {
+      if (!id.endsWith('.sol')) {
+        return
+      }
+    },
+  }
+}
