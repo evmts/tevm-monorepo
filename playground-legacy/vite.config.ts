36ee47f06a274767a8e67cff88c988232543f554
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-05-06 18:22:45 -0700

    :bug: fix(playground-legacy): Fix vite.config import

--- playground-legacy/vite.config.ts
+++ playground-legacy/vite.config.ts
@@ -1,4 +1,4 @@
-import { evmtsPlugin } from '@evmts/rollup-plugin'
+import { evmtsPlugin } from '@evmts/rollup-plugin-legacy'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
