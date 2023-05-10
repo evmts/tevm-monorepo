df24da97ce5be6e2d4290b4470a57ae8a512a5e5
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-05-10 00:00:20 -0700

    :sparkles: feat(rollup-plugin): Hook up solts to rollup-plugin

--- packages/rollup-plugin/src/index.ts
+++ packages/rollup-plugin/src/index.ts
@@ -1,68 +1,34 @@
 // @ts-ignore - TODO figure out why these types don't work
+import { FoundryConfig, HardhatConfig, foundryPlugin } from '@evmts/solts'
 import type { Plugin } from 'rollup'
-import { z } from 'zod'
 
-/**
- * Expected shape of the forge artifacts
- */
-export const forgeArtifactsValidator = z.object({
-  abi: z.array(
-    z.object({
-      inputs: z.array(
-        z.object({
-          internalType: z.string(),
-          name: z.string(),
-          type: z.string(),
-        }),
-      ),
-      name: z.string(),
-      outputs: z
-        .array(
-          z.object({
-            internalType: z.string(),
-            name: z.string(),
-            type: z.string(),
-          }),
-        )
-        .optional(),
-      stateMutability: z.string().optional(),
-      type: z.string(),
-    }),
-  ),
-  bytecode: z.object({
-    object: z.string(),
-    sourceMap: z.string(),
-  }),
-})
 
-type FoundryOptions = {}
-
-export const foundry = (options: FoundryOptions = {}): Plugin => {
-  console.log('options', options)
+export const foundry = (options: FoundryConfig = {}): Plugin => {
+  const plugin = foundryPlugin(options, console)
   return {
     name: '@evmts/rollup-plugin',
     version: '0.0.0',
-    buildStart: async () => {},
     load(id) {
       if (!id.endsWith('.sol')) {
         return
       }
+      return plugin.resolveEsmModule(id)
     },
   }
 }
 
-type HardhatOptions = {}
-
-export const hardhat = (options: HardhatOptions = {}): Plugin => {
+export const hardhat = (options: HardhatConfig): Plugin => {
   console.log('options', options)
   return {
     name: '@evmts/rollup-plugin',
     version: '0.0.0',
-    buildStart: async () => {},
     load(id) {
+      const hardhatPlugin = 'TODO' as any
+      const plugin = hardhatPlugin(options, console)
       if (!id.endsWith('.sol')) {
         return
       }
+      return plugin.resolveEsmModule(id)
     },
   }
 }
