9f5500bd44f7bac8e439f57d881f557abe162b6c
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-05-14 23:05:54 -0700

    :fire: fix: unused import

--- e2e/src/pure.spec.ts
+++ e2e/src/pure.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test'
+import { test } from '@playwright/test'
 
 test('should run a pure test', async ({ page }) => {
   await page.goto('http://localhost:5173')
