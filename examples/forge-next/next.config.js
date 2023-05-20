dbc17851730eef8cf2fdb0ef9e5f1e8b47a41af7
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-05-20 07:52:42 -0700

    :package: feat(examples): forge-next pnpm create wagmi forge-next --pnpm --skip-git --template next-rainbowkit

---
+++ examples/forge-next/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false }
+    return config
+  },
+}
