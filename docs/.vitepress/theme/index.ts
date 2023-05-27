ff301ff1563bddb53625770e9f0e88b32f502d0d
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-05-27 08:10:32 -0700

    :rotating_light: fix(docs): Fix linter breaking css

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,4 @@
-// import SvgImage from './components/SvgImage.vue'
 import defaultTheme from 'vitepress/theme'
 import './index.css'
 
-export default {
-	...defaultTheme,
-	enhanceApp({ app }) {
-		// this is how we would add a custom image
-		// app.component('SvgImage', SvgImage)
-	},
-}
+export default defaultTheme
