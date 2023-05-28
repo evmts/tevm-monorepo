a81f2e173ede675d87d8978fd97613ac27fa049e
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-05-28 11:38:32 -0700

    :sparkles: feat: Resolve modules recursively

--- plugins/solidity-resolver/src/solc/SourceDocument.ts
+++ plugins/solidity-resolver/src/solc/SourceDocument.ts
@@ -1,5 +1,18 @@
 import * as path from 'path';
 import { createRequire } from 'node:module';
+
+/**
+ * Copied from rollup (kinda)
+ * @see https://rollupjs.org/plugin-development/#this-getmoduleinfo
+ */
+interface ModuleInfo {
+  id: string; // the id of the module, for convenience
+  rawCode: string | null; // the source code of the module, `null` if external or not yet available
+  code: string | null; // the code after transformed to correctly resolve remappings and node_module imports
+  importedIds: string[]; // the module ids statically imported by this module
+  resolutions: ModuleInfo[]; // how statically imported ids were resolved, for use with this.load
+}
+
 const isomorphicRequire = createRequire(import.meta.url);
 
 
@@ -7,110 +20,88 @@ const formatPath = (contractPath: string) => {
   return contractPath.replace(/\\/g, '/');
 }
 
-export const sourceDocumentFactory = (absolutePath: string, code: string) => {
-  const unformattedCode: string = code;
-  const imports: Array<string> = [];
+function isImportLocal(importPath: string) {
+  return importPath.startsWith('.');
+}
 
-  function getAllLibraryImports(code: string): string[] {
-    const importRegEx = /^\s?import\s+[^'"]*['"](.*)['"]\s*/gm;
-    const imports: string[] = [];
-    let foundImport = importRegEx.exec(code);
-    while (foundImport != null) {
-      const importPath = foundImport[1];
 
-      if (!importPath) {
-        throw new Error('expected import path to exist')
-      }
-
-      if (!isImportLocal(importPath)) {
-        imports.push(importPath);
-      }
-
-      foundImport = importRegEx.exec(code);
-    }
-    return imports;
+/**
+* Resolve import statement to absolute file path
+*
+* @param {string} importPath import statement in *.sol contract
+*/
+function resolveImportPath(absolutePath: string, importPath: string): string {
+  // Foundry remappings
+  const remapping = 'TODO' as any // project.findImportRemapping(importPath);
+  if (remapping !== undefined && remapping != null) {
+    return formatPath(remapping.resolveImport(importPath));
   }
-
-  function isImportLocal(importPath: string) {
-    return importPath.startsWith('.');
+  // Local import "./LocalContract.sol"
+  if (isImportLocal(importPath)) {
+    return formatPath(path.resolve(path.dirname(absolutePath), importPath));
+  } /*else if (project !== undefined && project !== null) {*/
+  // try resolving with node resolution
+  try {
+    return isomorphicRequire.resolve(importPath)
+  } catch (e) {
+    console.error(`Could not resolve import ${importPath} from ${absolutePath}`, e)
+    return importPath
   }
+}
 
-  /**
- * Resolve import statement to absolute file path
- *
- * @param {string} importPath import statement in *.sol contract
- */
-  function resolveImportPath(importPath: string): string {
-    // Foundry remappings
-    const remapping = 'TODO' as any // project.findImportRemapping(importPath);
-    if (remapping !== undefined && remapping != null) {
-      return formatPath(remapping.resolveImport(importPath));
+function replaceDependencyPath(code: string, importPath: string, depImportAbsolutePath: string) {
+  const importRegEx = /(^\s?import\s+[^'"]*['"])(.*)(['"]\s*)/gm;
+  return code.replace(importRegEx, (match, p1, p2, p3) => {
+    if (p2 === importPath) {
+      return p1 + depImportAbsolutePath + p3;
+    } else {
+      return match;
     }
-    // Local import "./LocalContract.sol"
+  });
+}
+
+function resolveImports(absolutePath: string, code: string): string[] {
+  const imports: string[] = []
+  const importRegEx = /^\s?import\s+[^'"]*['"](.*)['"]\s*/gm;
+  let foundImport = importRegEx.exec(code);
+  while (foundImport != null) {
+    const importPath = foundImport[1];
+
+    if (!importPath) {
+      throw new Error('expected import path to exist')
+    }
+
     if (isImportLocal(importPath)) {
-      return formatPath(path.resolve(path.dirname(absolutePath), importPath));
-    } /*else if (project !== undefined && project !== null) {*/
-    // try resolving with node resolution
-    try {
-      return isomorphicRequire.resolve(importPath)
-    } catch (e) {
-      console.error(`Could not resolve import ${importPath} from ${absolutePath}`, e)
-      return importPath
+      const importFullPath = formatPath(path.resolve(path.dirname(absolutePath), importPath));
+      imports.push(importFullPath);
+    } else {
+      imports.push(importPath);
     }
+
+    foundImport = importRegEx.exec(code);
   }
+  return imports
+}
 
-  function getAllImportFromPackages() {
-    const importsFromPackages = new Array<string>();
-    imports.forEach(importElement => {
-      if (!isImportLocal(importElement)) {
-        importsFromPackages.push(importElement);
-      }
-    });
-    return importsFromPackages;
-  }
+export const moduleFactory = (absolutePath: string, rawCode: string): ModuleInfo => {
+  const importedIds = resolveImports(absolutePath, rawCode).map(code => resolveImportPath(absolutePath, code));
+  const resolutions = importedIds.map((importedId) => {
+    const depImportAbsolutePath = resolveImportPath(absolutePath, importedId);
+    const depRawCode = isImportLocal(importedId) ? isomorphicRequire(depImportAbsolutePath) : null;
+    return moduleFactory(depImportAbsolutePath, depRawCode);
+  });
+  const code = importedIds.reduce((code, importPath) => {
+    const depImportAbsolutePath = resolveImportPath(absolutePath, importPath);
+    return replaceDependencyPath(code, importPath, depImportAbsolutePath);
+  }, rawCode);
 
-  function replaceDependencyPath(importPath: string, depImportAbsolutePath: string) {
-    const importRegEx = /(^\s?import\s+[^'"]*['"])(.*)(['"]\s*)/gm;
-    code = code.replace(importRegEx, (match, p1, p2, p3) => {
-      if (p2 === importPath) {
-        return p1 + depImportAbsolutePath + p3;
-      } else {
-        return match;
-      }
-    });
-  }
-
-  function resolveImports() {
-    const importRegEx = /^\s?import\s+[^'"]*['"](.*)['"]\s*/gm;
-    let foundImport = importRegEx.exec(code);
-    while (foundImport != null) {
-      const importPath = foundImport[1];
-
-      if (!importPath) {
-        throw new Error('expected import path to exist')
-      }
-
-      if (isImportLocal(importPath)) {
-        const importFullPath = formatPath(path.resolve(path.dirname(absolutePath), importPath));
-        imports.push(importFullPath);
-      } else {
-        imports.push(importPath);
-      }
-
-      foundImport = importRegEx.exec(code);
-    }
-  }
 
   return {
-    resolveImports,
-    replaceDependencyPath,
-    getAllImportFromPackages,
-    getAllLibraryImports,
-    resolveImportPath,
-    isImportLocal,
-    unformattedCode,
-    imports,
-    code
+    id: absolutePath,
+    rawCode,
+    code,
+    importedIds,
+    resolutions,
   }
 }
 
