663ab5807dc66e4384cd015fcd1b7e298ef51a33
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-05-31 21:38:40 -0700

    :construction: do more demo

--- examples/beta/src/App.tsx
+++ examples/beta/src/App.tsx
@@ -1,13 +1,28 @@
+// @ts-nocheck
+
+import { MyERC20 } from './MyERC20.sol'
+import { useContractRead, useContractWrite } from '@evmts/react'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
-import { useAccount, useContractRead } from 'wagmi'
-import { ExampleContract } from './ExampleContract.sol'
+import { useState } from 'react'
+import { useAccount } from 'wagmi'
 
 export function App() {
-	const { isConnected } = useAccount()
+	const { isConnected, address } = useAccount()
+	const [sendAddress, setSendAddress] = useState('0x4200000000000000000000000000000000000042')
+	const [sendAmount, setSendAmount] = useState(BigInt(0))
 
-	useContractRead({
-		abi: ExampleContract
-	})
+	const { data: tokenBalance } = useContractRead(
+		MyERC20.balanceOf(address),
+		{
+			enabled: isConnected,
+		}
+	)
+
+	const { writeAsync: transfer } = useContractWrite(MyERC20.transfer)
+
+	const send = () => {
+		transfer({ args: [address, sendAddress, sendAmount] })
+	}
 
 	return (
 		<>
@@ -16,8 +31,19 @@ export function App() {
 			<ConnectButton />
 
 			{isConnected && (
-				<>
-				</>
+				<div style={{ display: 'flex' }}>
+					<h2>Connected to {address}</h2>
+					<h3>Balance: {tokenBalance.toString()}</h3>
+					<div>
+						Send to: <input type="text" placeholder="Address" value={sendAddress} onChange={e => {
+							setSendAddress(e.target.value)
+						}} />
+					</div>
+					<div>
+						Amount to send: <input value={sendAmount.toString()} onChange={e => { setSendAmount(BigInt(e.target.value)) }} type="number" placeholder="Address" />
+					</div>
+					<button type="button" onClick={() => send()}>Send tokens</button>
+				</div>
 			)}
 		</>
 	)
