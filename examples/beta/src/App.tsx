c8f35cafe148d0c22afbbb0644585279d09d02f8
Will Cory <willcory10@gmail.com>
2023-06-12 23:34:55 -0400

    :bug: fix(example-beta): Fix readContract example (#135)
    ## Description
    
    _Concise description of proposed changes_
    
    ## Testing
    
    Explain the quality checks that have been done on the code changes
    
    ## Additional Information
    
    - [ ] I read the [contributing docs](../docs/contributing.md) (if this
    is your first contribution)
    
    Your ENS/address:
    
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

--- examples/beta/src/App.tsx
+++ examples/beta/src/App.tsx
@@ -1,23 +1,11 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { useAccount } from 'wagmi'
 
-import { Counter } from './Counter'
+import { ReadContract } from './ReadContract'
 import { Account } from './components/Account'
 import { Balance } from './components/Balance'
 import { BlockNumber } from './components/BlockNumber'
 import { NetworkSwitcher } from './components/NetworkSwitcher'
-import { ReadContract } from './components/ReadContract'
-import { ReadContracts } from './components/ReadContracts'
-import { ReadContractsInfinite } from './components/ReadContractsInfinite'
-import { SendTransaction } from './components/SendTransaction'
-import { SendTransactionPrepared } from './components/SendTransactionPrepared'
-import { SignMessage } from './components/SignMessage'
-import { SignTypedData } from './components/SignTypedData'
-import { Token } from './components/Token'
-import { WatchContractEvents } from './components/WatchContractEvents'
-import { WatchPendingTransactions } from './components/WatchPendingTransactions'
-import { WriteContract } from './components/WriteContract'
-import { WriteContractPrepared } from './components/WriteContractPrepared'
 
 export function App() {
 	const { isConnected } = useAccount()
@@ -37,8 +25,8 @@ export function App() {
 					<Account />
 					<br />
 					<hr />
-					<h2>Counter</h2>
-					<Counter />
+					<h2>EVMts readContract()</h2>
+					<ReadContract />
 					<br />
 					<hr />
 					<h2>Balance</h2>
@@ -47,54 +35,6 @@ export function App() {
 					<hr />
 					<h2>Block Number</h2>
 					<BlockNumber />
-					<br />
-					<hr />
-					<h2>Read Contract</h2>
-					<ReadContract />
-					<br />
-					<hr />
-					<h2>Read Contracts</h2>
-					<ReadContracts />
-					<br />
-					<hr />
-					<h2>Read Contracts Infinite</h2>
-					<ReadContractsInfinite />
-					<br />
-					<hr />
-					<h2>Send Transaction</h2>
-					<SendTransaction />
-					<br />
-					<hr />
-					<h2>Send Transaction (Prepared)</h2>
-					<SendTransactionPrepared />
-					<br />
-					<hr />
-					<h2>Sign Message</h2>
-					<SignMessage />
-					<br />
-					<hr />
-					<h2>Sign Typed Data</h2>
-					<SignTypedData />
-					<br />
-					<hr />
-					<h2>Token</h2>
-					<Token />
-					<br />
-					<hr />
-					<h2>Watch Contract Events</h2>
-					<WatchContractEvents />
-					<br />
-					<hr />
-					<h2>Watch Pending Transactions</h2>
-					<WatchPendingTransactions />
-					<br />
-					<hr />
-					<h2>Write Contract</h2>
-					<WriteContract />
-					<br />
-					<hr />
-					<h2>Write Contract (Prepared)</h2>
-					<WriteContractPrepared />
 				</>
 			)}
 		</>
