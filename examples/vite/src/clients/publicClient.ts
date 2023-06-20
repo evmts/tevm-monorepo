04028dd2f70b43e2c54f59c1751e0e80bdf32bb8
Will Cory <willcory10@gmail.com>
2023-06-20 09:18:28 -0400

    :fire: chore: remove unused example apps for now (#161)
    ## Description
    
    _Concise description of proposed changes_
    
    ## Testing
    
    Explain the quality checks that have been done on the code changes
    
    ## Additional Information
    
    - [ ] I read the [contributing docs](../docs/contributing.md) (if this
    is your first contribution)
    
    Your ENS/address:
    
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

--- examples/vite/src/clients/publicClient.ts
+++
@@ -1,8 +0,0 @@
-import { createPublicClient, forkUrl, optimism } from '@evmts/core'
-
-export const publicClient = createPublicClient({
-	chain: optimism,
-	transport: forkUrl({
-		url: 'https://mainnet.optimism.io',
-	}),
-})
