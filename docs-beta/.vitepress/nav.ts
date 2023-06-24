7be47525fd0d3f78ea51d6f47e172b9d3f7cde16
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-06-24 15:20:04 -0700

    :sparkles: feat(docs-beta): Update nav

--- docs-beta/.vitepress/nav.ts
+++ docs-beta/.vitepress/nav.ts
@@ -3,4 +3,9 @@ import { DefaultTheme } from 'vitepress'
 export const nav: DefaultTheme.NavItem[] = [
 	{ text: 'Home', link: '/' },
 	{ text: 'API', link: '/reference/overview' },
+	{ text: 'Docs', link: '/getting-started/introduction' },
+	{
+		text: 'Example',
+		link: 'https://github.com/evmts/evmts-monorepo/issues/10',
+	},
 ]
