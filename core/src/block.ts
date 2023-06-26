d7853d2e0f9a3166b04fbe25f06f219c805c8c38
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-06-25 22:39:56 -0700

    :fire: chore: remove all the dead code

--- core/src/block.ts
+++
@@ -1,7 +0,0 @@
-import { common } from './hardFork'
-import { Block } from '@ethereumjs/block'
-
-export const block = Block.fromBlockData(
-	{ header: { extraData: Buffer.alloc(97) } },
-	{ common },
-)
