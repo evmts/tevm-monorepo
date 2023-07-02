d616721f0ad6eb8fba5d0d1286875e3d164f642c
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-07-02 09:15:18 -0700

    :bug: fix(ts-plugin): Fix relative solidity detection

--- lsp/ts-plugin/src/utils/isRelativeSolidity.ts
+++ lsp/ts-plugin/src/utils/isRelativeSolidity.ts
@@ -1,7 +1,10 @@
 import { isSolidity } from './isSolidity'
 
+const isRelative = (fileName: string) =>
+	fileName.startsWith('./') || fileName.startsWith('../')
+
 /**
  * Util to determine if a file path is a solidity file referenced via a relative import
  */
 export const isRelativeSolidity = (fileName: string) =>
-	fileName.startsWith('./') && isSolidity(fileName)
+	isRelative(fileName) && isSolidity(fileName)
