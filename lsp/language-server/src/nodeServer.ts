40784f8c753d3ca47751d836010c5ce083989425
Will Cory <willcory10@gmail.com>
2023-07-07 20:38:03 -0700

    :construction_worker: feat(language-server): Use tsup for build (#238)
    ## Description
    
    Switch from using tsc to tsup
    
    ## Testing
    
    Explain the quality checks that have been done on the code changes
    
    ## Additional Information
    
    - [ ] I read the [contributing docs](../docs/contributing.md) (if this
    is your first contribution)
    
    Your ENS/address:
    
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

--- lsp/language-server/src/nodeServer.ts
+++ lsp/language-server/src/nodeServer.ts
@@ -1,7 +1,13 @@
+#!/usr/bin/env node
 import { plugin } from './languageServerPlugin'
 import {
 	createConnection,
 	startLanguageServer,
 } from '@volar/language-server/node'
 
-startLanguageServer(createConnection(), plugin)
+if (process.argv.includes('--version')) {
+	const pkgJSON = require('../package.json')
+	console.log(`${pkgJSON['version']}`)
+} else {
+	startLanguageServer(createConnection(), plugin)
+}
