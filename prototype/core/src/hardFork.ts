c740741dd524e55278a122ad7cc87910a6a58d8b
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-07-07 22:43:35 -0700

    :fire: chore: Remove prototype

--- prototype/core/src/hardFork.ts
+++
@@ -1,6 +0,0 @@
-import { Chain, Common, Hardfork } from '@ethereumjs/common'
-
-export const common = new Common({
-	chain: Chain.Rinkeby,
-	hardfork: Hardfork.Istanbul,
-})
--- prototype/solidity-resolver-legacy/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './buildContracts'
-export * from './getArtifactsPath'
-export * from './getFoundryConfig'
-export * from './getArtifacts'
-export * from './createModule'
-export * from './getFileName'
-export * from './forgeArtifactsValidator'
