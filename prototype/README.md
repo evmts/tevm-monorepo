9d69233db16be6436e6a1a326c3bc66b93d14d20
Will Cory <willcory10@gmail.com>
2023-07-08 00:02:08 -0700

    :fire: chore: Remove prototype (#244)
    ## Description
    
    Remove prototype app now that alpha release exists
    
    ## Testing
    
    Explain the quality checks that have been done on the code changes
    
    ## Additional Information
    
    - [ ] I read the [contributing docs](../docs/contributing.md) (if this
    is your first contribution)
    
    Your ENS/address:
    
    ---------
    
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

--- prototype/README.md
+++
@@ -1,4 +0,0 @@
-# prototype/
-
-The original EVMts proof of concept.  This is here for reference and will be deleted after beta release
-
--- prototype/solidity-resolver-legacy/src/buildContracts.ts
+++
@@ -1,9 +0,0 @@
-import { FoundryOptions } from './getFoundryConfig'
-import { execa } from 'execa'
-
-export const buildContracts = async ({
-	forgeExecutable = 'forge',
-	projectRoot = process.cwd(),
-}: FoundryOptions) => {
-	return execa(forgeExecutable, ['build', '--root', projectRoot])
-}
