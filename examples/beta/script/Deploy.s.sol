6cad9a8adbe158c59e3d3d26f33db7002ae21031
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-07-07 22:40:54 -0700

    :bug: fix(example-beta): Fix deployment scripts

--- examples/beta/script/Deploy.s.sol
+++ examples/beta/script/Deploy.s.sol
@@ -1,17 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.19;
 
-import {Script} from "forge-std/Script.sol";
-import {Counter} from "../src/contracts/Counter.sol";
+import {Script} from "forge-std/src/Script.sol";
+import {WagmiMintExample} from "../src/contracts/WagmiMintExample.sol";
 
 contract Deploy is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
 
-
         vm.startBroadcast(deployerPrivateKey);
 
-        Counter counter = new Counter();
+        WagmiMintExample wagmiMintExample = new WagmiMintExample();
 
         vm.stopBroadcast();
     }
--- examples/beta/src/wagmi.ts
+++ examples/beta/src/wagmi.ts
@@ -1,13 +1,23 @@
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
 import { configureChains, createConfig } from 'wagmi'
-import { goerli, mainnet } from 'wagmi/chains'
-import { publicProvider } from 'wagmi/providers/public'
+import { mainnet, optimismGoerli } from 'wagmi/chains'
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 
 const walletConnectProjectId = '898f836c53a18d0661340823973f0cb4'
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-	[mainnet, ...(import.meta.env?.MODE === 'development' ? [goerli] : [])],
-	[publicProvider()],
+	[mainnet, optimismGoerli],
+	[
+		jsonRpcProvider({
+			rpc: (chain) => {
+				const urls = {
+					1: { http: import.meta.env.VITE_RPC_URL_1 },
+					420: { http: import.meta.env.VITE_RPC_URL_420 },
+				}
+				return [1, 420].includes(chain.id) ? urls[chain.id as 1 | 420] : null
+			},
+		}),
+	],
 )
 
 const { connectors } = getDefaultWallets({
