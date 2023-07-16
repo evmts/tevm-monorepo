64e0652b286a5fbebcfa3679575d5ecbef12a4a8
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-07-16 04:25:21 -0700

    :test_tube: chore: Add coverage thresholds

--- core/vitest.config.ts
+++ core/vitest.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
 		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 		coverage: {
 			reporter: ['text', 'json-summary', 'json'],
+			lines: 80,
+			branches: 80,
+			functions: 80,
+			statements: 80
 		},
 	},
 })
