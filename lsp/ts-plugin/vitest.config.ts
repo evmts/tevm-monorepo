98f8286d6842bf38ccd14cf115e42086afa71ff8
Will Cory <willcory10@gmail.com>
2023-07-16 07:29:13 -0700

    :test_tube: chore: Raise the code coverage bar (#324)
    ## Description
    
    Raise code coverage bar
    - keep ts-plugin lower since it's migrating to volar
    
    ## Testing
    
    Explain the quality checks that have been done on the code changes
    
    ## Additional Information
    
    - [ ] I read the [contributing docs](../docs/contributing.md) (if this
    is your first contribution)
    
    Your ENS/address:
    
    ---------
    
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

--- lsp/ts-plugin/vitest.config.ts
+++ lsp/ts-plugin/vitest.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
 		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 		coverage: {
 			reporter: ['text', 'json-summary', 'json'],
-			lines: 80,
-			branches: 80,
-			functions: 80,
-			statements: 80,
+			lines: 90,
+			branches: 90,
+			functions: 90,
+			statements: 90,
 		},
 	},
 })
