1a354a6d2116ef132da4501e877cee658238a72c
Will Cory <willcory10@gmail.com>
2023-07-15 18:29:26 -0700

    :construction_worker: chore: Add test coverage to gha (#311)
    ## Description
    
    - add test coverage to config package
    - add to github actions
    - don't bother adding to packages that are just exporting stuff from
    bundler like all the bundler plugins
    
    ## Testing
    
    Explain the quality checks that have been done on the code changes
    
    ## Additional Information
    
    - [ ] I read the [contributing docs](../docs/contributing.md) (if this
    is your first contribution)
    
    Your ENS/address:
    
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

--- lsp/ts-plugin/vitest.config.ts
+++ lsp/ts-plugin/vitest.config.ts
@@ -4,5 +4,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
 	test: {
 		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+		coverage: {
+			reporter: ['text', 'json-summary', 'json'],
+		},
 	},
 })
