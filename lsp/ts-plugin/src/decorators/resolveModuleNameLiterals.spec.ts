692c326f58b3f873395eb268b5f546fdb1517864
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-07-15 17:26:44 -0700

    :white_check_mark: test(ts-plugin): Add complete test coverage to lsp

--- lsp/ts-plugin/src/decorators/resolveModuleNameLiterals.spec.ts
+++ lsp/ts-plugin/src/decorators/resolveModuleNameLiterals.spec.ts
@@ -1,9 +1,21 @@
 import { resolveModuleNameLiteralsDecorator } from '.'
+import { solidityModuleResolver } from '../utils'
 import { EVMtsConfig } from '@evmts/config'
 import typescript from 'typescript/lib/tsserverlibrary'
-import { describe, expect, it, vi } from 'vitest'
+import { MockedFunction, describe, expect, it, vi } from 'vitest'
 
 const config: EVMtsConfig = {}
+
+const mockSolidityModuleResolver = solidityModuleResolver as MockedFunction<
+	typeof solidityModuleResolver
+>
+
+// mock solidityModuleResolver
+vi.mock('../utils', () => {
+	return {
+		solidityModuleResolver: vi.fn(),
+	}
+})
 
 describe(resolveModuleNameLiteralsDecorator.name, () => {
 	it('should decorate resolveModuleNameLiterals', () => {
@@ -34,10 +46,10 @@ describe(resolveModuleNameLiteralsDecorator.name, () => {
 		)
 
 		expect(host).toMatchInlineSnapshot(`
-      {
-        "resolveModuleNameLiterals": [Function],
-      }
-    `)
+			{
+			  "resolveModuleNameLiterals": [Function],
+			}
+		`)
 
 		const moduleNames: any[] = []
 		const containingFile = 'foo.ts'
@@ -53,6 +65,111 @@ describe(resolveModuleNameLiteralsDecorator.name, () => {
 			createInfo.languageServiceHost.resolveModuleNameLiterals,
 		).toHaveBeenCalledWith(moduleNames, containingFile, ...rest)
 
-		expect(res).toEqual([])
+		expect(res).toMatchInlineSnapshot('[]')
+	})
+
+	it('should return resolved module when solidityModuleResolver returns a resolved module', () => {
+		const logger = {
+			info: vi.fn(),
+			error: vi.fn(),
+			log: vi.fn(),
+			warn: vi.fn(),
+		}
+		const createInfo = {
+			languageServiceHost: {
+				resolveModuleNameLiterals: vi.fn().mockReturnValue([undefined]),
+			},
+			project: {
+				getCompilerOptions: () => ({ baseUrl: 'foo' }),
+				projectService: {
+					logger: {
+						info: vi.fn(),
+					},
+				},
+			},
+		} as any
+		const host = resolveModuleNameLiteralsDecorator(
+			createInfo,
+			typescript,
+			logger,
+			config,
+		)
+
+		const moduleNames = [{ text: 'moduleName' }]
+		const containingFile = 'foo.ts'
+		const rest = [{} as any, {} as any, {} as any, {} as any] as const
+
+		// When solidityModuleResolver returns a resolved module
+		mockSolidityModuleResolver.mockReturnValueOnce({
+			resolvedModule: {},
+		} as any)
+
+		const res = host.resolveModuleNameLiterals?.(
+			moduleNames as any,
+			containingFile,
+			...rest,
+		)
+		expect(res).toMatchInlineSnapshot(`
+			[
+			  {
+			    "resolvedModule": {
+			      "resolvedModule": {},
+			    },
+			  },
+			]
+		`)
+	})
+
+	it('should handle error when solidityModuleResolver throws an error', () => {
+		const logger = {
+			info: vi.fn(),
+			error: vi.fn(),
+			log: vi.fn(),
+			warn: vi.fn(),
+		}
+		const createInfo = {
+			languageServiceHost: {
+				resolveModuleNameLiterals: vi.fn().mockReturnValue([undefined]),
+			},
+			project: {
+				getCompilerOptions: () => ({ baseUrl: 'foo' }),
+				projectService: {
+					logger: {
+						info: vi.fn(),
+					},
+				},
+			},
+		} as any
+		const host = resolveModuleNameLiteralsDecorator(
+			createInfo,
+			typescript,
+			logger,
+			config,
+		)
+
+		const moduleNames = [{ text: 'moduleName' }]
+		const containingFile = 'foo.ts'
+		const rest = [{} as any, {} as any, {} as any, {} as any] as const
+
+		// When solidityModuleResolver throws an error
+		mockSolidityModuleResolver.mockImplementationOnce(() => {
+			throw new Error('Error')
+		})
+
+		const res = host.resolveModuleNameLiterals?.(
+			moduleNames as any,
+			containingFile,
+			...rest,
+		)
+		expect(res).toMatchInlineSnapshot(`
+			[
+			  undefined,
+			]
+		`)
+		expect(logger.error.mock.lastCall).toMatchInlineSnapshot(`
+			[
+			  [Error: Error],
+			]
+		`)
 	})
 })
