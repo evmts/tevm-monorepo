94209ae59a7f5d999180dbd2e8b2edc24afa4737
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-07-17 16:13:00 -0700

    :rotating_light: feat(bundler): Add warning if solc compiler is not configured correctly

---
+++ bundlers/bundler/src/utils/checkSolcVersion.ts
@@ -0,0 +1,19 @@
+import { Logger } from '../types'
+import { ResolvedConfig } from '@evmts/config'
+// TODO wrap this in a typesafe version
+// @ts-ignore
+import solc from 'solc'
+
+export const checkSolcVersion = (
+	config: ResolvedConfig,
+	logger: Logger,
+	version = solc.version,
+) => {
+	if (config.compiler.solcVersion !== version) {
+		logger.warn(
+			`The solc version in the config (${config.compiler.solcVersion}) does not match the solc version installed (${solc.version}).
+			This may cause unexpected behavior.
+			Consider updating the version in package.json to "solc": "${solc.version}"`,
+		)
+	}
+}
