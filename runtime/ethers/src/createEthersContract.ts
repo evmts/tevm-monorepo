ba2e443d5c151d9469900262953f6d2706641897
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-07-29 16:49:23 -0700

    :memo: feat(ethers): Add jsdoc comments

--- runtime/ethers/src/createEthersContract.ts
+++ runtime/ethers/src/createEthersContract.ts
@@ -1,17 +1,30 @@
 import type { TypesafeEthersContract } from './TypesafeEthersContract'
 import type { EvmtsContract } from '@evmts/core'
-import type { Abi, Address, FormatAbi } from 'abitype'
-import { Contract, type ContractRunner, Interface } from 'ethers'
+import type { Abi, Address } from 'abitype'
+import { Contract, type ContractRunner, Interface, type InterfaceAbi } from 'ethers'
 
 export type CreateEthersContractOptions<TChainIds extends number> =
 	| {
-			chainId: TChainIds
-			runner: ContractRunner
-	  }
+		/**
+		 * Provide a chainId if EVMts config has addresses for contracts configured for that chain.
+		 * Otherwise provide the `address` prop to specify the address
+		 */
+		chainId: TChainIds
+		/**
+		 * Ethers.js provider or signer
+		 */
+		runner: ContractRunner
+	}
 	| {
-			address: Address
-			runner: ContractRunner
-	  }
+		/**
+		 * Address of the contract. If EVMts config has addresses configured already simply provide the chainId
+		 */
+		address: Address
+		/**
+		 * Ethers.js provider or signer
+		 */
+		runner: ContractRunner
+	}
 
 /**
  * Create an ethers contract from an evmts contract
@@ -22,29 +35,27 @@ export type CreateEthersContractOptions<TChainIds extends number> =
  * const contract = createEthersContract(myContract, {chainId: 1, runner: provider})
  */
 export const createEthersContract = <
-	TName extends string,
 	TAddresses extends Record<number, Address>,
 	TAbi extends Abi,
-	THumanReadableAbi = FormatAbi<TAbi>,
 >(
 	contract: Pick<
-		EvmtsContract<TName, TAddresses, TAbi, THumanReadableAbi>,
+		EvmtsContract<any, TAddresses, TAbi, any>,
 		'abi' | 'addresses'
 	>,
-	options: CreateEthersContractOptions<number | (keyof TAddresses & number)>,
+	options: CreateEthersContractOptions<keyof TAddresses & number>,
 ): TypesafeEthersContract<TAbi> => {
 	const getAddress = () => {
 		if ('address' in options && options.address) {
 			return options.address
 		} else if ('chainId' in options && options.chainId) {
-			return contract.addresses[options.chainId]
+			return contract.addresses[options.chainId as keyof TAddresses]
 		} else {
 			throw new Error('No chainId or address provided')
 		}
 	}
 	return new Contract(
 		getAddress(),
-		new Interface(contract.abi as any),
+		new Interface(contract.abi as InterfaceAbi),
 		options.runner,
 	) as TypesafeEthersContract<TAbi>
 }
