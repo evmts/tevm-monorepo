c81f2acbfa7aa486ad4d11694d60bcc1803e3d7f
Will Cory <willcory10@gmail.com>
2023-07-29 05:20:35 -1000

    :sparkles: feat: Add ethers.js support (#385)
    ## Description
    
    Add ethers.js support without compromising on typesafety. Typesafe
    events not yet supported
    - Use abitype to infer types
    - update package.json to reflect peerDeps correclty
    - Wrap contract methods with BaseContractMethod type
    - Wrap contract with TypesafeEthersContract type
    - Implement API that takes an EVMts contract as an arg
    
    ## Testing
    
    Explain the quality checks that have been done on the code changes
    
    ## Additional Information
    
    - [ ] I read the [contributing docs](../docs/contributing.md) (if this
    is your first contribution)
    
    Your ENS/address:
    
    ---------
    
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

--- runtime/ethers/src/createEthersContract.spec.ts
+++ runtime/ethers/src/createEthersContract.spec.ts
@@ -1,9 +1,7 @@
-import { createEthersContract } from '.'
+import { createEthersContract } from './createEthersContract'
 import { evmtsContractFactory } from '@evmts/core'
-import { Contract, JsonRpcProvider /*Wallet*/ } from 'ethers'
-import { expect, test } from 'vitest'
-
-// const anvilPrivateKey = '0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6'
+import { Contract, JsonRpcProvider } from 'ethers'
+import { describe, expect, test } from 'vitest'
 
 const abi = [
 	{
@@ -206,20 +204,63 @@ const abi = [
 		type: 'event',
 	},
 ] as const
-const contract = evmtsContractFactory({
+
+const evmtsContract = evmtsContractFactory({
 	abi,
 	name: 'test',
 	addresses: { 420: '0x32307adfFE088e383AFAa721b06436aDaBA47DBE' },
 	bytecode: '0x0',
 })
-const provider = new JsonRpcProvider('https://goerli.optimism.io', 5)
 
-test(createEthersContract.name, () => {
-	if (createEthersContract.name) {
-		expect(createEthersContract.name).toBe(createEthersContract.name)
-		return
-	}
-	const c = createEthersContract(contract, { chainId: 420, runner: provider })
-	expect(c).toBeInstanceOf(Contract)
-	expect(c).toMatchInlineSnapshot()
+const provider = new JsonRpcProvider('https://goerli.optimism.io', 420)
+
+describe(createEthersContract.name, () => {
+	test('should work with chainId and evmts contract addresses', async () => {
+		const c = createEthersContract(evmtsContract, {
+			chainId: 420,
+			runner: provider,
+		})
+		expect(c).toBeInstanceOf(Contract)
+		expect(await c.name()).toMatchInlineSnapshot('"OptimismUselessToken-1"')
+		expect(await c.symbol()).toMatchInlineSnapshot('"OUT-1"')
+		expect(await c.decimals()).toMatchInlineSnapshot('18n')
+		expect(await c.totalSupply()).toMatchInlineSnapshot(
+			'70000000000000000000000n',
+		)
+		expect(
+			await c.balanceOf('0x32307adfFE088e383AFAa721b06436aDaBA47DBE'),
+		).toMatchInlineSnapshot('0n')
+	})
+
+	test('should work with custom address', async () => {
+		const c = createEthersContract(evmtsContract, {
+			address: evmtsContract.addresses[420],
+			runner: provider,
+		})
+		expect(c).toBeInstanceOf(Contract)
+		expect(await c.name()).toMatchInlineSnapshot('"OptimismUselessToken-1"')
+		expect(await c.symbol()).toMatchInlineSnapshot('"OUT-1"')
+		expect(await c.decimals()).toMatchInlineSnapshot('18n')
+		expect(await c.totalSupply()).toMatchInlineSnapshot(
+			'70000000000000000000000n',
+		)
+		expect(
+			await c.balanceOf('0x32307adfFE088e383AFAa721b06436aDaBA47DBE'),
+		).toMatchInlineSnapshot('0n')
+	})
+
+	test('should throw an error if no chainId or address provided', () => {
+		expect(() =>
+			// @ts-expect-error
+			createEthersContract(evmtsContract, { runner: provider }),
+		).toThrowErrorMatchingInlineSnapshot('"No chainId or address provided"')
+		expect(() =>
+			createEthersContract(evmtsContract, {
+				runner: provider,
+				address: undefined,
+				// @ts-expect-error
+				chainId: undefined,
+			}),
+		).toThrowErrorMatchingInlineSnapshot('"No chainId or address provided"')
+	})
 })
