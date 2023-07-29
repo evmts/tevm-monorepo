d89c9313bc77423161aaf878c5db60838840dbc4
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-07-29 16:00:02 -0700

    :white_check_mark: test: Add unit test to esbuild plugin

--- bundlers/esbuild-plugin/src/index.spec.ts
+++ bundlers/esbuild-plugin/src/index.spec.ts
@@ -1,7 +1,38 @@
-import { describe, expect, it } from 'vitest'
+import { esbuildPluginEvmts } from '@evmts/bundler'
+import { describe, expect, it, vi } from 'vitest'
 
-describe('TODO', () => {
-	it('should write a test', () => {
-		expect(parseInt('420wtf how is this parsing')).toBe(420)
+describe(esbuildPluginEvmts.name, () => {
+	it('should properly export the unplugin bundler from @evmts/bundler', async () => {
+		const plugin = esbuildPluginEvmts()
+
+		expect(plugin.name).toMatchInlineSnapshot('"@evmts/rollup-plugin"')
+
+		const initialOptions = {}
+		const resolve = vi.fn()
+		const onStart = vi.fn()
+		const onEnd = vi.fn()
+		const onResolve = vi.fn()
+		const onLoad = vi.fn()
+		const onDispose = vi.fn()
+		const esbuild = {
+			context: vi.fn(),
+			build: vi.fn(),
+			version: 420,
+			buildSync: vi.fn(),
+			transform: vi.fn(),
+			transformSync: vi.fn()
+		} as any
+		await plugin.setup({
+			initialOptions,
+			resolve,
+			onStart,
+			onEnd,
+			onResolve,
+			onLoad,
+			onDispose,
+			esbuild
+		})
+		expect(onStart).toBeCalledTimes(1)
+		expect(onLoad).toBeCalledTimes(1)
 	})
 })
