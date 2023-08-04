fc28f545635a23a76e4acce0ff48d0902eed484c
Will Cory <willcory10@gmail.com>
2023-08-04 08:34:35 -0700

    :bug: fix(bundler): Remove another recursive function for an iterative approach (#416)
    ## Description
    
    Fix bug where a recursive function was causing bundler to fail when
    import graph was deep
    
    ## Testing
    
    Explain the quality checks that have been done on the code changes
    
    ## Additional Information
    
    - [ ] I read the [contributing docs](../docs/contributing.md) (if this
    is your first contribution)
    
    Your ENS/address:
    
    ---------
    
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

---
+++ .changeset/violet-shrimps-camp.md
@@ -0,0 +1,5 @@
+---
+"@evmts/bundler": patch
+---
+
+Fixed another stack too deep bug from a recursive function in bundler
--- bundlers/bundler/src/solc/moduleFactory.ts
+++ bundlers/bundler/src/solc/moduleFactory.ts
@@ -1,52 +1,81 @@
 import type { ModuleInfo } from '../types'
+import { invariant } from '../utils/invariant'
 import { resolveImportPath } from './resolveImportPath'
 import { resolveImports } from './resolveImports'
 import { readFileSync } from 'fs'
-
 export const moduleFactory = (
 	absolutePath: string,
 	rawCode: string,
 	remappings: Record<string, string>,
 	libs: string[],
 ): ModuleInfo => {
-	const importedIds = resolveImports(absolutePath, rawCode).map((paths) =>
-		resolveImportPath(absolutePath, paths, remappings, libs),
-	)
-	const resolutions = importedIds.map((importedId) => {
-		const depImportAbsolutePath = resolveImportPath(
-			absolutePath,
-			importedId,
-			remappings,
-			libs,
-		)
-		const depRawCode = readFileSync(depImportAbsolutePath, 'utf8')
-		return moduleFactory(depImportAbsolutePath, depRawCode, remappings, libs)
-	})
-	const importRegEx = /(^\s?import\s+[^'"]*['"])(.*)(['"]\s*)/gm
-	const code = importedIds.reduce((code, importedId) => {
-		const depImportAbsolutePath = resolveImportPath(
-			absolutePath,
-			importedId,
-			remappings,
-			libs,
-		)
-		return code.replace(importRegEx, (match, p1, p2, p3) => {
-			const resolvedPath = resolveImportPath(absolutePath, p2, remappings, libs)
-			if (resolvedPath === importedId) {
-				return `${p1}${depImportAbsolutePath}${p3}`
-			} else {
-				return match
-			}
-		})
-	}, rawCode)
+	const stack = [{ absolutePath, rawCode }]
+	const modules = new Map<string, ModuleInfo>()
 
-	const out = {
-		id: absolutePath,
-		rawCode,
-		code,
-		importedIds,
-		resolutions,
+	while (stack.length) {
+		const nextItem = stack.pop()
+		invariant(nextItem, 'Module should exist')
+		const { absolutePath, rawCode } = nextItem
+
+		if (modules.has(absolutePath)) continue
+
+		const importedIds = resolveImports(absolutePath, rawCode).map((paths) =>
+			resolveImportPath(absolutePath, paths, remappings, libs),
+		)
+
+		const importRegEx = /(^\s?import\s+[^'"]*['"])(.*)(['"]\s*)/gm
+		const code = importedIds.reduce((code, importedId) => {
+			const depImportAbsolutePath = resolveImportPath(
+				absolutePath,
+				importedId,
+				remappings,
+				libs,
+			)
+			return code.replace(importRegEx, (match, p1, p2, p3) => {
+				const resolvedPath = resolveImportPath(
+					absolutePath,
+					p2,
+					remappings,
+					libs,
+				)
+				if (resolvedPath === importedId) {
+					return `${p1}${depImportAbsolutePath}${p3}`
+				} else {
+					return match
+				}
+			})
+		}, rawCode)
+
+		modules.set(absolutePath, {
+			id: absolutePath,
+			rawCode,
+			code,
+			importedIds,
+			resolutions: [],
+		})
+
+		importedIds.forEach((importedId) => {
+			const depImportAbsolutePath = resolveImportPath(
+				absolutePath,
+				importedId,
+				remappings,
+				libs,
+			)
+			const depRawCode = readFileSync(depImportAbsolutePath, 'utf8')
+
+			stack.push({ absolutePath: depImportAbsolutePath, rawCode: depRawCode })
+		})
 	}
 
-	return out
+	for (const [_, m] of modules.entries()) {
+		const { importedIds } = m
+		m.resolutions = []
+		importedIds.forEach((importedId) => {
+			const resolution = modules.get(importedId)
+			invariant(resolution, `resolution for ${importedId} not found`)
+			m.resolutions.push(resolution)
+		})
+	}
+
+	return modules.get(absolutePath) as ModuleInfo
 }
