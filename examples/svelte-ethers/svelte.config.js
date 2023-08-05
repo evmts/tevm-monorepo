b59ef27db6aa048064dcb83f2c5f82a75d18da8b
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-08-05 10:50:44 -0700

    :rotating_light: fix: linter

--- examples/svelte-ethers/svelte.config.js
+++ examples/svelte-ethers/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,8 +6,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
+		adapter: adapter(),
+	},
+}
 
-export default config;
+export default config
--- examples/svelte-ethers/vite.config.js
+++ examples/svelte-ethers/vite.config.js
@@ -1,10 +1,10 @@
-import { vitePluginEvmts } from '@evmts/vite-plugin';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { vitePluginEvmts } from '@evmts/vite-plugin'
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	plugins: [sveltekit(), vitePluginEvmts()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
-});
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+	},
+})
