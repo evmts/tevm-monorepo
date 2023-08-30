258a96fa5ab776c1c9d07470b86d71de8360c63c
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-08-30 03:12:05 -0700

    :recycle: chore: Clean up compileContracts

--- bundlers/bundler/src/solc/compileContracts.ts
+++ bundlers/bundler/src/solc/compileContracts.ts
@@ -5,11 +5,7 @@ import { findAll } from 'solidity-ast/utils'
 import type { ModuleInfo } from '../types'
 import { invariant } from '../utils/invariant'
 import { moduleFactory } from './moduleFactory'
-import {
-	type SolcInputDescription,
-	type SolcOutput,
-	solcCompile,
-} from './solc'
+import { type SolcInputDescription, type SolcOutput, solcCompile } from './solc'
 
 // Compile the Solidity contract and return its ABI
 export const compileContractSync = (
@@ -21,49 +17,36 @@ export const compileContractSync = (
 	artifacts: SolcOutput['contracts'][string] | undefined
 	modules: Record<'string', ModuleInfo>
 } => {
-	const source: string = readFileSync(
-		resolve.sync(filePath, {
-			basedir,
-		}),
-		'utf8',
-	)
-
 	const entryModule = moduleFactory(
 		filePath,
-		source,
+		readFileSync(
+			resolve.sync(filePath, {
+				basedir,
+			}),
+			'utf8',
+		),
 		config.remappings,
 		config.libs,
 	)
 
-	const getAllModulesRecursively = (
-		entryModule: ModuleInfo,
-	): Record<string, ModuleInfo> => {
-		const modules: Record<string, ModuleInfo> = {}
-		const stack = [entryModule]
+	const modules: Record<string, ModuleInfo> = {}
 
-		while (stack.length !== 0) {
-			const m = stack.pop()
-			// This is always existing because we check the length but need to make TS happy
-			invariant(m, 'Module should exist')
-
-			// Continue the loop if this module has already been visited.
-			if (m.id in modules) {
-				continue
-			}
-
-			modules[m.id] = m
-
-			for (const dep of m.resolutions) {
-				stack.push(dep)
-			}
+	// Get modules by recursively resolving dependencies
+	const stack = [entryModule]
+	while (stack.length !== 0) {
+		const m = stack.pop()
+		invariant(m, 'Module should exist')
+		if (m.id in modules) {
+			continue
 		}
-
-		return modules
+		modules[m.id] = m
+		for (const dep of m.resolutions) {
+			stack.push(dep)
+		}
 	}
-	const allModules = getAllModulesRecursively(entryModule)
 
 	const sources = Object.fromEntries(
-		Object.entries(allModules).map(([id, module]) => {
+		Object.entries(modules).map(([id, module]) => {
 			return [id, { content: module.code as string }]
 		}),
 	)
@@ -75,7 +58,7 @@ export const compileContractSync = (
 			outputSelection: {
 				'*': {
 					'*': ['abi', 'userdoc'],
-					...(includeAst ? { "": ["ast"] } : {}),
+					...(includeAst ? { '': ['ast'] } : {}),
 				},
 			},
 		},
@@ -105,5 +88,5 @@ export const compileContractSync = (
 		}
 	}
 
-	return { artifacts: output.contracts[entryModule.id], modules: allModules }
+	return { artifacts: output.contracts[entryModule.id], modules }
 }
