4650a2f0b5dce94ff1e253a1a743c8ea0ab5d48c
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-09-27 14:45:13 -0700

    :recycle: update bun

--- examples/bun/readContract.ts
+++ examples/bun/readContract.ts
@@ -2,6 +2,11 @@ import { ExampleContract } from './ExampleContract.sol'
 import { createPublicClient, http } from 'viem'
 import { optimismGoerli } from 'viem/chains'
 
+const addresses = {
+	"1": "0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2",
+	"420": "0x1df10ec981ac5871240be4a94f250dd238b77901"
+}
+
 export const publicClient = createPublicClient({
 	chain: optimismGoerli,
 	transport: http('https://goerli.optimism.io'),
@@ -10,8 +15,10 @@ export const publicClient = createPublicClient({
 const defaultOwner = '0x8f0ebdaa1cf7106be861753b0f9f5c0250fe0819'
 
 export const readContract = (owner = defaultOwner) =>
-	publicClient.readContract(
-		ExampleContract.read({ chainId: optimismGoerli.id }).balanceOf(owner),
+	publicClient.readContract({
+		address: addresses[420],
+		...ExampleContract.read({ chainId: optimismGoerli.id }).balanceOf(owner),
+	}
 	)
 
 if (import.meta.main) {
