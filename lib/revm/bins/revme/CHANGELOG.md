cda17e95f438c1579fd3acb590b717e1d9080564
Will Cory <willcory10@gmail.com>
2023-09-30 13:24:23 -0700

    :bug: fix(revm): Add correct package to workspace (#497)
    ## Description
    
    Changed the name from revmts to revm and forgot to update pnpm-workspace
    
    ## Testing
    
    Explain the quality checks that have been done on the code changes
    
    ## Additional Information
    
    - [ ] I read the [contributing docs](../docs/contributing.md) (if this
    is your first contribution)
    
    Your ENS/address:
    
    ---------
    
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

--- lib/revm/bins/revme/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-# v0.1.0
-date: 18.12.2021
-
-Initial release. statetest are done, other things I have just started working on.
--- lib/revm/crates/revm/src/inspector/noop.rs
+++
@@ -1,8 +0,0 @@
-//! Dummy NoOp Inspector, helpful as standalone replacement.
-
-use crate::{Database, Inspector};
-
-#[derive(Clone, Copy)]
-pub struct NoOpInspector;
-
-impl<DB: Database> Inspector<DB> for NoOpInspector {}
