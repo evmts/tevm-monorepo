6dcae1bca912509407eae6075dda2a3b73b46591
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-09-29 21:09:26 -0700

    :arrow_up: chore: Upgrade all packages

---
+++ .changeset/spicy-phones-impress.md
@@ -0,0 +1,16 @@
+---
+"@evmts/experimental-solc": patch
+"@evmts/bundler": patch
+"@evmts/esbuild-plugin": patch
+"@evmts/webpack-plugin": patch
+"@evmts/rollup-plugin": patch
+"@evmts/rspack-plugin": patch
+"@evmts/vite-plugin": patch
+"@evmts/bun-plugin": patch
+"@evmts/ts-plugin": patch
+"@evmts/config": patch
+"@evmts/ethers": patch
+"@evmts/core": patch
+---
+
+Upgraded all NPM dependencies to latest
--- core/src/event/Event.ts
+++ core/src/event/Event.ts
@@ -5,7 +5,7 @@ import type {
 	ParseAbi,
 } from 'abitype'
 import type { BlockNumber, BlockTag, CreateEventFilterParameters } from 'viem'
-import type { MaybeExtractEventArgsFromAbi } from 'viem/dist/types/types/contract'
+import type { MaybeExtractEventArgsFromAbi } from 'viem/_types/types/contract'
 
 export type ValueOf<T> = T[keyof T]
 
@@ -13,40 +13,40 @@ export type Events<
 	TName extends string,
 	THumanReadableAbi extends readonly string[],
 > = {
-	[TEventName in ExtractAbiEventNames<ParseAbi<THumanReadableAbi>>]: (<
-		TStrict extends boolean = false,
-		TFromBlock extends BlockNumber | BlockTag | undefined = undefined,
-		TToBlock extends BlockNumber | BlockTag | undefined = undefined,
-	>(
-		params: Pick<
-			CreateEventFilterParameters<
-				ExtractAbiEvent<ParseAbi<THumanReadableAbi>, TEventName>,
-				ParseAbi<THumanReadableAbi>,
-				TStrict,
-				TFromBlock,
-				TToBlock,
-				TEventName,
-				MaybeExtractEventArgsFromAbi<ParseAbi<THumanReadableAbi>, TEventName>
+		[TEventName in ExtractAbiEventNames<ParseAbi<THumanReadableAbi>>]: (<
+			TStrict extends boolean = false,
+			TFromBlock extends BlockNumber | BlockTag | undefined = undefined,
+			TToBlock extends BlockNumber | BlockTag | undefined = undefined,
+		>(
+			params: Pick<
+				CreateEventFilterParameters<
+					ExtractAbiEvent<ParseAbi<THumanReadableAbi>, TEventName>,
+					ParseAbi<THumanReadableAbi>,
+					TStrict,
+					TFromBlock,
+					TToBlock,
+					TEventName,
+					MaybeExtractEventArgsFromAbi<ParseAbi<THumanReadableAbi>, TEventName>
+				>,
+				'fromBlock' | 'toBlock' | 'args' | 'strict'
 			>,
-			'fromBlock' | 'toBlock' | 'args' | 'strict'
-		>,
-	) => CreateEventFilterParameters<
-		ExtractAbiEvent<ParseAbi<THumanReadableAbi>, TEventName>,
-		ParseAbi<THumanReadableAbi>,
-		TStrict,
-		TFromBlock,
-		TToBlock,
-		TEventName,
-		MaybeExtractEventArgsFromAbi<ParseAbi<THumanReadableAbi>, TEventName>
-	> & {
-		evmtsContractName: TName
-		eventName: TEventName
-		abi: [ExtractAbiEvent<ParseAbi<THumanReadableAbi>, TEventName>]
-	}) & {
-		eventName: TEventName
-		humanReadableAbi: FormatAbi<
-			[ExtractAbiEvent<ParseAbi<THumanReadableAbi>, TEventName>]
-		>
-		abi: [ExtractAbiEvent<ParseAbi<THumanReadableAbi>, TEventName>]
+		) => CreateEventFilterParameters<
+			ExtractAbiEvent<ParseAbi<THumanReadableAbi>, TEventName>,
+			ParseAbi<THumanReadableAbi>,
+			TStrict,
+			TFromBlock,
+			TToBlock,
+			TEventName,
+			MaybeExtractEventArgsFromAbi<ParseAbi<THumanReadableAbi>, TEventName>
+		> & {
+			evmtsContractName: TName
+			eventName: TEventName
+			abi: [ExtractAbiEvent<ParseAbi<THumanReadableAbi>, TEventName>]
+		}) & {
+			eventName: TEventName
+			humanReadableAbi: FormatAbi<
+				[ExtractAbiEvent<ParseAbi<THumanReadableAbi>, TEventName>]
+			>
+			abi: [ExtractAbiEvent<ParseAbi<THumanReadableAbi>, TEventName>]
+		}
 	}
-}
