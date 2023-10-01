fc817bb774e86a2255c22f35921cb3d6d2273b2f
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-09-30 17:05:51 -0700

    :rotating_light: fix: Run linter

--- core/src/write/Write.ts
+++ core/src/write/Write.ts
@@ -11,29 +11,29 @@ export type Write<
 	TName extends string,
 	THumanReadableAbi extends readonly string[],
 > = {
-		[TFunctionName in
+	[TFunctionName in
 		ExtractAbiFunctionNames<
 			ParseAbi<THumanReadableAbi>,
 			'payable' | 'nonpayable'
 		>]: <
-			TArgs extends AbiParametersToPrimitiveTypes<
-				ExtractAbiFunction<ParseAbi<THumanReadableAbi>, TFunctionName>['inputs']
-			> &
+		TArgs extends AbiParametersToPrimitiveTypes<
+			ExtractAbiFunction<ParseAbi<THumanReadableAbi>, TFunctionName>['inputs']
+		> &
 			any[] = AbiParametersToPrimitiveTypes<
-				ExtractAbiFunction<ParseAbi<THumanReadableAbi>, TFunctionName>['inputs']
-			> &
+			ExtractAbiFunction<ParseAbi<THumanReadableAbi>, TFunctionName>['inputs']
+		> &
 			any[],
-		>(
-			...args: TArgs
-		) => TArgs['length'] extends 0
-			? {
+	>(
+		...args: TArgs
+	) => TArgs['length'] extends 0
+		? {
 				functionName: TFunctionName
 				humanReadableAbi: FormatAbi<
 					[ExtractAbiFunction<ParseAbi<THumanReadableAbi>, TFunctionName>]
 				>
 				abi: [ExtractAbiFunction<ParseAbi<THumanReadableAbi>, TFunctionName>]
-			}
-			: {
+		  }
+		: {
 				evmtsContractName: TName
 				functionName: TFunctionName
 				args: TArgs
@@ -41,5 +41,5 @@ export type Write<
 					[ExtractAbiFunction<ParseAbi<THumanReadableAbi>, TFunctionName>]
 				>
 				abi: [ExtractAbiFunction<ParseAbi<THumanReadableAbi>, TFunctionName>]
-			}
-	}
+		  }
+}
--- examples/esbuild/src/constants.ts
+++ examples/esbuild/src/constants.ts
@@ -2,5 +2,10 @@ import { mainnet } from 'viem/chains'
 import { z } from 'zod'
 
 export const rpcUrls = {
-	1: z.string().url().optional().default(mainnet.rpcUrls.public.http[0]).parse(process.env.RPC_URL_MAINNET),
+	1: z
+		.string()
+		.url()
+		.optional()
+		.default(mainnet.rpcUrls.public.http[0])
+		.parse(process.env.RPC_URL_MAINNET),
 }
