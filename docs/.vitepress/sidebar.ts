8f7b167997f05004504d0bd52fa23b623e86d0b0
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-10-08 11:34:23 -0700

    :memo: Create sidebar
    s

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -3,109 +3,111 @@ import type { DefaultTheme } from 'vitepress'
 export const sidebar: DefaultTheme.Sidebar = [
 	{ text: 'Home', link: '/' },
 	{
-		text: 'Getting Started',
-		items: [
-			{ text: 'Introduction', link: '/gettingstarted/introduction' },
-			{ text: 'Overview', link: '/gettingstarted/overview' },
-			{ text: 'Quick Start', link: '/gettingstarted/quickstart' },
-		],
-	},
-	{
-		text: 'Tutorial',
-		items: [
-			{ text: 'Overview', link: '/tutorial/overview' },
-			{ text: 'Setup', link: '/tutorial/setup' },
-			{
-				text: 'Configuration',
-				link: '/tutorial/configuration',
-			},
-			{
-				text: 'Hello World',
-				link: '/tutorial/hello-world',
-			},
-		],
-	},
-	{
-		text: 'Wagmi Usage',
-		items: [
-			{ text: 'Overview', link: '/wagmi/overview' },
-			{ text: 'useContractRead', link: '/wagmi/use-contract-read' },
-			{ text: 'useContractWrite', link: '/wagmi/use-contract-write' },
-		],
-	},
-	{
-		text: 'Viem Usage',
-		items: [
-			{ text: 'Overview', link: '/viem/overview' },
-			// 			{ text: 'call', link: '/viem/call' },
-			// 			{ text: 'sendTransaction', link: '/viem/send-transaction' },
-			// 			{ text: 'createEventFilter', link: '/viem/create-event-filter' },
-		],
-	},
-	{
-		text: 'Ethers.js Usage',
-		items: [{ text: 'Overview', link: '/ethers/overview' }],
-	},
-	{
-		text: 'web3.js Usage',
-		items: [{ text: 'Overview', link: '/web3js/overview' }],
-	},
-	{
-		text: 'Bundler Configuration',
+		text: '🚧 Learn EVMts',
 		items: [
 			{
-				text: 'ESBuild',
-				link: '/guides/esbuild',
+				text: '🚧 Getting Started', items: [
+					{ text: '🚧 Quick start', link: '/learn/gettingstarted/quickstart' },
+					{ text: '🚧 Why EVMts', link: '/learn/gettingstarted/thinking' },
+					{ text: '🚧 Tutorial: balanceOf', link: '/learn/gettingstarted/tutorial1' },
+					{ text: '🚧 Tutorial: optimistic counter', link: '/learn/gettingstarted/tutorial2' },
+				]
 			},
 			{
-				text: 'NEXT.js',
-				link: '/guides/next',
+				text: '🚧 Installation', items: [
+					{ text: '🚧 Start a new EVMts project', link: '/learn/installation/starting' },
+					{ text: '🚧 Add EVMts to an existing project', link: '/learn/installation/existing' },
+					{ text: '🚧 Editor setup', link: '/learn/installation/editor' },
+					{ text: '🚧 Bundler setup', link: '/learn/installation/bundler' },
+					{ text: '🚧 Developer tool', link: '/learn/installation/developer' },
+				]
 			},
 			{
-				text: 'Rollup',
-				link: '/guides/rollup',
+				text: '🚧 Everything is an Action', items: [
+					{ text: '🚧 Actions', link: '/learn/installation/starting' },
+					{ text: '🚧 ActionCreators', link: '/learn/installation/existing' },
+					{ text: '🚧 ActionHandler', link: '/learn/installation/editor' },
+					{ text: '🚧 ActionListener', link: '/learn/installation/bundler' },
+				]
 			},
 			{
-				text: 'Rspack',
-				link: '/guides/rspack',
-			},
-			{
-				text: 'Bun',
-				link: '/configuration/bun',
-			},
-			{
-				text: 'Vite',
-				link: '/guides/Vite',
-			},
-			{
-				text: 'Webpack',
-				link: '/guides/Webpack',
+				text: '🚧 Working with EVMts', items: [
+					{ text: '🚧 Working with Listeners', link: '/learn/installation/starting' },
+					{ text: '🚧 Optimistic updates', link: '/learn/installation/starting' },
+					{ text: '🚧 Solidity scripting', link: '/learn/installation/existing' },
+					{ text: '🚧 Debugging', link: '/learn/installation/editor' },
+					{ text: '🚧 Testing', link: '/learn/installation/bundler' },
+				]
 			},
 		],
 	},
 	{
-		text: 'TypeScript Configuration',
+		text: '🚧 API Reference',
 		items: [
 			{
-				text: 'TypeScript',
-				link: '/guides/typescript',
+				text: '🚧 Core types', items: [
+					{ text: '🚧 Action', link: '/learn/gettingstarted/quickstart' },
+					{ text: '🚧 ActionCreator', link: '/learn/gettingstarted/thinking' },
+					{ text: '🚧 ActionHandler', link: '/learn/gettingstarted/tutorial1' },
+					{ text: '🚧 ActionListener', link: '/learn/gettingstarted/tutorial2' },
+					{ text: '🚧 AddressBook', link: '/learn/gettingstarted/tutorial2' },
+					{ text: '🚧 JsonRpc and JsonWs', link: '/learn/gettingstarted/tutorial2' },
+					{ text: '🚧 Wallet', link: '/learn/gettingstarted/tutorial2' },
+					{ text: '🚧 Vm', link: '/learn/gettingstarted/tutorial2' },
+					{ text: '🚧 BlockExplorer', link: '/learn/gettingstarted/tutorial2' },
+				]
 			},
 			{
-				text: 'VSCode',
-				link: '/guides/vscode',
+				text: '🚧 Contract actions', items: [
+					{ text: '🚧 VM Actions', link: '/learn/installation/starting' },
+					{ text: '🚧 JsonRpc Actions', link: '/learn/installation/existing' },
+					{ text: '🚧 WalletRpc Actions', link: '/learn/installation/editor' },
+					{ text: '🚧 Encoding/Decoding Actions', link: '/learn/installation/bundler' },
+					{ text: '🚧 Multicall3 Actions', link: '/learn/installation/developer' },
+					{ text: '🚧 Batching Actions', link: '/learn/installation/developer' },
+					{ text: '🚧 OP Stack Actions', link: '/learn/installation/developer' },
+					{ text: '🚧 Smart-contract wallet Actions', link: '/learn/installation/developer' },
+					{ text: '🚧 Ens Actions', link: '/learn/installation/developer' },
+				]
 			},
-		],
-	},
-	{
-		text: 'Reference',
-		items: [
 			{
-				text: 'CLI',
-				link: '/reference/cli',
+				text: '🚧 Build reference', items: [
+					{ text: '🚧 Config reference', link: '/learn/installation/starting' },
+					{ text: '🚧 TypeScript Plugin', link: '/learn/installation/existing' },
+					{
+						text: '🚧 Bundlers',
+						items: [
+							{ text: '🚧 Webpack', link: '/learn/installation/starting' },
+							{ text: '🚧 Vite', link: '/learn/installation/starting' },
+							{ text: '🚧 Rollup', link: '/learn/installation/starting' },
+							{ text: '🚧 ESBuild', link: '/learn/installation/starting' },
+							{ text: '🚧 Babel', link: '/learn/installation/starting' },
+							{ text: '🚧 Other build systems', link: '/learn/installation/starting' },
+						]
+					},
+				]
 			},
 			{
-				text: 'Config',
-				link: '/reference/config',
+				text: '🚧 CLI Reference', items: [
+					{ text: '🚧 Create', link: '/learn/installation/starting' },
+					{ text: '🚧 Initialize', link: '/learn/installation/starting' },
+					{ text: '🚧 Generate', link: '/learn/installation/existing' },
+				]
+			},
+			{
+				text: '🚧 Usage with other Ethereum libraries', items: [
+					{ text: '🚧 Ethers', link: '/learn/installation/starting' },
+					{ text: '🚧 Viem', link: '/learn/installation/starting' },
+					{ text: '🚧 Hardhat', link: '/learn/installation/starting' },
+					{ text: '🚧 Foundry', link: '/learn/installation/starting' },
+					{ text: '🚧 MUD', link: '/learn/installation/starting' },
+				]
+			},
+			{
+				text: '🚧 Advanced', items: [
+					{ text: '🚧 Building your own custom Actions', link: '/learn/installation/starting' },
+					{ text: '🚧 Contributing to EVMts', link: '/learn/installation/starting' },
+				]
 			},
 		],
 	},
