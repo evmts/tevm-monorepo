cafff545db061ce11dbf491333609a5781349800
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-10-08 15:33:19 -0700

    :technologist: simplify reference docs

--- docs/reference/sidebar.ts
+++ docs/reference/sidebar.ts
@@ -1,20 +1,17 @@
 import type { DefaultTheme } from 'vitepress'
 import { homeSidebar } from '../home/sidebar'
+import { } from './actions/sidebar'
+import { } from './cli/sidebar'
+import { } from './coretypes/sidebar'
+import { } from './localforks/sidebar'
+import { } from './react/sidebar'
+import { } from './utils/sidebar'
 
 export const apiReferenceSidebar: DefaultTheme.Sidebar = [
   homeSidebar,
   {
     text: '🚧 API Reference',
     items: [
-      actionsReferenceSidebar,
-      jsonRpcActionsReferenceSidebar,
-      advancedReferenceSidebar,
-      buildReferenceSidebar,
-      cliReferenceSidebar,
-      creatorsReferenceSidebar,
-      handlersReferenceSidebar,
-      localForksReferenceSidebar,
-      usageWithOtherLibrariesReferenceSidebar,
     ],
   },
 ]
