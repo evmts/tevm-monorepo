26cb22c9640cb40b496a0e29ac37ebfcbef0d277
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-10-14 19:31:04 -0700

    :construction: feat: get the script working

--- viem-effect/generateSrc.ts
+++ viem-effect/generateSrc.ts
@@ -10,14 +10,16 @@ import * as path from 'path';
 import { parse, transformFromAstSync } from '@babel/core';
 import traverse from '@babel/traverse';
 import template from '@babel/template';
-
-const UTILS_DIR = path.join(__dirname, '../../src/utils');
+const UTILS_DIR = path.join(__dirname, './src-viem/utils');
+const OUTPUT_BASE_DIR = path.join(__dirname, './src');
 
 const mainFiles = glob.sync(path.join(UTILS_DIR, '**/*(!(.test|.bench)).ts'), {
-  ignore: [path.join(UTILS_DIR, 'unit/**')]
+  ignore: [path.join(UTILS_DIR, 'unit/**'), path.join(UTILS_DIR, 'errors/**')] // Added the errors directory to ignore list
 });
 
-const testFiles = glob.sync(path.join(UTILS_DIR, '**/*.test.ts'));
+const testFiles = glob.sync(path.join(UTILS_DIR, '**/*.test.ts'), {
+  ignore: [path.join(UTILS_DIR, 'errors/**')]  // Added the errors directory to ignore list
+});
 
 const asyncFunctions = ['placeholderAsyncFunction'];
 
@@ -28,36 +30,46 @@ async function processFiles() {
 
   for (const file of mainFiles) {
     const baseName = path.basename(file, '.ts');
-    const fileDir = path.dirname(file);
+    const relativePath = path.relative(UTILS_DIR, path.dirname(file));
+    const outputPath = path.join(OUTPUT_BASE_DIR, relativePath);
+
+    // Make the directory if it doesn't exist
+    if (!fs.existsSync(outputPath)) {
+      fs.mkdirSync(outputPath, { recursive: true });
+    }
 
     if (viemModule[baseName] === undefined) {
       skippedFiles.push(baseName);
       continue;
     }
 
-    const newFileName = path.join(fileDir, `${baseName}Effect.js`);
-    fs.renameSync(file, newFileName);
+    const newFileName = path.join(outputPath, `${baseName}Effect.js`);
 
-    const content = generateContent(baseName, fileDir);
+    const content = generateContent(baseName, outputPath);
     fs.writeFileSync(newFileName, content);
   }
 
   for (const file of testFiles) {
     const baseName = path.basename(file, '.test.ts');
+    const relativePath = path.relative(UTILS_DIR, path.dirname(file));
+    const outputPath = path.join(OUTPUT_BASE_DIR, relativePath);
+
+    // Make the directory if it doesn't exist
+    if (!fs.existsSync(outputPath)) {
+      fs.mkdirSync(outputPath, { recursive: true });
+    }
 
     if (skippedFiles.includes(baseName)) {
       continue;
     }
 
-    const newTestFileName = path.join(path.dirname(file), `${baseName}Effect.test.ts`);
-    fs.renameSync(file, newTestFileName);
+    const newTestFileName = path.join(outputPath, `${baseName}Effect.test.ts`);
 
-    let testContent = fs.readFileSync(newTestFileName, 'utf-8');
+    let testContent = fs.readFileSync(file, 'utf-8');
     testContent = transformTestFile(testContent, baseName);
     fs.writeFileSync(newTestFileName, testContent);
   }
 }
-
 function generateContent(baseName: string, fileDir: string): string {
   const capitalizedErrorType = capitalizeFirstLetter(baseName) + 'ErrorType';
   const relativePath = getRelativePathToWrapViemSync(fileDir);
@@ -75,7 +87,9 @@ export const Effect = ${wrapFunction}(${baseName});
 }
 
 function transformTestFile(content: string, baseName: string): string {
+  const filename = `${baseName}.ts`;
   const ast = parse(content, {
+    filename,
     presets: ['@babel/preset-typescript'],
     sourceType: 'module'
   });
@@ -91,12 +105,21 @@ function transformTestFile(content: string, baseName: string): string {
         const wrappedCall = template.expression.ast`Effect.runSync(${baseName}Effect())`;
         path.replaceWith(wrappedCall);
       }
+    },
+    ImportDeclaration(path) {
+      // This handles import specifiers to update the imported module
+      for (const specifier of path.node.specifiers) {
+        if (specifier.type === 'ImportSpecifier' || specifier.type === 'ImportDefaultSpecifier') {
+          if (specifier.local.name === baseName) {
+            path.node.source.value = path.node.source.value.replace(baseName, `${baseName}Effect`);
+          }
+        }
+      }
     }
   });
 
-  return transformFromAstSync(ast!)!.code!;
+  return transformFromAstSync(ast!, undefined, { filename })!.code!;
 }
-
 function getRelativePathToWrapViemSync(fileDir: string): string {
   const depth = fileDir.replace(UTILS_DIR, '').split(path.sep).filter(part => part).length;
   return '../'.repeat(depth) + 'wrapViemSync.js';
