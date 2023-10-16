ecb9591c967a0e3decff65f6fa18a01ca162100e
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-10-15 18:09:38 -0700

    :bug: fix(bundler): Don't export types file

--- bundlers/bundler/src/index.js
+++ bundlers/bundler/src/index.js
@@ -1,6 +1,16 @@
+/**
+ * @typedef {import('./types.js').AsyncBundlerResult} AsyncBundlerResult
+ * @typedef {import('./types.js').Bundler} Bundler
+ * @typedef {import('./types.js').BundlerResult} BundlerResult
+ * @typedef {import('./types.js').CompiledContracts} CompiledContracts
+ * @typedef {import('./types.js').FileAccessObject} FileAccessObject
+ * @typedef {import('./types.js').Logger} Logger
+ * @typedef {import('./types.js').ModuleInfo} ModuleInfo
+ * @typedef {import('./types.js').SolidityResolver} SolidityResolver
+ * @typedef {import('./types.js').SyncBundlerResult} SyncBundlerResult
+ */
 export * from './solc/index.js'
 export * from './unplugin.js'
-export * from './types.js'
 export * from './bundler.js'
 export * from './runtime/index.js'
 export * from './utils/index.js'
