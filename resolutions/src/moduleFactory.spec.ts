2543617043ad3a09d4ab2c684df5d89404754275
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-10-28 14:45:43 -0700

    :white_check_mark: test: Make tests pass

--- resolutions/src/moduleFactory.spec.ts
+++ resolutions/src/moduleFactory.spec.ts
@@ -1,238 +1,71 @@
 import { moduleFactory } from './moduleFactory.js'
-import type { FileAccessObject, ModuleInfo } from './types.js'
+import type { FileAccessObject } from './types.js'
 import { runPromise, runSync } from 'effect/Effect'
+import { existsSync, readFileSync } from 'fs'
+import { readFile } from 'fs/promises'
+import { join } from 'path'
 import {
-	type Mock,
-	afterEach,
-	beforeEach,
 	describe,
 	expect,
 	it,
-	vi,
 } from 'vitest'
 
 const fao: FileAccessObject = {
-	existsSync: vi.fn(),
-	readFile: vi.fn(),
-	readFileSync: vi.fn(),
+	existsSync: existsSync,
+	readFile: readFile,
+	readFileSync: readFileSync,
 }
 
-vi.mock('fs', () => ({
-	readFileSync: vi.fn(),
-	readFile: vi.fn(),
-}))
-const mockReadFileSync = fao.readFileSync as Mock
-const mockReadFile = fao.readFile as Mock
+class Fixture {
+	constructor(
+		public readonly name: string
+	) { }
+	dir = () => join(__dirname, 'fixtures', this.name)
+	entrypoint = () => join(this.dir(), 'Contract.sol')
+	rawCode = () => readFileSync(this.entrypoint(), 'utf8')
+	remappings = () => {
+		if (existsSync(join(this.dir(), 'remappings.json'))) {
+			return JSON.parse(readFileSync(join(this.dir(), 'remappings.json'), 'utf8'))
+		}
+		return {}
+	}
+	libs = () => {
+		if (existsSync(join(this.dir(), 'lib'))) {
+			return [join(this.dir(), 'lib')]
+		}
+		return []
+	}
+}
+const fixtures = {
+	basic: new Fixture('basic'),
+	withlib: new Fixture('withlib'),
+	withremappings: new Fixture('withremappings')
+}
 
 describe('moduleFactory', () => {
-	const remappings = {
-		'key1/': '/path/to/key1',
-		'key2/': '/path/to/key2',
-	} as const
-
-	const testModuleCode = `import "key1/somefile"
-import "./localfile"
-import "./anotherLocalFile"
-import "othermodule"
-import "otherOthermodule"`
-
-	const absolutePath = '/project/src/testModule.sol'
-
-	const localfileMockContent = 'contract LocalFile {}'
-	const key1MockContent = 'contract Key1 {}'
-	const othermoduleMockContent = 'contract OtherModule {}'
-
-	let testModule: ModuleInfo
-
-	beforeEach(async () => {
-		mockReadFile
-			.mockReturnValueOnce(localfileMockContent)
-			.mockReturnValueOnce(key1MockContent)
-			.mockReturnValueOnce(othermoduleMockContent)
-
-		const modules = await runPromise(
-			moduleFactory(
-				absolutePath,
-				testModuleCode,
-				remappings,
-				['../node_modules'],
-				fao,
-				false,
-			),
-		)
-		testModule = modules.get(absolutePath) as ModuleInfo
-	})
-
-	it.only('should correctly resolve import paths', () => {
-		expect(testModule.importedIds).toMatchInlineSnapshot(`
-			[
-			  "/path/to/key1somefile",
-			  "/project/src/localfile",
-			  "/project/src/anotherLocalFile",
-			  "othermodule",
-			  "otherOthermodule",
-			]
-		`)
-	})
-
-	it('should correctly replace import statements in code', () => {
-		expect(testModule.code).toMatchInlineSnapshot(`
-			"import \\"/path/to/key1somefile\\"
-			import \\"/project/src/localfile\\"
-			import \\"/project/src/anotherLocalFile\\"
-			import \\"othermodule\\"
-			import \\"otherOthermodule\\""
-		`)
-	})
-
-	it('should correctly resolve module dependencies', () => {
-		expect(testModule.resolutions.map((r) => r.id)).toMatchInlineSnapshot(`
-			[
-			  "/path/to/key1somefile",
-			  "/project/src/localfile",
-			  "/project/src/anotherLocalFile",
-			  "othermodule",
-			  "otherOthermodule",
-			]
-		`)
-	})
-
-	it('should not replace import statements if resolveImportPath returns the original import', async () => {
-		// This import path does not start with a remapping key, is not local, and cannot be resolved by Node
-		const testModuleCodeUnresolvedImport = `import "unresolved/import"`
-		const unresolvedImportMockContent = 'contract UnresolvedImport {}'
-
-		mockReadFile
-			.mockReturnValueOnce(unresolvedImportMockContent)
-			.mockReturnValueOnce(key1MockContent)
-			.mockReturnValueOnce(othermoduleMockContent)
-
-		const testModuleUnresolvedImport = (
-			await runPromise(
-				moduleFactory(
-					absolutePath,
-					testModuleCodeUnresolvedImport,
-					remappings,
-					['../node_modules'],
-					fao,
-					false,
-				),
-			)
-		).get(absolutePath) as ModuleInfo
-
-		// Update the expected snapshot to reflect the change.
-		// The snapshot string might need to be adjusted based on the actual output of your function.
-		expect(testModuleUnresolvedImport.code).toMatchInlineSnapshot(`
-    "import \\"unresolved/import\\""
-  `)
-	})
-
-	afterEach(() => {
-		vi.clearAllMocks()
-	})
-})
-
-describe('moduleFactory (sync)', () => {
-	const remappings = {
-		'key1/': '/path/to/key1',
-		'key2/': '/path/to/key2',
-	} as const
-
-	const testModuleCode = `import "key1/somefile"
-import "./localfile"
-import "./anotherLocalFile"
-import "othermodule"
-import "otherOthermodule"`
-
-	const absolutePath = '/project/src/testModule.sol'
-
-	const localfileMockContent = 'import "othermodule"\ncontract LocalFile {}'
-	const key1MockContent = 'contract Key1 {}'
-	const othermoduleMockContent = 'contract OtherModule {}'
-
-	let testModule: ModuleInfo
-
-	beforeEach(() => {
-		mockReadFileSync
-			.mockReturnValueOnce(localfileMockContent)
-			.mockReturnValueOnce(key1MockContent)
-			.mockReturnValueOnce(othermoduleMockContent)
-
-		testModule = runSync(
-			moduleFactory(
-				absolutePath,
-				testModuleCode,
-				remappings,
-				['../node_modules'],
-				fao,
-				true,
-			),
-		).get(absolutePath) as ModuleInfo
-	})
-
-	it('should correctly resolve import paths', () => {
-		expect(testModule.importedIds).toMatchInlineSnapshot(`
-			[
-			  "/path/to/key1somefile",
-			  "/project/src/localfile",
-			  "/project/src/anotherLocalFile",
-			  "othermodule",
-			  "otherOthermodule",
-			]
-		`)
-	})
-
-	it('should correctly replace import statements in code', () => {
-		expect(testModule.code).toMatchInlineSnapshot(`
-			"import \\"/path/to/key1somefile\\"
-			import \\"/project/src/localfile\\"
-			import \\"/project/src/anotherLocalFile\\"
-			import \\"othermodule\\"
-			import \\"otherOthermodule\\""
-		`)
-	})
-
-	it('should correctly resolve module dependencies', () => {
-		expect(testModule.resolutions.map((r) => r.id)).toMatchInlineSnapshot(`
-			[
-			  "/path/to/key1somefile",
-			  "/project/src/localfile",
-			  "/project/src/anotherLocalFile",
-			  "othermodule",
-			  "otherOthermodule",
-			]
-		`)
-	})
-
-	it('should not replace import statements if resolveImportPath returns the original import', () => {
-		// This import path does not start with a remapping key, is not local, and cannot be resolved by Node
-		const testModuleCodeUnresolvedImport = `import "unresolved/import"`
-		const unresolvedImportMockContent = 'contract UnresolvedImport {}'
-
-		mockReadFileSync
-			.mockReturnValueOnce(unresolvedImportMockContent)
-			.mockReturnValueOnce(key1MockContent)
-			.mockReturnValueOnce(othermoduleMockContent)
-
-		const testModuleUnresolvedImport = runSync(
-			moduleFactory(
-				absolutePath,
-				testModuleCodeUnresolvedImport,
-				remappings,
-				['../node_modules'],
-				fao,
-				true,
-			),
-		).get(absolutePath) as ModuleInfo
-
-		// Update the expected snapshot to reflect the change.
-		// The snapshot string might need to be adjusted based on the actual output of your function.
-		expect(testModuleUnresolvedImport.code).toMatchInlineSnapshot(`
-    "import \\"unresolved/import\\""
-  `)
-	})
-
-	afterEach(() => {
-		vi.clearAllMocks()
+	const cases: Array<Array<keyof typeof fixtures>> = [['basic'], ['withlib'], ['withremappings']]
+	it.each(cases)(`should resolve correctly for case $s`, async (testCase: keyof typeof fixtures) => {
+		let runSyncronously = true
+		let modules = runSync(moduleFactory(
+			fixtures[testCase].entrypoint(),
+			fixtures[testCase].rawCode(),
+			fixtures[testCase].remappings(),
+			fixtures[testCase].libs(),
+			fao,
+			runSyncronously,
+		))
+		expect(modules.keys()).toMatchSnapshot()
+		expect(modules.get(fixtures[testCase].entrypoint())).toMatchSnapshot()
+		runSyncronously = false
+		modules = await runPromise(moduleFactory(
+			fixtures[testCase].entrypoint(),
+			fixtures[testCase].rawCode(),
+			fixtures[testCase].remappings(),
+			fixtures[testCase].libs(),
+			fao,
+			runSyncronously,
+		))
+		expect(modules.keys()).toMatchSnapshot()
+		expect(modules.get(fixtures[testCase].entrypoint())).toMatchSnapshot()
 	})
 })
