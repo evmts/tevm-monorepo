d6976aa2511ec0be92042879f79d7512523cdc9d
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-11-05 16:40:06 -0800

    :package: Feat: Add @evmts/package package

---
+++ packages/index.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+	...require('@evmts/blockexplorer'),
+	...require('@evmts/core'),
+	...require('@evmts/effect'),,
+	...require('@evmts/vm'),,
+	...require('@evmts/revm'),,
+	...require('@evmts/schemas'),
+	...require('@evmts/state'),
+}
---
+++ packages/index.mjs
@@ -0,0 +1,7 @@
+export * from '@evmts/blockexplorer'
+export * from '@evmts/core'
+export * from '@evmts/effect'
+export * from '@evmts/vm'
+export * from '@evmts/revm'
+export * from '@evmts/schemas'
+export * from '@evmts/state'
