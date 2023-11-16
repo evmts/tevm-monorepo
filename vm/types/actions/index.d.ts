95fb90508ecc19c4df08f60201037ea35e07c474
Will Cory <willcory10@gmail.com>
2023-11-15 19:35:00 -0800

    :bug: Fix: remove committed types from vm (#665)
    ## Description
    
    Not sure how this got committed its in git ignore
    
    ## Testing
    
    Explain the quality checks that have been done on the code changes
    
    ## Additional Information
    
    - [ ] I read the [contributing docs](../docs/contributing.md) (if this
    is your first contribution)
    
    Your ENS/address:
    
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

--- vm/types/actions/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./putAccount.js";
-export * from "./putContractCode.js";
-export * from "./runCall.js";
-export * from "./runContractCall.js";
-export * from "./runScript.js";
-//# sourceMappingURL=index.d.ts.map
