ca9a433ae71a31303408e6dd80a4ff8a88b4c8e1
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-11-15 21:57:34 -0800

    :bug: Fix(core): Fix type exports of EvmtsContract

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,5 @@
-export * from './EvmtsContract'
-export * from './evmtsContractFactory'
+export type { EvmtsContract } from './EvmtsContract'
+export { evmtsContractFactory } from './evmtsContractFactory'
 // reexported because they are needed to be able to
 // instanciate an EvmtsContract with a json abi
 export { formatAbi, parseAbi } from 'abitype'
