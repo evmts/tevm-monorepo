38bbd1c2a49d446e678cc76287ef9b35eca8591c
Will Cory <willcory10@gmail.com>
2023-11-25 15:20:38 -0800

    :package: Chore: Add boilerplate for volar server and vscode extension (#730)
    ## Description
    
    Copy in starter projects for volar
    
    ## Testing
    
    Explain the quality checks that have been done on the code changes
    
    ## Additional Information
    
    - [ ] I read the [contributing docs](../docs/contributing.md) (if this
    is your first contribution)
    
    Your ENS/address:
    
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

---
+++ lsp/vscode/scripts/build.js
@@ -0,0 +1,41 @@
+require("esbuild").context({
+	entryPoints: {
+		client: './src/extension.ts',
+		server: '../language-server/src/index.ts',
+	},
+	sourcemap: true,
+	bundle: true,
+	metafile: process.argv.includes('--metafile'),
+	outdir: './dist',
+	external: [
+		'vscode',
+	],
+	format: 'cjs',
+	platform: 'node',
+	tsconfig: './tsconfig.json',
+	define: { 'process.env.NODE_ENV': '"production"' },
+	minify: process.argv.includes('--minify'),
+	plugins: [
+		{
+			name: 'umd2esm',
+			setup(build) {
+				build.onResolve({ filter: /^(vscode-.*-languageservice|jsonc-parser)/ }, args => {
+					const pathUmdMay = require.resolve(args.path, { paths: [args.resolveDir] });
+					// Call twice the replace is to solve the problem of the path in Windows
+					const pathEsm = pathUmdMay.replace('/umd/', '/esm/').replace('\\umd\\', '\\esm\\');
+					return { path: pathEsm };
+				});
+			},
+		},
+	],
+}).then(async ctx => {
+	console.log('building...');
+	if (process.argv.includes('--watch')) {
+		await ctx.watch();
+		console.log('watching...');
+	} else {
+		await ctx.rebuild();
+		await ctx.dispose();
+		console.log('finished.');
+	}
+});
