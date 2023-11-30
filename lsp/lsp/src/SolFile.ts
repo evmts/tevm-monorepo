d0ca4bc94ea14e1935e36a7c66b6ea55c125efaa
Will Cory <willcory10@gmail.com>
2023-11-29 21:54:23 -0800

    :construction: Feat: Implement some of volar evmts (#732)
    ## Description
    
    Implement the very very basic functionality for the EVMts volar
    implementation. Still a WIP
    
    ## Testing
    
    WIP none atm
    
    ## Additional Information
    
    - [x] I read the [contributing docs](../docs/contributing.md) (if this
    is your first contribution)
    
    Your ENS/address:
    
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

---
+++ lsp/lsp/src/SolFile.ts
@@ -0,0 +1,74 @@
+import {
+	FileCapabilities,
+	FileKind,
+	type VirtualFile,
+} from '@volar/language-core'
+import type ts from 'typescript/lib/tsserverlibrary.js'
+import { bundler, createCache } from '@evmts/base'
+import { loadConfig } from '@evmts/config'
+// @ts-expect-error
+import solc from 'solc'
+import { runSync } from 'effect/Effect'
+import { existsSync, readFileSync } from 'fs'
+import { readFile } from 'fs/promises'
+
+export class SolFile implements VirtualFile {
+	// regular file not typescript host file
+	kind: VirtualFile['kind'] = FileKind.TextFile
+	// diagnostic: true
+	// foldingRange: true
+	// documentFormatting: true
+	// documentSymbol: true
+	// codeAction: true
+	// inlayHint: true
+	capabilities: VirtualFile['capabilities'] = FileCapabilities.full
+	// This seems to be source maps but I can't find anyone using htis
+	codegenStacks: VirtualFile['codegenStacks'] = []
+
+	mappings: VirtualFile['mappings'] = []
+	embeddedFiles: VirtualFile['embeddedFiles'] = []
+
+	constructor(
+		public readonly fileName: VirtualFile['fileName'],
+		public snapshot: ts.IScriptSnapshot,
+	) {
+		this.update(snapshot)
+	}
+
+	public update(newSnapshot: ts.IScriptSnapshot) {
+		this.snapshot = newSnapshot
+		const c = runSync(loadConfig(process.cwd()))
+		const cache = createCache(console)
+		const b = bundler(c, console, {
+			existsSync: existsSync,
+			readFile: readFile,
+			readFileSync: readFileSync,
+		}, solc, cache)
+		const tsFile = b.resolveTsModuleSync(this.fileName, process.cwd(), false, false)
+		this.embeddedFiles = [{
+			fileName: this.fileName + '.ts',
+			snapshot: {
+				getText(start, end) {
+					return tsFile.code.substring(start, end);
+				},
+				getLength() {
+					return tsFile.code.length;
+				},
+				getChangeRange() {
+					return undefined;
+				},
+			},
+			kind: FileKind.TypeScriptHostFile,
+			capabilities: {
+				...FileCapabilities.full,
+				foldingRange: false,
+				documentSymbol: false,
+				documentFormatting: false,
+			},
+			// TODO generate source mappings https://github.com/evmts/evmts-monorepo/issues/731
+			mappings: [],
+			embeddedFiles: [],
+			codegenStacks: []
+		}]
+	}
+}
