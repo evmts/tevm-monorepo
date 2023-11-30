ebd385aac1920c0c2794582eefca34c814f6808e
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-11-29 20:47:05 -0800

    :construction: Feat: Implement some of volar evmts

---
+++ lsp/lsp/src/plugin.ts
@@ -0,0 +1,44 @@
+import { language } from './language.js'
+import {
+	type LanguageServerPlugin,
+} from '@volar/language-server/node.js'
+import { ScriptKind } from 'typescript/lib/tsserverlibrary.js'
+import { create as createTsService } from 'volar-service-typescript';
+
+export const plugin: LanguageServerPlugin = () => {
+	return {
+		extraFileExtensions: [
+			{
+				extension: 'sol',
+				isMixedContent: false,
+				scriptKind: ScriptKind.Deferred,
+			},
+		],
+		watchFileExtensions: [
+			'sol',
+			'js',
+			'ts',
+			'tsx',
+			'jsx',
+			'json',
+		],
+		resolveConfig(config) {
+			// languages
+			config.languages ??= {}
+			config.languages['sol'] ??= language
+
+			// services
+			config.services ??= {}
+			config.services['sol'] ??= (context) => ({
+
+				provideDiagnostics(document) {
+					console.log(document, context)
+					return []
+				},
+			})
+			config.services['typescript'] ??= createTsService()
+
+			return config
+		},
+	}
+}
