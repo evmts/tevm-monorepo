0145389f16634b3594e8e6a0eda082662d79417f
Will Cory <willcory10@gmail.com>
2023-12-20 00:53:19 -0800

    :memo: Feat: Add new homepage to docs CHA-98 (#707)
    ## Description
    
    Add new homepage for EVMts beta
    
    ## Testing
    
    Explain the quality checks that have been done on the code changes
    
    ## Additional Information
    
    - [ ] I read the [contributing docs](../docs/contributing.md) (if this
    is your first contribution)
    
    Your ENS/address:
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Integrated analytics into the documentation site using a new Vue
    component.
    	- Updated the website layout to include new analytics integration.
    	- Implemented Tailwind CSS for styling the documentation site.
    
    - **Documentation**
    - Modified the documentation generation workflow to include additional
    checks.
    
    - **Refactor**
    - Altered the export pattern in theme configuration to support named
    exports.
    
    - **Chores**
    - Configured PostCSS with Tailwind CSS and Autoprefixer for better
    browser compatibility.
    
    - **Style**
    - Added new styles and configurations for Tailwind CSS to enhance the
    visual design of the site.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

---
+++ apps/docs/tailwind.config.cjs
@@ -0,0 +1,4 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./.vitepress/**/*.{js,ts,vue}', './**/*.md'],
+}
--- vm/vm/src/viem/types.ts
+++ vm/vm/src/viem/types.ts
@@ -1,37 +1,46 @@
-import type { Abi } from 'abitype'
-import type { NonVerboseTevmJsonRpcRequest } from '../jsonrpc/TevmJsonRpcRequest.js'
-import type { BackendReturnType } from '../jsonrpc/createJsonRpcClient.js'
+import type { RunContractCallAction } from '../actions/contractCall/RunContractCallAction.js'
+import type { RunContractCallResult } from '../actions/contractCall/RunContractCallResult.js'
+import type {
+	PutAccountAction,
+	PutContractCodeAction,
+	RunCallAction,
+} from '../actions/index.js'
 import type { RunScriptAction } from '../actions/runScript/RunScriptAction.js'
 import type { RunScriptResult } from '../actions/runScript/RunScriptResult.js'
-import type { PutAccountAction, PutContractCodeAction, RunCallAction } from '../actions/index.js'
+import type { NonVerboseTevmJsonRpcRequest } from '../jsonrpc/TevmJsonRpcRequest.js'
+import type { BackendReturnType } from '../jsonrpc/createJsonRpcClient.js'
 import type { TevmPutAccountResponse } from '../jsonrpc/putAccount/TevmPutAccountResponse.js'
 import type { TevmPutContractCodeResponse } from '../jsonrpc/putContractCode/TevmPutContractCodeResponse.js'
 import type { TevmCallResponse } from '../jsonrpc/runCall/TevmCallResponse.js'
-import type { RunContractCallAction } from '../actions/contractCall/RunContractCallAction.js'
-import type { RunContractCallResult } from '../actions/contractCall/RunContractCallResult.js'
+import type { Abi } from 'abitype'
 
 export type ViemTevmClient = {
-  tevmRequest<T extends NonVerboseTevmJsonRpcRequest>(r: T): Promise<BackendReturnType<T>['result']>
-  runScript<
-    TAbi extends Abi | readonly unknown[] = Abi,
-    TFunctionName extends string = string,
-  >(
-    action: RunScriptAction<TAbi, TFunctionName>,
-  ): Promise<RunScriptResult<TAbi, TFunctionName>>
-  putAccount(action: PutAccountAction): Promise<TevmPutAccountResponse['result']>
-  putContractCode(
-    action: PutContractCodeAction,
-  ): Promise<TevmPutContractCodeResponse['result']>
-  runCall(action: RunCallAction): Promise<TevmCallResponse['result']>
-  runContractCall<
-    TAbi extends Abi | readonly unknown[] = Abi,
-    TFunctionName extends string = string,
-  >(
-    action: RunContractCallAction<TAbi, TFunctionName>,
-  ): Promise<RunContractCallResult<TAbi, TFunctionName>>
+	tevmRequest<T extends NonVerboseTevmJsonRpcRequest>(
+		r: T,
+	): Promise<BackendReturnType<T>['result']>
+	runScript<
+		TAbi extends Abi | readonly unknown[] = Abi,
+		TFunctionName extends string = string,
+	>(
+		action: RunScriptAction<TAbi, TFunctionName>,
+	): Promise<RunScriptResult<TAbi, TFunctionName>>
+	putAccount(
+		action: PutAccountAction,
+	): Promise<TevmPutAccountResponse['result']>
+	putContractCode(
+		action: PutContractCodeAction,
+	): Promise<TevmPutContractCodeResponse['result']>
+	runCall(action: RunCallAction): Promise<TevmCallResponse['result']>
+	runContractCall<
+		TAbi extends Abi | readonly unknown[] = Abi,
+		TFunctionName extends string = string,
+	>(
+		action: RunContractCallAction<TAbi, TFunctionName>,
+	): Promise<RunContractCallResult<TAbi, TFunctionName>>
 }
 
-export type ViemTevmClientDecorator = (client: Pick<import('viem').Client, 'request'>) => ViemTevmClient
+export type ViemTevmClientDecorator = (
+	client: Pick<import('viem').Client, 'request'>,
+) => ViemTevmClient
 
 export type ViemTevmExtension = () => ViemTevmClientDecorator
-
