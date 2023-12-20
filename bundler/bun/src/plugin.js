0e1bd110b45bec182ddcd0b325c39dbf18e1490b
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-12-20 01:01:10 -0800

    :white_check_mark: Fix

--- bundler/bun/src/plugin.js
+++ bundler/bun/src/plugin.js
@@ -70,9 +70,7 @@ export const tevmBunPlugin = ({ solc = defaultSolc.version }) => {
 					`${path}.cjs`,
 				]
 				const existsArr = await Promise.all(
-					filePaths.map((filePath) =>
-						bunFileAccesObject.exists(filePath),
-					),
+					filePaths.map((filePath) => bunFileAccesObject.exists(filePath)),
 				)
 				for (const [i, exists] of existsArr.entries()) {
 					if (exists) {
