9d59126bad51e04db528e4810411056e89c7c521
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-12-20 15:32:05 -0800

    :sparkles: Feat(vm): Add writeContractOptimistic hook

--- vm/vm/src/viem/types.ts
+++ vm/vm/src/viem/types.ts
@@ -13,6 +13,7 @@ import type { TevmPutAccountResponse } from '../jsonrpc/putAccount/TevmPutAccoun
 import type { TevmPutContractCodeResponse } from '../jsonrpc/putContractCode/TevmPutContractCodeResponse.js'
 import type { TevmCallResponse } from '../jsonrpc/runCall/TevmCallResponse.js'
 import type { Abi } from 'abitype'
+import type { Account, Chain, Transport, WriteContractParameters, WriteContractReturnType } from 'viem'
 
 export type ViemTevmClient = {
 	tevmRequest<T extends NonVerboseTevmJsonRpcRequest>(
@@ -39,8 +40,31 @@ export type ViemTevmClient = {
 	): Promise<RunContractCallResult<TAbi, TFunctionName>>
 }
 
+export type ViemTevmOptimisticClient<
+	TChain extends Chain | undefined = Chain,
+	TAccount extends Account | undefined = Account | undefined,
+> = {
+	writeContractOptimistic<
+		TAbi extends Abi | readonly unknown[] = Abi,
+		TFunctionName extends string = string,
+		TChainOverride extends Chain | undefined = Chain | undefined
+	>(
+		action: WriteContractParameters<TAbi, TFunctionName, TChain, TAccount, TChainOverride>
+	): Promise<{ optimisticResult: () => Promise<RunContractCallResult<TAbi, TFunctionName>>, result: () => Promise<WriteContractReturnType> }>
+}
+
 export type ViemTevmClientDecorator = (
 	client: Pick<import('viem').Client, 'request'>,
 ) => ViemTevmClient
 
+export type ViemTevmOptimisticClientDecorator = <TTransport extends Transport = Transport, TChain extends Chain | undefined = Chain | undefined, TAccount extends Account | undefined = Account | undefined>(
+	client: Pick<import('viem').WalletClient<
+		TTransport,
+		TChain,
+		TAccount
+	>, 'request' | 'writeContract'>,
+) => ViemTevmOptimisticClient<TChain, TAccount>
+
 export type ViemTevmExtension = () => ViemTevmClientDecorator
+
+export type ViemTevmOptimisticExtension = () => ViemTevmOptimisticClientDecorator
