09989c197d42c55eb123862dab112a59b4b6a6c8
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-12-29 22:20:58 -0800

    :fire: Remove client package that got accidentally committed

--- vm/client/vm/src/jsonrpc/putAccount/TevmPutAccountRequest.ts
+++
@@ -1,8 +0,0 @@
-import type { PutAccountAction } from '../../actions/putAccount/PutAccountAction.js'
-
-export type TevmPutAccountRequest = {
-	params: PutAccountAction
-	jsonrpc: '2.0'
-	method: 'tevm_putAccount'
-	id?: string | number | null
-}
