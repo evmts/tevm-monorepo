bf6e307c218213255b8b7d560ad5dfa4c4758be6
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-12-31 15:36:22 -0800

    :white_check_mark: Fix: lint

--- vm/jsonrpc/src/createHttpHandler.ts
+++ vm/jsonrpc/src/createHttpHandler.ts
@@ -1,13 +1,13 @@
-import type { TevmJsonRpcRequest } from './TevmJsonRpcRequest.js';
-import { createJsonRpcClient } from './createJsonRpcClient.js';
-import type { EVM } from '@ethereumjs/evm';
-import type { IncomingMessage, ServerResponse } from 'http';
-import { parse, stringify } from 'superjson';
+import type { TevmJsonRpcRequest } from './TevmJsonRpcRequest.js'
+import { createJsonRpcClient } from './createJsonRpcClient.js'
+import type { EVM } from '@ethereumjs/evm'
+import type { IncomingMessage, ServerResponse } from 'http'
+import { parse, stringify } from 'superjson'
 
 type CreatehttpHandlerParameters = {
-	evm: EVM;
-	forkUrl?: string;
-};
+	evm: EVM
+	forkUrl?: string
+}
 
 /**
  * Creates an http request handler for tevm requests
@@ -16,10 +16,16 @@ export function createHttpHandler({
 	evm,
 	forkUrl,
 }: CreatehttpHandlerParameters) {
-	const client = createJsonRpcClient(evm);
+	const client = createJsonRpcClient(evm)
 
-	const handleErrorResponse = (res: ServerResponse, id: string, method: string, code: number, message: string) => {
-		res.writeHead(code, { 'Content-Type': 'application/json' });
+	const handleErrorResponse = (
+		res: ServerResponse,
+		id: string,
+		method: string,
+		code: number,
+		message: string,
+	) => {
+		res.writeHead(code, { 'Content-Type': 'application/json' })
 		const errorResponse = {
 			id,
 			method,
@@ -28,9 +34,9 @@ export function createHttpHandler({
 				code,
 				message,
 			},
-		};
-		res.end(JSON.stringify(errorResponse));
-	};
+		}
+		res.end(JSON.stringify(errorResponse))
+	}
 
 	const processRequest = async (request: TevmJsonRpcRequest) => {
 		if (!request.method.startsWith('tevm_')) {
@@ -43,7 +49,7 @@ export function createHttpHandler({
 						code: 404,
 						message: 'Invalid jsonrpc request: Fork url not set',
 					},
-				};
+				}
 			}
 
 			// TODO there is no error handling here
@@ -53,18 +59,18 @@ export function createHttpHandler({
 				headers: {
 					'Content-Type': 'application/json',
 				},
-			}).then((res) => res.json());
+			}).then((res) => res.json())
 
 			return {
 				id: request.id,
 				...(response as object),
-			};
+			}
 		}
 
 		const parsedRequest = {
 			...request,
 			params: request.params && parse(JSON.stringify(request.params)),
-		};
+		}
 
 		// Verify if it's a valid JSON RPC 2.0 request
 		if (parsedRequest.jsonrpc !== '2.0') {
@@ -76,10 +82,13 @@ export function createHttpHandler({
 					code: 500,
 					message: `Invalid jsonrpc request: Invalid schema ${parsedRequest.jsonrpc}`,
 				},
-			};
+			}
 		}
 
-		if (parsedRequest.method as any === '' || typeof parsedRequest.method !== 'string') {
+		if (
+			(parsedRequest.method as any) === '' ||
+			typeof parsedRequest.method !== 'string'
+		) {
 			return {
 				id: parsedRequest.id,
 				method: parsedRequest.method,
@@ -88,18 +97,18 @@ export function createHttpHandler({
 					code: 500,
 					message: 'Invalid jsonrpc request',
 				},
-			};
+			}
 		}
 
 		try {
-			const result = await client(parsedRequest as any);
+			const result = await client(parsedRequest as any)
 			return {
 				id: parsedRequest.id,
 				...result,
 				result: JSON.parse(stringify(result.result)),
-			};
+			}
 		} catch (e) {
-			console.error(e);
+			console.error(e)
 			return {
 				id: parsedRequest.id,
 				method: parsedRequest.method,
@@ -108,35 +117,41 @@ export function createHttpHandler({
 					code: 500,
 					message: 'Internal server error',
 				},
-			};
+			}
 		}
-	};
+	}
 
 	return async (req: IncomingMessage, res: ServerResponse) => {
-		let body = '';
+		let body = ''
 
 		req.on('data', (chunk) => {
-			body += chunk.toString();
-		});
+			body += chunk.toString()
+		})
 
 		req.on('end', async () => {
 			try {
-				const raw = JSON.parse(body);
+				const raw = JSON.parse(body)
 
 				if (Array.isArray(raw)) {
-					const responses = await Promise.all(raw.map(processRequest));
-					res.writeHead(200, { 'Content-Type': 'application/json' });
+					const responses = await Promise.all(raw.map(processRequest))
+					res.writeHead(200, { 'Content-Type': 'application/json' })
 					console.log(JSON.stringify(responses, null, 2))
-					res.end(JSON.stringify(responses));
+					res.end(JSON.stringify(responses))
 				} else {
-					const response = await processRequest(raw);
-					res.writeHead(200, { 'Content-Type': 'application/json' });
-					res.end(JSON.stringify(response));
+					const response = await processRequest(raw)
+					res.writeHead(200, { 'Content-Type': 'application/json' })
+					res.end(JSON.stringify(response))
 				}
 			} catch (e) {
-				console.error(e);
-				handleErrorResponse(res, 'unknown', 'unknown', 500, 'Invalid jsonrpc request: Unable to parse json');
+				console.error(e)
+				handleErrorResponse(
+					res,
+					'unknown',
+					'unknown',
+					500,
+					'Invalid jsonrpc request: Unable to parse json',
+				)
 			}
-		});
-	};
+		})
+	}
 }
