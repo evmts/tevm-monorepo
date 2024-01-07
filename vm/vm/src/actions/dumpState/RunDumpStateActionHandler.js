f00ae985290fab09148fe07b61ddfb149fdb8f62
0xNonCents <0xNonCents@gmail.com>
2024-01-07 13:40:05 -0500

    use actionHandlers, update Ethers and Viem state manager

--- vm/vm/src/actions/dumpState/RunDumpStateActionHandler.js
+++ vm/vm/src/actions/dumpState/RunDumpStateActionHandler.js
@@ -1,7 +1,31 @@
+import { Address } from '@ethereumjs/util'
 
 /**
  * @type {import("./RunDumpStateHandlerGeneric.js").RunDumpStateHandlerGeneric}
  */
-export const RunDumpStateActionHandler = async ( tevm ) => {
-  return await tevm._evm.stateManager.dumpState();
+export const RunDumpStateActionHandler = async (stateManager) => {
+	const accountAddresses = await stateManager.getAccountAddresses()
+
+	const state = {}
+
+	for (const address of accountAddresses) {
+		try {
+			const hexAddress = `0x${address}`
+			const account = await stateManager.getAccount(
+				Address.fromString(hexAddress),
+			)
+
+			if (account !== undefined) {
+				const storage = await stateManager.dumpStorage(
+					Address.fromString(hexAddress),
+				)
+
+				state[hexAddress] = { ...account, storage }
+			}
+		} catch (error) {
+			console.error(`Error fetching account for ${address}: ${error.message}`)
+		}
+	}
+
+	return state
 }
--- vm/vm/src/actions/loadState/RunLoadStateActionHandler.js
+++ vm/vm/src/actions/loadState/RunLoadStateActionHandler.js
@@ -1,7 +1,20 @@
+import { Account, Address } from "@ethereumjs/util"
 
 /**
  * @type {import("./RunLoadStateHandlerGeneric.js").RunLoadStateActionHandler}
  */
 export const RunLoadStateActionHandler = async ( tevm, tevmState ) => {
-  await tevm._evm.stateManager.loadState(tevmState);
+  for (const [k, v] of Object.entries(tevmState)) {
+    const { nonce, balance, storageRoot, codeHash, storage } = v
+    const account = new Account(nonce, balance, storageRoot, codeHash)
+    const address = Address.fromString(k)
+    tevm._evm.stateManager.putAccountData(address, account)
+    if (storage !== undefined) {
+      for (const [storageKey, storageData] of Object.entries(storage)) {
+        const key = Uint8Array.from(Buffer.from(storageKey, 'hex'))
+        const data = Uint8Array.from(Buffer.from(storageData, 'hex'))
+        tevm._evm.stateManager.putContractStorage(address, key, data)
+      }
+    }
+  }
 }
