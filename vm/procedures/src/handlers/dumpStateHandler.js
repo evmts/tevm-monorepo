f7277849210711f27d394938f9a15fc9809f9240
0xNonCents <0xNonCents@gmail.com>
2024-01-09 18:04:30 -0500

    rm error catch

--- vm/procedures/src/handlers/dumpStateHandler.js
+++ vm/procedures/src/handlers/dumpStateHandler.js
@@ -12,7 +12,6 @@ export const RunDumpStateActionHandler = async (stateManager) => {
 const state = {}
 
 	for (const address of accountAddresses) {
-		try {
 			const hexAddress = `0x${address}`
 			const account = await stateManager.getAccount(
 				Address.fromString(hexAddress),
@@ -25,9 +24,6 @@ const state = {}
 
 				state[hexAddress] = { ...account, storage }
 			}
-		} catch (error) {
-			console.error(`Error fetching account for ${address}: ${error.message}`)
-		}
 	}
 
 	return state
