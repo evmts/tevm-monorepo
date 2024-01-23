e884f7dbd7daf475c4663711ad12133e6e167696
Will Cory <willcory10@gmail.com>
2024-01-22 23:29:27 -0800

    Update packages/actions/src/eth/index.js
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- packages/actions/src/eth/index.js
+++ packages/actions/src/eth/index.js
@@ -1,7 +1,6 @@
 export * from './chainIdHandler.js'
 export * from './getCodeHandler.js'
 export * from './gasPriceHandler.js'
-export * from './gasPriceHandler.js'
 export * from './blockNumberHandler.js'
 export * from './getBalanceHandler.js'
 export * from './getStorageAtHandler.js'
