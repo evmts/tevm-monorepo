88d2f2ca9087f61a3e77a14a128a841613e21459
Will Cory <willcory@Wills-MacBook-Pro.local>
2024-01-25 16:49:25 -0800

    :sparkles: Feat: Add eth_call handler

--- packages/actions/src/eth/ethCallHandler.js
+++ packages/actions/src/eth/ethCallHandler.js
@@ -5,10 +5,10 @@ import { callHandler } from '../index.js'
  * @returns {import('@tevm/actions-types').EthCallHandler}
  */
 export const ethCallHandler = (evm) => async (params) => {
-  return callHandler(evm)(params).then(res => {
-    if (res.errors?.length) {
-      throw res.errors?.[0]
-    }
-    return res.rawData
-  })
+	return callHandler(evm)({ skipBalance: true, ...params }).then((res) => {
+		if (res.errors?.length) {
+			throw res.errors?.[0]
+		}
+		return res.rawData
+	})
 }
