16c6678492683e04684eb9ab7490210afadfd8da
Will Cory <willcory10@gmail.com>
2024-02-06 22:35:23 -0800

    :fire: Fix: remove unused files (#908)
    ## Description
    
    _Concise description of proposed changes_
    
    ## Testing
    
    Explain the quality checks that have been done on the code changes
    
    ## Additional Information
    
    - [ ] I read the [contributing docs](../docs/contributing.md) (if this
    is your first contribution)
    
    Your ENS/address:
    
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

--- vm/plugins.ts
+++
@@ -1,7 +0,0 @@
-import { evmtsBunPlugin } from '@evmts/bun-plugin'
-import { plugin } from 'bun'
-
-// Load EVMts plugin to enable solidity imports
-// EVMts is configured in the tsconfig.json
-// in future it will be configured via evmts.config.ts
-plugin(evmtsBunPlugin({}))
