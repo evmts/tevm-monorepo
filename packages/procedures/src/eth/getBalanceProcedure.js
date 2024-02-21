d20437a5a3c92efaadcc32099b486289d31a3200
Will Cory <willcory@Wills-MacBook-Pro.local>
2024-02-21 07:44:25 -0800

    :rotating_light: Lint

--- packages/procedures/src/eth/getBalanceProcedure.js
+++ packages/procedures/src/eth/getBalanceProcedure.js
@@ -7,25 +7,27 @@ import { numberToHex } from '@tevm/utils'
  */
 export const getBalanceProcedure =
 	({ vm, forkUrl }) =>
-		async (req) => {
-			if (!req.params[1]) {
-				throw new Error('getBalanceProcedure received invalid parameters: Block parameter (req.params[1]) is missing or invalid. Expected a hex string or block tag (e.g., "latest", "earliest").')
-			}
-			return ({
-				...(req.id ? { id: req.id } : {}),
-				jsonrpc: '2.0',
-				method: req.method,
-				result: numberToHex(
-					await getBalanceHandler({ vm, forkUrl })({
-						address: req.params[0],
-						...(req.params[1].startsWith('0x')
-							? { blockNumber: BigInt(req.params[1]) }
-							: {
+	async (req) => {
+		if (!req.params[1]) {
+			throw new Error(
+				'getBalanceProcedure received invalid parameters: Block parameter (req.params[1]) is missing or invalid. Expected a hex string or block tag (e.g., "latest", "earliest").',
+			)
+		}
+		return {
+			...(req.id ? { id: req.id } : {}),
+			jsonrpc: '2.0',
+			method: req.method,
+			result: numberToHex(
+				await getBalanceHandler({ vm, forkUrl })({
+					address: req.params[0],
+					...(req.params[1].startsWith('0x')
+						? { blockNumber: BigInt(req.params[1]) }
+						: {
 								blockTag: /** @type {import('@tevm/utils').BlockTag}*/ (
 									req.params[1]
 								),
-							}),
-					}),
-				),
-			})
+						  }),
+				}),
+			),
 		}
+	}
