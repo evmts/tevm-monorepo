84099473c545e7d5e01b219a1db3f5c75eac5f48
Will Cory <willcory@Wills-MacBook-Pro.local>
2024-02-25 10:07:21 -0800

    :construction: Update zod params for account

--- packages/zod/src/params/zGetAccountParams.js
+++ packages/zod/src/params/zGetAccountParams.js
@@ -1,3 +1,4 @@
+import { z } from 'zod'
 import { zAddress } from '../index.js'
 import { zBaseParams } from './zBaseParams.js'
 
@@ -5,5 +6,13 @@ import { zBaseParams } from './zBaseParams.js'
  * Zod validator for a valid getAccount action
  */
 export const zGetAccountParams = zBaseParams
-	.extend({ address: zAddress })
+	.extend({
+		address: zAddress,
+		returnStorage: z
+			.boolean()
+			.optional()
+			.describe(
+				'If true will return storage. Defaults to false. This can be expensive'
+			)
+	})
 	.describe('Params to create an account or contract')
