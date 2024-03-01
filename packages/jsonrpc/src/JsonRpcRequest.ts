cee58304e8205189c1f80351e598af649a06a576
Will Cory <willcory@Wills-MacBook-Pro.local>
2024-03-01 00:43:54 -0800

    :bug: Make JSON-RPC request readonly

--- packages/jsonrpc/src/JsonRpcRequest.ts
+++ packages/jsonrpc/src/JsonRpcRequest.ts
@@ -2,7 +2,7 @@
  * Helper type for creating JSON-RPC request types
  */
 export type JsonRpcRequest<TMethod extends string, TParams> = {
-	jsonrpc: '2.0'
-	method: TMethod
-	id?: string | number | null
-} & (TParams extends readonly [] ? { params?: TParams } : { params: TParams })
+	readonly jsonrpc: '2.0'
+	readonly method: TMethod
+	readonly id?: string | number | null
+} & (TParams extends readonly [] ? { readonly params?: TParams } : { readonly params: TParams })
