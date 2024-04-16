049c779b2929f5e554d606a773f979a1f0a4ebec
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-04-15 20:45:41 -0700

    :white_check_mark: Chore: Clean up rlp package

---
+++ packages/rlp/src/Rlp.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from 'bun:test'
+import { Rlp } from './Rlp.js'
+
+test('reexports the Rlp method from ethereumjs', () => {
+  expect(Rlp.decode(Uint8Array.from([]))).toEqual(Uint8Array.from([]))
+  expect(Rlp.encode("")).toEqual(Uint8Array.from([128]))
+})
