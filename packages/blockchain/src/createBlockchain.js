806e7b62ec100f5fec90a87214c7b00737339a60
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-04-18 19:02:19 -0700

    :construction: Start updating blockchain

--- packages/blockchain/src/createBlockchain.js
+++ packages/blockchain/src/createBlockchain.js
@@ -11,40 +11,40 @@ import { createMemoryDb, parseGwei } from '@tevm/utils'
  * @returns {Promise<TevmBlockchain>}
  */
 export const createBlockchain = async ({ common }) => {
-	const db = createMemoryDb()
-	/**
-	 * @type {import('@tevm/utils').GenesisState}
-	 */
-	const genesisState = {}
+  const db = createMemoryDb()
+  /**
+   * @type {import('@tevm/utils').GenesisState}
+   */
+  const genesisState = {}
 
-	const genesisBlock = Block.fromBlockData(
-		{
-			header: {
-				...common.genesis(),
-				baseFeePerGas: parseGwei('1'),
-				coinbase: '0xc014ba5ec014ba5ec014ba5ec014ba5ec014ba5e',
-			},
-			...(common.isActivatedEIP(4895)
-				? {
-						withdrawals:
+  const genesisBlock = Block.fromBlockData(
+    {
+      header: {
+        ...common.genesis(),
+        baseFeePerGas: parseGwei('1'),
+        coinbase: '0xc014ba5ec014ba5ec014ba5ec014ba5ec014ba5e',
+      },
+      ...(common.isActivatedEIP(4895)
+        ? {
+          withdrawals:
 							/** @type {Array<import('@tevm/utils').WithdrawalData>}*/ ([]),
-				  }
-				: {}),
-		},
-		{ common, setHardfork: false, skipConsensusFormatValidation: true },
-	)
-	const stateRoot = await genesisStateRoot(genesisState)
+        }
+        : {}),
+    },
+    { common, setHardfork: false, skipConsensusFormatValidation: true },
+  )
+  const stateRoot = await genesisStateRoot(genesisState)
 
-	return TevmBlockchain.create({
-		genesisState,
-		hardforkByHeadBlockNumber: false,
-		db,
-		common,
-		validateBlocks: false,
-		validateConsensus: false,
-		genesisBlock,
-		genesisStateRoot: stateRoot,
-		// using ethereumjs defaults for this and disabling it
-		// consensus,
-	})
+  return TevmBlockchain.create({
+    genesisState,
+    hardforkByHeadBlockNumber: false,
+    db,
+    common,
+    validateBlocks: false,
+    validateConsensus: false,
+    genesisBlock,
+    genesisStateRoot: stateRoot,
+    // using ethereumjs defaults for this and disabling it
+    // consensus,
+  })
 }
