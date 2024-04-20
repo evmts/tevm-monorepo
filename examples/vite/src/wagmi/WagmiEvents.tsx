b944382e01ba0d38576e86a386cc588bcaefd1c0
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-04-20 13:50:24 -0700

    :bug: Fix: Missing key in react component example-vite

--- examples/vite/src/wagmi/WagmiEvents.tsx
+++ examples/vite/src/wagmi/WagmiEvents.tsx
@@ -1,48 +1,48 @@
-import { addresses } from '../addresses'
-import { WagmiMintExample } from '../contracts/WagmiMintExample.sol'
 import { useState } from 'react'
 import { useAccount, useBlockNumber, useContractEvent } from 'wagmi'
+import { addresses } from '../addresses'
+import { WagmiMintExample } from '../contracts/WagmiMintExample.sol'
 
 export const WagmiEvents = () => {
-	const { address } = useAccount()
+  const { address } = useAccount()
 
-	const { data: blockNumber } = useBlockNumber()
+  const { data: blockNumber } = useBlockNumber()
 
-	// TODO add types to TevmContract type
-	const [events, setEvents] = useState<any[]>([])
+  // TODO add types to TevmContract type
+  const [events, setEvents] = useState<any[]>([])
 
-	/**
-	 * ABI of events can be found at events.Foo
-	 * - Don't call fn and it is an object without args
-	 * - Call fn with args and fromBlock etc. and it returns an object with args
-	 */
-	const transferEvents = WagmiMintExample.events.Transfer({
-		fromBlock: blockNumber && blockNumber - BigInt(1_000),
-		args: {
-			to: address,
-		},
-	})
+  /**
+   * ABI of events can be found at events.Foo
+   * - Don't call fn and it is an object without args
+   * - Call fn with args and fromBlock etc. and it returns an object with args
+   */
+  const transferEvents = WagmiMintExample.events.Transfer({
+    fromBlock: blockNumber && blockNumber - BigInt(1_000),
+    args: {
+      to: address,
+    },
+  })
 
-	useContractEvent({
-		...transferEvents,
-		address: addresses[420],
-		listener: (event) => {
-			setEvents([...events, event])
-		},
-	})
+  useContractEvent({
+    ...transferEvents,
+    address: addresses[420],
+    listener: (event) => {
+      setEvents([...events, event])
+    },
+  })
 
-	return (
-		<div>
-			<div style={{ display: 'flex', flexDirection: 'column-reverse' }}>
-				{events.map((event, i) => {
-					return (
-						<div>
-							<div>Event {i}</div>
-							<div>{JSON.stringify(event)}</div>
-						</div>
-					)
-				})}
-			</div>
-		</div>
-	)
+  return (
+    <div>
+      <div style={{ display: 'flex', flexDirection: 'column-reverse' }}>
+        {events.map((event, i) => {
+          return (
+            <div key={i}>
+              <div>Event {i}</div>
+              <div>{JSON.stringify(event)}</div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
