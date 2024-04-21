58da860621b2026a8044500cad46bf797232b16d
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-04-21 09:01:26 -0700

    :pencil2: Fix: Renaming bug

--- packages/blockchain/src/index.ts
+++ packages/blockchain/src/index.ts
@@ -1,4 +1,4 @@
 export { createBlockchain } from './createBlockchain.js'
-export { type TevmBlockchain } from './TevmBlockchain.js'
+export { type Blockchain } from './TevmBlockchain.js'
 export { Chain } from './Chain.js'
 export { ReceiptsManager } from './RecieptManager.js'
