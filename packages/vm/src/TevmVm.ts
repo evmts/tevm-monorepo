0765e1207e0048f91950de2389d71e144cae7041
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-04-29 16:35:07 -0700

    :white_check_mark: Fix: build

--- packages/vm/src/TevmVm.ts
+++ packages/vm/src/TevmVm.ts
@@ -1,119 +1,120 @@
 import { VM } from '@ethereumjs/vm'
-import { type Chain, createBlockchain } from '@tevm/blockchain'
+import { type Chain, createChain } from '@tevm/blockchain'
 import { Common } from '@tevm/common'
 import { Evm, createEvm, getActivePrecompiles } from '@tevm/evm'
 import { type StateManager, createStateManager } from '@tevm/state'
 import { EthjsAccount, EthjsAddress, hexToBytes } from '@tevm/utils'
 
 export class TevmVm extends VM {
-	declare evm: Evm
-	declare blockchain: Chain
-	declare common: Common
-	/**
-	 * VM async constructor. Creates engine instance and initializes it.
-	 *
-	 * @param opts VM engine constructor options
-	 */
-	static override async create(opts: Parameters<typeof VM.create>[0] = {}): Promise<TevmVm> {
-		// Save if a `StateManager` was passed (for activatePrecompiles)
-		const didPassStateManager = opts.stateManager !== undefined
+  declare evm: Evm
+  declare blockchain: Chain
+  declare common: Common
+  /**
+   * VM async constructor. Creates engine instance and initializes it.
+   *
+   * @param opts VM engine constructor options
+   */
+  static override async create(opts: Parameters<typeof VM.create>[0] = {}): Promise<TevmVm> {
+    // Save if a `StateManager` was passed (for activatePrecompiles)
+    const didPassStateManager = opts.stateManager !== undefined
 
-		// Add common, SM, blockchain, EVM here
-		if (opts.common === undefined) {
-			opts.common = new Common({ chain: 1, eips: [1559, 4895] })
-		}
+    let normalizedOpts = { ...opts }
+    // Add common, SM, blockchain, EVM here
+    if (normalizedOpts.common === undefined) {
+      normalizedOpts.common = new Common({ chain: 1, eips: [1559, 4895] })
+    }
 
-		if (opts.stateManager === undefined) {
-			opts.stateManager = createStateManager({})
-		}
+    if (normalizedOpts.stateManager === undefined) {
+      normalizedOpts.stateManager = createStateManager({})
+    }
 
-		if (opts.blockchain === undefined) {
-			opts.blockchain = await createBlockchain({ common: opts.common })
-		}
+    if (normalizedOpts.blockchain === undefined) {
+      normalizedOpts.blockchain = await createChain({ common: normalizedOpts.common })
+    }
 
-		const genesisState = opts.genesisState ?? {}
-		if (opts.genesisState !== undefined) {
-			await opts.stateManager.generateCanonicalGenesis(genesisState)
-		}
+    const genesisState = normalizedOpts.genesisState ?? {}
+    if (normalizedOpts.genesisState !== undefined) {
+      await normalizedOpts.stateManager.generateCanonicalGenesis(genesisState)
+    }
 
-		if (opts.profilerOpts !== undefined) {
-			const profilerOpts = opts.profilerOpts
-			if (profilerOpts.reportAfterBlock === true && profilerOpts.reportAfterTx === true) {
-				throw new Error(
-					'Cannot have `reportProfilerAfterBlock` and `reportProfilerAfterTx` set to `true` at the same time',
-				)
-			}
-		}
+    if (normalizedOpts.profilerOpts !== undefined) {
+      const profilerOpts = normalizedOpts.profilerOpts
+      if (profilerOpts.reportAfterBlock === true && profilerOpts.reportAfterTx === true) {
+        throw new Error(
+          'Cannot have `reportProfilerAfterBlock` and `reportProfilerAfterTx` set to `true` at the same time',
+        )
+      }
+    }
 
-		if (opts.evm === undefined) {
-			let enableProfiler = false
-			if (opts.profilerOpts?.reportAfterBlock === true || opts.profilerOpts?.reportAfterTx === true) {
-				enableProfiler = true
-			}
-			opts.evm = await Evm.create({
-				common: opts.common,
-				stateManager: opts.stateManager,
-				blockchain: opts.blockchain,
-				profiler: {
-					enabled: enableProfiler,
-				},
-			})
-		}
+    if (normalizedOpts.evm === undefined) {
+      let enableProfiler = false
+      if (normalizedOpts.profilerOpts?.reportAfterBlock === true || normalizedOpts.profilerOpts?.reportAfterTx === true) {
+        enableProfiler = true
+      }
+      normalizedOpts.evm = await Evm.create({
+        common: normalizedOpts.common,
+        stateManager: normalizedOpts.stateManager,
+        ...(normalizedOpts.blockchain !== undefined ? { blockchain: opts.blockchain } : {}),
+        profiler: {
+          enabled: enableProfiler,
+        },
+      })
+    }
 
-		if (opts.activatePrecompiles === true && !didPassStateManager) {
-			await opts.evm.journal.checkpoint()
-			// put 1 wei in each of the precompiles in order to make the accounts non-empty and thus not have them deduct `callNewAccount` gas.
-			for (const [addressStr] of getActivePrecompiles(opts.common)) {
-				const address = new EthjsAddress(hexToBytes(`0x${addressStr}`))
-				let account = await opts.evm.stateManager.getAccount(address)
-				// Only do this if it is not overridden in genesis
-				// Note: in the case that custom genesis has storage fields, this is preserved
-				if (account === undefined) {
-					account = new EthjsAccount()
-					const newAccount = EthjsAccount.fromAccountData({
-						balance: 1,
-						storageRoot: account.storageRoot,
-					})
-					await opts.evm.stateManager.putAccount(address, newAccount)
-				}
-			}
-			await opts.evm.journal.commit()
-		}
+    if (normalizedOpts.activatePrecompiles === true && !didPassStateManager) {
+      await normalizedOpts.evm.journal.checkpoint()
+      // put 1 wei in each of the precompiles in order to make the accounts non-empty and thus not have them deduct `callNewAccount` gas.
+      for (const [addressStr] of getActivePrecompiles(normalizedOpts.common)) {
+        const address = new EthjsAddress(hexToBytes(`0x${addressStr}`))
+        let account = await normalizedOpts.evm.stateManager.getAccount(address)
+        // Only do this if it is not overridden in genesis
+        // Note: in the case that custom genesis has storage fields, this is preserved
+        if (account === undefined) {
+          account = new EthjsAccount()
+          const newAccount = EthjsAccount.fromAccountData({
+            balance: 1,
+            storageRoot: account.storageRoot,
+          })
+          await normalizedOpts.evm.stateManager.putAccount(address, newAccount)
+        }
+      }
+      await normalizedOpts.evm.journal.commit()
+    }
 
-		return new TevmVm(opts)
-	}
+    return new TevmVm(normalizedOpts)
+  }
 
-	declare stateManager: StateManager
+  declare stateManager: StateManager
 
-	public deepCopy = async (): Promise<TevmVm> => {
-		const common = this.common.copy()
-		common.setHardfork(this.common.hardfork())
+  public deepCopy = async (): Promise<TevmVm> => {
+    const common = this.common.copy()
+    common.setHardfork(this.common.hardfork())
 
-		// TODO we should deep copy once @tevm/blockchain is merged
-		const blockchain = this.blockchain.shallowCopy()
-		if (!('deepCopy' in this.stateManager)) {
-			throw new Error('StateManager does not support deepCopy. Was a Tevm state manager used?')
-		}
-		const stateManager = await (this.stateManager as StateManager).deepCopy()
+    // TODO we should deep copy once @tevm/blockchain is merged
+    const blockchain = this.blockchain.shallowCopy()
+    if (!('deepCopy' in this.stateManager)) {
+      throw new Error('StateManager does not support deepCopy. Was a Tevm state manager used?')
+    }
+    const stateManager = await (this.stateManager as StateManager).deepCopy()
 
-		const evmCopy = await createEvm({
-			blockchain,
-			common,
-			stateManager,
-			allowUnlimitedContractSize: this.evm.allowUnlimitedContractSize ?? false,
-			customPrecompiles: (this.evm as any)._customPrecompiles,
-			// customPredeploys isn't needed because it will be copied along in stateManager.deepCopy
-			// customPredeploys,
-			profiler: Boolean((this.evm as any).optsCached?.profiler?.enabled) ?? false,
-		})
-		return TevmVm.create({
-			stateManager,
-			blockchain: this.blockchain,
-			activatePrecompiles: true,
-			common,
-			evm: evmCopy,
-			setHardfork: this._setHardfork,
-			...(this._opts.profilerOpts ? { profilerOpts: this._opts.profilerOpts } : {}),
-		})
-	}
+    const evmCopy = await createEvm({
+      blockchain,
+      common,
+      stateManager,
+      allowUnlimitedContractSize: this.evm.allowUnlimitedContractSize ?? false,
+      customPrecompiles: (this.evm as any)._customPrecompiles,
+      // customPredeploys isn't needed because it will be copied along in stateManager.deepCopy
+      // customPredeploys,
+      profiler: Boolean((this.evm as any).optsCached?.profiler?.enabled) ?? false,
+    })
+    return TevmVm.create({
+      stateManager,
+      blockchain: this.blockchain,
+      activatePrecompiles: true,
+      common,
+      evm: evmCopy,
+      setHardfork: this._setHardfork,
+      ...(this._opts.profilerOpts ? { profilerOpts: this._opts.profilerOpts } : {}),
+    })
+  }
 }
--- packages/vm/src/createVm.spec.ts
+++ packages/vm/src/createVm.spec.ts
@@ -1,61 +1,61 @@
 import { describe, expect, it } from 'bun:test'
-import { createBlockchain } from '@tevm/blockchain'
+import { createChain } from '@tevm/blockchain'
 import { createCommon } from '@tevm/common'
 import { createEvm } from '@tevm/evm'
 import { createStateManager } from '@tevm/state'
 import { createVm } from './createVm.js'
 
 describe(createVm.name, () => {
-	it('wraps ethereumjs vm', async () => {
-		const common = createCommon({})
-		const stateManager = createStateManager({})
-		const blockchain = await createBlockchain({
-			common,
-		})
-		const evm = await createEvm({
-			stateManager,
-			blockchain,
-			common,
-		})
-		const vm = await createVm({
-			evm,
-			common,
-			blockchain,
-			stateManager,
-		})
-		const newBlock = await vm
-			.buildBlock({
-				parentBlock: await blockchain.getCanonicalHeadBlock(),
-			})
-			.then((b) => b.build())
-			.then((b) => b.toJSON())
-		expect(newBlock).toMatchObject({
-			header: {
-				baseFeePerGas: '0x342770c0',
-				blobGasUsed: '0x0',
-				coinbase: '0x0000000000000000000000000000000000000000',
-				difficulty: '0x0',
-				excessBlobGas: '0x0',
-				extraData: '0x',
-				gasLimit: '0x1388',
-				gasUsed: '0x0',
-				logsBloom:
-					'0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
-				mixHash: '0x0000000000000000000000000000000000000000000000000000000000000000',
-				nonce: '0x0000000000000000',
-				number: '0x1',
-				parentBeaconBlockRoot: '0x0000000000000000000000000000000000000000000000000000000000000000',
-				parentHash: '0xe4000aed2620e6dd377af93917b22f21275cf97679249222c7f69b82759e425c',
-				receiptTrie: '0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421',
-				//  "stateRoot": "0x7e9c1d8ffc75d0809c9162a697cd5344b99b1973a35c2c07277b75b0bbb7340c",
-				// "timestamp": "0x661f5473",
-				transactionsTrie: '0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421',
-				uncleHash: '0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347',
-				withdrawalsRoot: '0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421',
-			},
-			transactions: [],
-			uncleHeaders: [],
-			withdrawals: [],
-		})
-	})
+  it('wraps ethereumjs vm', async () => {
+    const common = createCommon({})
+    const stateManager = createStateManager({})
+    const blockchain = await createChain({
+      common,
+    })
+    const evm = await createEvm({
+      stateManager,
+      blockchain,
+      common,
+    })
+    const vm = await createVm({
+      evm,
+      common,
+      blockchain,
+      stateManager,
+    })
+    const newBlock = await vm
+      .buildBlock({
+        parentBlock: await blockchain.getCanonicalHeadBlock(),
+      })
+      .then((b) => b.build())
+      .then((b) => b.toJSON())
+    expect(newBlock).toMatchObject({
+      header: {
+        baseFeePerGas: '0x342770c0',
+        blobGasUsed: '0x0',
+        coinbase: '0x0000000000000000000000000000000000000000',
+        difficulty: '0x0',
+        excessBlobGas: '0x0',
+        extraData: '0x',
+        gasLimit: '0x1388',
+        gasUsed: '0x0',
+        logsBloom:
+          '0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+        mixHash: '0x0000000000000000000000000000000000000000000000000000000000000000',
+        nonce: '0x0000000000000000',
+        number: '0x1',
+        parentBeaconBlockRoot: '0x0000000000000000000000000000000000000000000000000000000000000000',
+        parentHash: '0xe4000aed2620e6dd377af93917b22f21275cf97679249222c7f69b82759e425c',
+        receiptTrie: '0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421',
+        //  "stateRoot": "0x7e9c1d8ffc75d0809c9162a697cd5344b99b1973a35c2c07277b75b0bbb7340c",
+        // "timestamp": "0x661f5473",
+        transactionsTrie: '0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421',
+        uncleHash: '0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347',
+        withdrawalsRoot: '0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421',
+      },
+      transactions: [],
+      uncleHeaders: [],
+      withdrawals: [],
+    })
+  })
 })
