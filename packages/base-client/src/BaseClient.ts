5b7326eb4dbfffe212c13818fcfa6a1e1c5bdf8a
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-04-30 19:43:13 -0700

    :bug: Fix: Bug with chainId on common not matching client

--- packages/base-client/src/BaseClient.ts
+++ packages/base-client/src/BaseClient.ts
@@ -1,4 +1,4 @@
-import type { Chain, ReceiptsManager } from '@tevm/blockchain'
+import type { ReceiptsManager } from '@tevm/blockchain'
 import type { Logger } from '@tevm/logger'
 import type { TxPool } from '@tevm/txpool'
 import type { TevmVm } from '@tevm/vm'
@@ -8,87 +8,69 @@ import type { MiningConfig } from './MiningConfig.js'
  * The base client used by Tevm. Add extensions to add additional functionality
  */
 export type BaseClient<TMode extends 'fork' | 'normal' = 'fork' | 'normal', TExtended = {}> = {
-	/**
-	 * The logger instance
-	 */
-	readonly logger: Logger
-	/**
-	 * Interface for querying receipts and historical state
-	 */
-	readonly getReceiptsManager: () => Promise<ReceiptsManager>
-	/**
-	 * Represents the entire blockchain including it's logs and historical state
-	 */
-	readonly getChain: () => Promise<Chain>
-	/**
-	 * The configuration for mining. Defaults to 'auto'
-	 * - 'auto' will mine a block on every transaction
-	 * - 'interval' will mine a block every `interval` milliseconds
-	 * - 'manual' will not mine a block automatically and requires a manual call to `mineBlock`
-	 */
-	readonly miningConfig: MiningConfig
-	/**
-	 * Gets the chainId of the current EVM
-	 * @example
-	 * ```ts
-	 * const client = createMemoryClient()
-	 * const chainId = await client.getChainId()
-	 * console.log(chainId)
-	 * ```
-	 */
-	readonly getChainId: () => Promise<number>
-	/**
-	 * Sets the chain id of the current EVM
-	 */
-	readonly setChainId: (chainId: number) => void
-	/**
-	 * Fork url if the EVM is forked
-	 * @example
-	 * ```ts
-	 * const client = createMemoryClient({ forkUrl: 'https://mainnet.infura.io/v3/your-api-key' })
-	 * console.log(client.forkUrl)
-	 * ```
-	 */
-	readonly forkUrl?: string | undefined
-	/**
-	 * The mode the current client is running in
-	 * `fork` mode will fetch and cache all state from the block forked from the provided URL
-	 * `normal` mode will not fetch any state and will only run the EVM in memory
-	 * @example
-	 * ```ts
-	 * let client = createMemoryClient()
-	 * console.log(client.mode) // 'normal'
-	 * client = createMemoryClient({ forkUrl: 'https://mainnet.infura.io/v3/your-api-key' })
-	 * console.log(client.mode) // 'fork'
-	 * ```
-	 */
-	readonly mode: TMode
-	/**
-	 * Returns promise that resulves when the client is ready
-	 * The client is usable without calling this method but may
-	 * have extra latency on the first call from initialization
-	 * @example
-	 * ```ts
-	 * const client = createMemoryClient()
-	 * await client.ready()
-	 * ```
-	 */
-	readonly ready: () => Promise<true>
-	/**
-	 * Internal instance of the VM. Can be used for lower level operations.
-	 * Normally not recomended to use unless building libraries or extensions
-	 * on top of Tevm.
-	 */
-	readonly getVm: () => Promise<TevmVm>
-	/**
-	 * Gets the pool of pending transactions to be included in next block
-	 */
-	readonly getTxPool: () => Promise<TxPool>
-	/**
-	 * Extends the base client with additional functionality. This enables optimal code splitting
-	 * and extensibility
-	 */
-	readonly extend: <TExtension extends Record<string, any>>(
-		decorator: (client: BaseClient<TMode, TExtended>) => TExtension,
-	) => BaseClient<TMode, TExtended & TExtension>
+  /**
+   * The logger instance
+   */
+  readonly logger: Logger
+  /**
+   * Interface for querying receipts and historical state
+   */
+  readonly getReceiptsManager: () => Promise<ReceiptsManager>
+  /**
+   * The configuration for mining. Defaults to 'auto'
+   * - 'auto' will mine a block on every transaction
+   * - 'interval' will mine a block every `interval` milliseconds
+   * - 'manual' will not mine a block automatically and requires a manual call to `mineBlock`
+   */
+  readonly miningConfig: MiningConfig
+  /**
+   * Fork url if the EVM is forked
+   * @example
+   * ```ts
+   * const client = createMemoryClient({ forkUrl: 'https://mainnet.infura.io/v3/your-api-key' })
+   * console.log(client.forkUrl)
+   * ```
+   */
+  readonly forkUrl?: string | undefined
+  /**
+   * The mode the current client is running in
+   * `fork` mode will fetch and cache all state from the block forked from the provided URL
+   * `normal` mode will not fetch any state and will only run the EVM in memory
+   * @example
+   * ```ts
+   * let client = createMemoryClient()
+   * console.log(client.mode) // 'normal'
+   * client = createMemoryClient({ forkUrl: 'https://mainnet.infura.io/v3/your-api-key' })
+   * console.log(client.mode) // 'fork'
+   * ```
+   */
+  readonly mode: TMode
+  /**
+   * Returns promise that resulves when the client is ready
+   * The client is usable without calling this method but may
+   * have extra latency on the first call from initialization
+   * @example
+   * ```ts
+   * const client = createMemoryClient()
+   * await client.ready()
+   * ```
+   */
+  readonly ready: () => Promise<true>
+  /**
+   * Internal instance of the VM. Can be used for lower level operations.
+   * Normally not recomended to use unless building libraries or extensions
+   * on top of Tevm.
+   */
+  readonly getVm: () => Promise<TevmVm>
+  /**
+   * Gets the pool of pending transactions to be included in next block
+   */
+  readonly getTxPool: () => Promise<TxPool>
+  /**
+   * Extends the base client with additional functionality. This enables optimal code splitting
+   * and extensibility
+   */
+  readonly extend: <TExtension extends Record<string, any>>(
+    decorator: (client: BaseClient<TMode, TExtended>) => TExtension,
+  ) => BaseClient<TMode, TExtended & TExtension>
 } & TExtended
--- packages/base-client/src/createBaseClient.js
+++ packages/base-client/src/createBaseClient.js
@@ -1,6 +1,6 @@
 import { ReceiptsManager, createChain, createMapDb } from '@tevm/blockchain'
 import { Common } from '@tevm/common'
-import {} from '@tevm/errors'
+import { } from '@tevm/errors'
 import { createEvm } from '@tevm/evm'
 import { createLogger } from '@tevm/logger'
 import { createStateManager, dumpCanonicalGenesis, getForkBlockTag } from '@tevm/state'
@@ -11,6 +11,9 @@ import { DEFAULT_CHAIN_ID } from './DEFAULT_CHAIN_ID.js'
 import { addPredeploy } from './addPredeploy.js'
 import { getChainId } from './getChainId.js'
 
+// TODO the common code is not very good and should be moved to common package
+// it has rotted from a previous implementation where the chainId was not used by vm
+
 /**
  * Creates the base instance of a memory client
  * @param {import('./BaseClientOptions.js').BaseClientOptions} [options]
@@ -20,244 +23,215 @@ import { getChainId } from './getChainId.js'
  *  ```
  */
 export const createBaseClient = (options = {}) => {
-	/**
-	 * @type {number|undefined}
-	 */
-	let overrideChainId = undefined
-	/**
-	 * @param {number} id
-	 */
-	const setChainId = (id) => {
-		overrideChainId = id
-	}
-	// First do everything that is not async
-	// Eagerly do async integration
-	// Return proxies that will block on initialization if not yet initialized
-	const common = Common.custom(
-		{
-			name: 'TevmCustom',
-			chainId: 10,
-			networkId: 10,
-		},
-		{
-			hardfork: options.hardfork ?? 'cancun',
-			baseChain: 1,
-			eips: [...(options.eips ?? []), 1559, 4895, 4844, 4788],
-		},
-	)
-	/**
-	 * @returns {import('@tevm/state').StateOptions }
-	 */
-	const getStateManagerOpts = () => {
-		/**
-		 * @type {import('@tevm/state').StateOptions['onCommit']}
-		 */
-		const onCommit = (stateManager) => {
-			if (!options.persister) {
-				throw new Error('No persister provided')
-			}
-			dumpCanonicalGenesis(stateManager)().then((state) => {
-				/**
-				 * @type {import('@tevm/state').ParameterizedTevmState}
-				 */
-				const parsedState = {}
+  /**
+   * @param {number} chainId
+   */
+  const createCommon = (chainId) => {
+    return Common.custom(
+      {
+        name: 'TevmCustom',
+        chainId,
+        networkId: 10,
+      },
+      {
+        hardfork: options.hardfork ?? 'cancun',
+        baseChain: 1,
+        eips: [...(options.eips ?? []), 1559, 4895, 4844, 4788],
+      },
+    )
+  }
 
-				for (const [k, v] of Object.entries(state)) {
-					const { nonce, balance, storageRoot, codeHash } = v
-					parsedState[k] = {
-						...v,
-						nonce: toHex(nonce),
-						balance: toHex(balance),
-						storageRoot: storageRoot,
-						codeHash: codeHash,
-					}
-				}
-				options.persister?.persistTevmState(parsedState)
-			})
-		}
-		if (options.fork?.url) {
-			return {
-				fork: {
-					...options.fork,
-					...(options.persister ? { onCommit } : {}),
-				},
-			}
-		}
-		// handle normal mode
-		if (options.persister) {
-			return {
-				onCommit: /** @type any*/ (onCommit),
-			}
-		}
-		return {}
-	}
-	const stateManager = createStateManager(getStateManagerOpts())
+  /**
+   * @returns {import('@tevm/state').StateOptions }
+   */
+  const getStateManagerOpts = () => {
+    /**
+     * @type {import('@tevm/state').StateOptions['onCommit']}
+     */
+    const onCommit = (stateManager) => {
+      if (!options.persister) {
+        throw new Error('No persister provided')
+      }
+      dumpCanonicalGenesis(stateManager)().then((state) => {
+        /**
+         * @type {import('@tevm/state').ParameterizedTevmState}
+         */
+        const parsedState = {}
 
-	/**
-	 * Create the extend function in a generic way
-	 * @param {import('./BaseClient.js').BaseClient} client
-	 * @returns {import('./BaseClient.js').BaseClient['extend']}
-	 */
-	const extend = (client) => (extension) => {
-		Object.assign(client, extension(client))
-		return /** @type {any}*/ (client)
-	}
+        for (const [k, v] of Object.entries(state)) {
+          const { nonce, balance, storageRoot, codeHash } = v
+          parsedState[k] = {
+            ...v,
+            nonce: toHex(nonce),
+            balance: toHex(balance),
+            storageRoot: storageRoot,
+            codeHash: codeHash,
+          }
+        }
+        options.persister?.persistTevmState(parsedState)
+      })
+    }
+    if (options.fork?.url) {
+      return {
+        fork: {
+          ...options.fork,
+          ...(options.persister ? { onCommit } : {}),
+        },
+      }
+    }
+    // handle normal mode
+    if (options.persister) {
+      return {
+        onCommit: /** @type any*/ (onCommit),
+      }
+    }
+    return {}
+  }
+  const stateManager = createStateManager(getStateManagerOpts())
 
-	const initChainId = async () => {
-		if (options.chainId) {
-			return options.chainId
-		}
-		const url = options.fork?.url
-		if (url) {
-			const id = await getChainId(url)
-			return id
-		}
-		return DEFAULT_CHAIN_ID
-	}
-	const initVm = async () => {
-		// Handle initializing the state from the persisted state
-		/**
-		 * @type {Promise<any>}
-		 */
-		let statePromise = Promise.resolve()
-		if (options.persister) {
-			const restoredState = options.persister.restoreState()
-			if (restoredState) {
-				/**
-				 * @type {import('@tevm/state').TevmState}
-				 */
-				const parsedState = {}
-				for (const [k, v] of Object.entries(restoredState)) {
-					const { nonce, balance, storageRoot, codeHash } = v
-					parsedState[k] = {
-						...v,
-						nonce: hexToBigInt(nonce),
-						balance: hexToBigInt(balance),
-						storageRoot: storageRoot,
-						codeHash: codeHash,
-					}
-				}
-				statePromise = stateManager.generateCanonicalGenesis(parsedState)
-			}
-		}
+  /**
+   * Create the extend function in a generic way
+   * @param {import('./BaseClient.js').BaseClient} client
+   * @returns {import('./BaseClient.js').BaseClient['extend']}
+   */
+  const extend = (client) => (extension) => {
+    Object.assign(client, extension(client))
+    return /** @type {any}*/ (client)
+  }
 
-		const blockTag = (() => {
-			const blockTag = /** @type {import('@tevm/state').StateManager}*/ getForkBlockTag(stateManager) || {
-				blockTag: 'latest',
-			}
-			if ('blockNumber' in blockTag && blockTag.blockNumber !== undefined) {
-				return numberToHex(blockTag.blockNumber)
-			}
-			if ('blockTag' in blockTag && blockTag.blockTag !== undefined) {
-				return blockTag.blockTag
-			}
-			return 'latest'
-		})()
-		const blockchain = await createChain({
-			common,
-			...(options.fork?.url !== undefined
-				? {
-						fork: {
-							url: options.fork.url,
-							blockTag: blockTag.startsWith('0x')
-								? hexToBigInt(/** @type {import('@tevm/utils').Hex}*/ (blockTag))
-								: /** @type {import('@tevm/utils').BlockTag}*/ (blockTag),
-						},
-					}
-				: {}),
-		})
-		const evm = await createEvm({
-			common,
-			stateManager,
-			blockchain,
-			allowUnlimitedContractSize: options.allowUnlimitedContractSize ?? false,
-			customPrecompiles: options.customPrecompiles ?? [],
-			profiler: options.profiler ?? false,
-		})
-		const vm = await createVm({
-			stateManager,
-			evm,
-			blockchain,
-			common,
-		})
+  const initChainId = async () => {
+    if (options.chainId) {
+      return options.chainId
+    }
+    const url = options.fork?.url
+    if (url) {
+      const id = await getChainId(url)
+      return id
+    }
+    return DEFAULT_CHAIN_ID
+  }
 
-		/**
-		 * Add custom predeploys
-		 */
-		if (options.customPredeploys) {
-			await Promise.all(
-				options.customPredeploys.map((predeploy) => {
-					addPredeploy({
-						vm,
-						address: predeploy.address,
-						deployedBytecode: predeploy.contract.deployedBytecode,
-					})
-				}),
-			)
-		}
+  const initVm = async () => {
+    // Handle initializing the state from the persisted state
+    /**
+     * @type {Promise<any>}
+     */
+    let statePromise = Promise.resolve()
+    if (options.persister) {
+      const restoredState = options.persister.restoreState()
+      if (restoredState) {
+        /**
+         * @type {import('@tevm/state').TevmState}
+         */
+        const parsedState = {}
+        for (const [k, v] of Object.entries(restoredState)) {
+          const { nonce, balance, storageRoot, codeHash } = v
+          parsedState[k] = {
+            ...v,
+            nonce: hexToBigInt(nonce),
+            balance: hexToBigInt(balance),
+            storageRoot: storageRoot,
+            codeHash: codeHash,
+          }
+        }
+        statePromise = stateManager.generateCanonicalGenesis(parsedState)
+      }
+    }
 
-		await statePromise
+    const blockTag = (() => {
+      const blockTag = /** @type {import('@tevm/state').StateManager}*/ getForkBlockTag(stateManager) || {
+        blockTag: 'latest',
+      }
+      if ('blockNumber' in blockTag && blockTag.blockNumber !== undefined) {
+        return numberToHex(blockTag.blockNumber)
+      }
+      if ('blockTag' in blockTag && blockTag.blockTag !== undefined) {
+        return blockTag.blockTag
+      }
+      return 'latest'
+    })()
+    const common = createCommon(await initChainId())
+    const blockchain = await createChain({
+      common,
+      ...(options.fork?.url !== undefined
+        ? {
+          fork: {
+            url: options.fork.url,
+            blockTag: blockTag.startsWith('0x')
+              ? hexToBigInt(/** @type {import('@tevm/utils').Hex}*/(blockTag))
+              : /** @type {import('@tevm/utils').BlockTag}*/ (blockTag),
+          },
+        }
+        : {}),
+    })
+    const evm = await createEvm({
+      common,
+      stateManager,
+      blockchain,
+      allowUnlimitedContractSize: options.allowUnlimitedContractSize ?? false,
+      customPrecompiles: options.customPrecompiles ?? [],
+      profiler: options.profiler ?? false,
+    })
+    const vm = await createVm({
+      stateManager,
+      evm,
+      blockchain,
+      common,
+    })
 
-		return vm
-	}
+    /**
+     * Add custom predeploys
+     */
+    if (options.customPredeploys) {
+      await Promise.all(
+        options.customPredeploys.map((predeploy) => {
+          addPredeploy({
+            vm,
+            address: predeploy.address,
+            deployedBytecode: predeploy.contract.deployedBytecode,
+          })
+        }),
+      )
+    }
 
-	const vmPromise = initVm()
-	const txPoolPromise = vmPromise.then((vm) => new TxPool({ vm }))
-	const chainIdPromise = initChainId()
-	const receiptManagerPromise = vmPromise.then((vm) => {
-		return new ReceiptsManager(createMapDb({ cache: new Map() }), vm.blockchain)
-	})
+    await statePromise
 
-	/**
-	 * Create and return the baseClient
-	 * It will be syncronously created but some functionality
-	 * will be asyncronously blocked by initialization of vm and chainId
-	 * @type {import('./BaseClient.js').BaseClient}
-	 */
-	const baseClient = {
-		logger: createLogger({
-			name: 'TevmClient',
-			level: options.loggingLevel ?? 'warn',
-		}),
-		getChain: () => {
-			return vmPromise.then((vm) => vm.blockchain)
-		},
-		getReceiptsManager: () => {
-			return receiptManagerPromise
-		},
-		getTxPool: () => {
-			return txPoolPromise
-		},
-		getChainId: () => {
-			if (overrideChainId) {
-				return Promise.resolve(overrideChainId)
-			}
-			return chainIdPromise
-		},
-		setChainId,
-		getVm: () => vmPromise,
-		miningConfig: options.miningConfig ?? { type: 'auto' },
-		mode: options.fork?.url ? 'fork' : 'normal',
-		...(options.fork?.url ? { forkUrl: options.fork.url } : { forkUrl: options.fork?.url }),
-		extend: (extension) => extend(baseClient)(extension),
-		ready: async () => {
-			const [chainId, vm] = await Promise.allSettled([chainIdPromise, vmPromise])
-			const errors = []
-			if (chainId.status === 'rejected') {
-				errors.push(chainId.reason)
-			}
-			if (vm.status === 'rejected') {
-				errors.push(vm.reason)
-			}
-			if (errors.length > 1) {
-				throw new AggregateError(errors)
-			}
-			if (errors.length === 1) {
-				throw errors[0]
-			}
-			return true
-		},
-	}
+    return vm
+  }
 
-	return baseClient
+  const vmPromise = initVm()
+  const txPoolPromise = vmPromise.then((vm) => new TxPool({ vm }))
+  const receiptManagerPromise = vmPromise.then((vm) => {
+    return new ReceiptsManager(createMapDb({ cache: new Map() }), vm.blockchain)
+  })
+
+  /**
+   * Create and return the baseClient
+   * It will be syncronously created but some functionality
+   * will be asyncronously blocked by initialization of vm and chainId
+   * @type {import('./BaseClient.js').BaseClient}
+   */
+  const baseClient = {
+    logger: createLogger({
+      name: 'TevmClient',
+      level: options.loggingLevel ?? 'warn',
+    }),
+    getReceiptsManager: () => {
+      return receiptManagerPromise
+    },
+    getTxPool: () => {
+      return txPoolPromise
+    },
+    getVm: () => vmPromise,
+    miningConfig: options.miningConfig ?? { type: 'auto' },
+    mode: options.fork?.url ? 'fork' : 'normal',
+    ...(options.fork?.url ? { forkUrl: options.fork.url } : { forkUrl: options.fork?.url }),
+    extend: (extension) => extend(baseClient)(extension),
+    ready: async () => {
+      await vmPromise
+      return true
+    },
+  }
+
+  return baseClient
 }
