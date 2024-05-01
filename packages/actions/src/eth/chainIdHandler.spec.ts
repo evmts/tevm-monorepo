39758f98a298386f29df936d7d8a2113050e5df4
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-04-30 21:03:57 -0700

    :recycle: Fix: broken test

--- packages/actions/src/eth/chainIdHandler.spec.ts
+++ packages/actions/src/eth/chainIdHandler.spec.ts
@@ -1,10 +1,10 @@
 import { describe, expect, it } from 'bun:test'
+import { createBaseClient } from '@tevm/base-client'
 import { blockNumberHandler } from '../index.js'
 import { chainIdHandler } from './chainIdHandler.js'
-import { createBaseClient } from '@tevm/base-client'
 
 describe(blockNumberHandler.name, () => {
 	it('should return the block number', async () => {
-		expect(await chainIdHandler(createBaseClient({fork: {url: 'https://mainnet.optimism.io'}}))({})).toBe(10n)
+		expect(await chainIdHandler(createBaseClient({ fork: { url: 'https://mainnet.optimism.io' } }))({})).toBe(10n)
 	})
 })
--- packages/blockchain/src/createBaseChain.js
+++ packages/blockchain/src/createBaseChain.js
@@ -21,7 +21,7 @@ const KECCAK256_RLP = hexToBytes(KECCAK256_RLP_S)
  * @returns {boolean} true if valid block tag
  */
 const isBlockTag = (blockTag) => {
-  return typeof blockTag === 'string' && ['latest', 'earliest', 'pending', 'safe', 'finalized'].includes(blockTag)
+	return typeof blockTag === 'string' && ['latest', 'earliest', 'pending', 'safe', 'finalized'].includes(blockTag)
 }
 
 // TODO move this to @tevm/block
@@ -32,83 +32,88 @@ const isBlockTag = (blockTag) => {
  * @param {import('@tevm/common').Common} common
  */
 const getBlockFromRpc = async ({ url, blockTag = 'latest' }, common) => {
-  const fetcher = createJsonRpcFetcher(url)
-  /**
-   * @param {import('viem').RpcBlock<'latest', true>} rpcBlock
-   * @returns {Block}
-   */
-  const asEthjsBlock = (rpcBlock) => {
-    return Block.fromRPC({
-      .../** @type {any}*/ (rpcBlock),
-      // filter out transactions we don't support as a hack
-      transactions: rpcBlock.transactions?.filter((tx) => {
-        // we currently don't support optimism deposit tx which uses this custom code
-        // Optimism type is currently not in viem types
-        // @ts-expect-error
-        if (tx.type === '0x7e') {
-          console.warn(
-            `Warning: Optimism deposit transactions (type 0x7e) are currently not supported and will be filtered out of blocks until support is added
+	const fetcher = createJsonRpcFetcher(url)
+	/**
+	 * @param {import('viem').RpcBlock<'latest', true>} rpcBlock
+	 * @returns {Block}
+	 */
+	const asEthjsBlock = (rpcBlock) => {
+		return Block.fromRPC(
+			{
+				.../** @type {any}*/ (rpcBlock),
+				// filter out transactions we don't support as a hack
+				transactions: rpcBlock.transactions?.filter((tx) => {
+					// we currently don't support optimism deposit tx which uses this custom code
+					// Optimism type is currently not in viem types
+					// @ts-expect-error
+					if (tx.type === '0x7e') {
+						console.warn(
+							`Warning: Optimism deposit transactions (type 0x7e) are currently not supported and will be filtered out of blocks until support is added
 filtering out tx ${/** @type {import('viem').RpcBlock}*/ (tx).hash}`,
-          )
-          return false
-        }
-        return true
-      }),
-    }, undefined, { common, setHardfork: false, freeze: true, skipConsensusFormatValidation: true })
-  }
-  // TODO handle errors from fetch better
-  if (typeof blockTag === 'bigint') {
-    const { result, error } = /** @type {{result: import('viem').RpcBlock<'latest', true>, error: {code: number | string, message: string}}}*/ (
-      await fetcher.request({
-        jsonrpc: '2.0',
-        id: 1,
-        method: 'eth_getBlockByNumber',
-        params: [numberToHex(blockTag), true],
-      })
-    )
-    if (error) {
-      throw error
-    }
-    if (!result) {
-      throw new Error('No block found')
-    }
-    return asEthjsBlock(result)
-  }
-  if (typeof blockTag === 'string' && blockTag.startsWith('0x')) {
-    const { result, error } = await fetcher.request({
-      jsonrpc: '2.0',
-      id: 1,
-      method: 'eth_getBlockByHash',
-      params: [blockTag, true],
-    })
-    if (error) {
-      console.error(error)
-      throw error
-    }
-    if (!result) {
-      console.error(error)
-      throw new Error('No block found')
-    }
-    return asEthjsBlock(/** @type {any}*/(result))
-  }
-  if (isBlockTag(blockTag)) {
-    // TODO add an isBlockTag helper
-    const { result, error } = await fetcher.request({
-      jsonrpc: '2.0',
-      id: 1,
-      method: 'eth_getBlockByNumber',
-      params: [blockTag, true],
-    })
-    if (error) {
-      console.error(error)
-      throw error
-    }
-    if (!result) {
-      throw new Error('No block found')
-    }
-    return asEthjsBlock(/** @type {any}*/(result))
-  }
-  throw new Error(`Invalid blocktag ${blockTag}`)
+						)
+						return false
+					}
+					return true
+				}),
+			},
+			undefined,
+			{ common, setHardfork: false, freeze: true, skipConsensusFormatValidation: true },
+		)
+	}
+	// TODO handle errors from fetch better
+	if (typeof blockTag === 'bigint') {
+		const { result, error } =
+			/** @type {{result: import('viem').RpcBlock<'latest', true>, error: {code: number | string, message: string}}}*/ (
+				await fetcher.request({
+					jsonrpc: '2.0',
+					id: 1,
+					method: 'eth_getBlockByNumber',
+					params: [numberToHex(blockTag), true],
+				})
+			)
+		if (error) {
+			throw error
+		}
+		if (!result) {
+			throw new Error('No block found')
+		}
+		return asEthjsBlock(result)
+	}
+	if (typeof blockTag === 'string' && blockTag.startsWith('0x')) {
+		const { result, error } = await fetcher.request({
+			jsonrpc: '2.0',
+			id: 1,
+			method: 'eth_getBlockByHash',
+			params: [blockTag, true],
+		})
+		if (error) {
+			console.error(error)
+			throw error
+		}
+		if (!result) {
+			console.error(error)
+			throw new Error('No block found')
+		}
+		return asEthjsBlock(/** @type {any}*/ (result))
+	}
+	if (isBlockTag(blockTag)) {
+		// TODO add an isBlockTag helper
+		const { result, error } = await fetcher.request({
+			jsonrpc: '2.0',
+			id: 1,
+			method: 'eth_getBlockByNumber',
+			params: [blockTag, true],
+		})
+		if (error) {
+			console.error(error)
+			throw error
+		}
+		if (!result) {
+			throw new Error('No block found')
+		}
+		return asEthjsBlock(/** @type {any}*/ (result))
+	}
+	throw new Error(`Invalid blocktag ${blockTag}`)
 }
 
 /**
@@ -118,52 +123,52 @@ filtering out tx ${/** @type {import('viem').RpcBlock}*/ (tx).hash}`,
  * @returns {Block}
  */
 const createGenesisBlock = (stateRoot, common) => {
-  const newCommon = common.copy()
-  newCommon.setHardforkBy({
-    blockNumber: 0,
-    td: BigInt(newCommon.genesis().difficulty),
-    timestamp: newCommon.genesis().timestamp ?? 0,
-  })
+	const newCommon = common.copy()
+	newCommon.setHardforkBy({
+		blockNumber: 0,
+		td: BigInt(newCommon.genesis().difficulty),
+		timestamp: newCommon.genesis().timestamp ?? 0,
+	})
 
-  /**
-   * @type {import('@tevm/block').HeaderData}
-   */
-  const header = {
-    ...newCommon.genesis(),
-    number: 0,
-    stateRoot,
-    ...(newCommon.isActivatedEIP(4895) ? { withdrawalsRoot: KECCAK256_RLP } : {}),
-  }
-  return Block.fromBlockData({ header, ...(newCommon.isActivatedEIP(4895) ? { withdrawals: [] } : {}) }, { common })
+	/**
+	 * @type {import('@tevm/block').HeaderData}
+	 */
+	const header = {
+		...newCommon.genesis(),
+		number: 0,
+		stateRoot,
+		...(newCommon.isActivatedEIP(4895) ? { withdrawalsRoot: KECCAK256_RLP } : {}),
+	}
+	return Block.fromBlockData({ header, ...(newCommon.isActivatedEIP(4895) ? { withdrawals: [] } : {}) }, { common })
 }
 /**
  * @param {import('./ChainOptions.js').ChainOptions} options
  * @returns {import('./BaseChain.js').BaseChain} Base chain object
  */
 export const createBaseChain = (options) => {
-  /**
-   * @type {import('./BaseChain.js').BaseChain}
-   */
-  const chain = {
-    options,
-    common: options.common,
-    blocks: new Map(),
-    blocksByTag: new Map(),
-    blocksByNumber: new Map(),
-    ready: () => genesisBlockPromise.then(() => true),
-  }
+	/**
+	 * @type {import('./BaseChain.js').BaseChain}
+	 */
+	const chain = {
+		options,
+		common: options.common,
+		blocks: new Map(),
+		blocksByTag: new Map(),
+		blocksByNumber: new Map(),
+		ready: () => genesisBlockPromise.then(() => true),
+	}
 
-  // Add genesis block and forked block to chain
-  const genesisBlockPromise = (async () => {
-    if (options.fork?.url) {
-      const block = await getBlockFromRpc(options.fork, options.common)
-      await putBlock(chain)(block)
-    } else {
-      await putBlock(chain)(
-        options.genesisBlock ?? createGenesisBlock(options.genesisStateRoot ?? EMPTY_STATE_ROOT, options.common),
-      )
-    }
-  })()
+	// Add genesis block and forked block to chain
+	const genesisBlockPromise = (async () => {
+		if (options.fork?.url) {
+			const block = await getBlockFromRpc(options.fork, options.common)
+			await putBlock(chain)(block)
+		} else {
+			await putBlock(chain)(
+				options.genesisBlock ?? createGenesisBlock(options.genesisStateRoot ?? EMPTY_STATE_ROOT, options.common),
+			)
+		}
+	})()
 
-  return chain
+	return chain
 }
