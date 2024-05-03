8f8603e9420497aceec7797e1e5ef085f1ae058d
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-03 13:07:07 -0700

    :bug: Fix: bug with using vm.shallowCopy instead of deepCopy

--- packages/vm/src/Vm.ts
+++ packages/vm/src/Vm.ts
@@ -1,10 +1,9 @@
 import type { BaseVm } from "./BaseVm.js"
 import type { BuildBlock } from "./actions/buildBlock.js"
-import type { DeepCopy } from "./actions/deepCopy.js"
 import type { RunBlock } from "./actions/runBlock.js"
 
 export type Vm = BaseVm & {
-  deepCopy: DeepCopy
+  deepCopy: () => Promise<Vm>
   buildBlock: BuildBlock
   runBlock: RunBlock
 }
