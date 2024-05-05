048dd670cb90eaa692b2965a65decb03e7180c41
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-05 12:49:30 -0700

    :bug: Fix: commit to different or same state root

--- packages/state/src/actions/commit.js
+++ packages/state/src/actions/commit.js
@@ -9,32 +9,36 @@ import { saveStateRoot } from './saveStateRoot.js'
  * last call to checkpoint.
  * @type {import("../state-types/index.js").StateAction<'commit'>}
  */
-export const commit = (baseState) => async () => {
-	baseState._caches.accounts.commit()
-	baseState._caches.storage.commit()
-	baseState._caches.contracts.commit()
-	// This is kinda hacky we are just ranodmly generating new state roots
-	// since we don't use a trie we are kinda hacking this
-	const state = await dumpCanonicalGenesis(baseState)()
-	/**
-	 * @type {import('@tevm/state').ParameterizedTevmState}
-	 */
-	const jsonSerializableState = {}
+export const commit =
+	(baseState) =>
+	async (createNewStateRoot = true) => {
+		baseState._caches.accounts.commit()
+		baseState._caches.storage.commit()
+		baseState._caches.contracts.commit()
+		// This is kinda hacky we are just ranodmly generating new state roots
+		// since we don't use a trie we are kinda hacking this
+		const state = await dumpCanonicalGenesis(baseState)()
+		/**
+		 * @type {import('@tevm/state').ParameterizedTevmState}
+		 */
+		const jsonSerializableState = {}
 
-	// This might be needlessy slow as state gets big but for now it feels the most rubust wrt correctness
-	for (const [k, v] of Object.entries(state)) {
-		const { nonce, balance, storageRoot, codeHash } = v
-		jsonSerializableState[k] = {
-			...v,
-			nonce: numberToHex(nonce),
-			balance: numberToHex(balance),
-			storageRoot: storageRoot,
-			codeHash: codeHash,
+		// This might be needlessy slow as state gets big but for now it feels the most rubust wrt correctness
+		for (const [k, v] of Object.entries(state)) {
+			const { nonce, balance, storageRoot, codeHash } = v
+			jsonSerializableState[k] = {
+				...v,
+				nonce: numberToHex(nonce),
+				balance: numberToHex(balance),
+				storageRoot: storageRoot,
+				codeHash: codeHash,
+			}
 		}
+		const newStateRoot = createNewStateRoot
+			? keccak256(toHex(JSON.stringify(jsonSerializableState)), 'bytes')
+			: baseState._currentStateRoot
+		saveStateRoot(baseState)(newStateRoot, state)
+		baseState._currentStateRoot = newStateRoot
+		baseState._options.onCommit?.(baseState)
+		return Promise.resolve()
 	}
-	const newStateRoot = keccak256(toHex(JSON.stringify(jsonSerializableState)), 'bytes')
-	saveStateRoot(baseState)(newStateRoot, state)
-	baseState._currentStateRoot = newStateRoot
-	baseState._options.onCommit?.(baseState)
-	return Promise.resolve()
-}
--- packages/state/src/actions/putAccount.js
+++ packages/state/src/actions/putAccount.js
@@ -2,12 +2,10 @@
  * Saves an account into state under the provided `address`.
  * @type {import("../state-types/index.js").StateAction<'putAccount'>}
  */
-export const putAccount =
-	({ _caches: { accounts } }) =>
-	async (address, account) => {
-		if (account !== undefined) {
-			accounts?.put(address, account)
-		} else {
-			accounts?.del(address)
-		}
+export const putAccount = (baseState) => async (address, account) => {
+	if (account !== undefined) {
+		baseState._caches.accounts?.put(address, account)
+	} else {
+		baseState._caches.accounts?.del(address)
 	}
+}
