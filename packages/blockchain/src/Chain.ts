66c7e039df01b2f4d6fc89182811cb8e5a1d3753
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-09 20:43:10 -0700

    :bug: Fix: call foo.ready() in places it needs to be called

--- packages/blockchain/src/Chain.ts
+++ packages/blockchain/src/Chain.ts
@@ -14,78 +14,78 @@ shallowCopy
  * Blockchain
  */
 export type Chain = {} & BaseChain & {
-		// for type compatability we must also implement some unused methods
-		/**
-		 * Returns a shallow copy of the blockchain that may share state with the original
-		 */
-		shallowCopy: () => Chain
-		deepCopy: () => Chain
-		consensus: Consensus
-		/**
-		 * Adds a block to the blockchain.
-		 *
-		 * @param block - The block to be added to the blockchain.
-		 */
-		putBlock(block: Block): Promise<void>
+  // for type compatability we must also implement some unused methods
+  /**
+   * Returns a shallow copy of the blockchain that may share state with the original
+   */
+  shallowCopy: () => Chain
+  deepCopy: () => Promise<Chain>
+  consensus: Consensus
+  /**
+   * Adds a block to the blockchain.
+   *
+   * @param block - The block to be added to the blockchain.
+   */
+  putBlock(block: Block): Promise<void>
 
-		/**
-		 * Deletes a block from the blockchain. All child blocks in the chain are
-		 * deleted and any encountered heads are set to the parent block.
-		 *
-		 * @param blockHash - The hash of the block to be deleted
-		 */
-		delBlock(blockHash: Uint8Array): Promise<void>
+  /**
+   * Deletes a block from the blockchain. All child blocks in the chain are
+   * deleted and any encountered heads are set to the parent block.
+   *
+   * @param blockHash - The hash of the block to be deleted
+   */
+  delBlock(blockHash: Uint8Array): Promise<void>
 
-		/**
-		 * Returns a block by its hash or number.
-		 */
-		getBlock(blockId: Uint8Array | number | bigint): Promise<Block>
+  /**
+   * Returns a block by its hash or number.
+   */
+  getBlock(blockId: Uint8Array | number | bigint): Promise<Block>
 
-		/**
-		 * Iterates through blocks starting at the specified iterator head and calls
-		 * the onBlock function on each block.
-		 *
-		 * @param name - Name of the state root head
-		 * @param onBlock - Function called on each block with params (block: Block,
-		 * @param maxBlocks - optional maximum number of blocks to iterate through
-		 * reorg: boolean)
-		 */
-		iterator(name: string, onBlock: OnBlock, maxBlocks?: number, releaseLockOnCallback?: boolean): Promise<number>
+  /**
+   * Iterates through blocks starting at the specified iterator head and calls
+   * the onBlock function on each block.
+   *
+   * @param name - Name of the state root head
+   * @param onBlock - Function called on each block with params (block: Block,
+   * @param maxBlocks - optional maximum number of blocks to iterate through
+   * reorg: boolean)
+   */
+  iterator(name: string, onBlock: OnBlock, maxBlocks?: number, releaseLockOnCallback?: boolean): Promise<number>
 
-		/**
-		 * Validates a block header, throwing if invalid. It is being validated against the reported `parentHash`.
-		 * @param header - header to be validated
-		 * @param height - If this is an uncle header, this is the height of the block that is including it
-		 */
-		validateHeader(header: BlockHeader, height?: bigint): Promise<void>
+  /**
+   * Validates a block header, throwing if invalid. It is being validated against the reported `parentHash`.
+   * @param header - header to be validated
+   * @param height - If this is an uncle header, this is the height of the block that is including it
+   */
+  validateHeader(header: BlockHeader, height?: bigint): Promise<void>
 
-		/**
-		 * Returns the specified iterator head.
-		 *
-		 * @param name - Optional name of the iterator head (default: 'vm')
-		 */
-		getIteratorHead(name?: string): Promise<Block>
+  /**
+   * Returns the specified iterator head.
+   *
+   * @param name - Optional name of the iterator head (default: 'vm')
+   */
+  getIteratorHead(name?: string): Promise<Block>
 
-		/**
-		 * Set header hash of a certain `tag`.
-		 * When calling the iterator, the iterator will start running the first child block after the header hash currently stored.
-		 * @param tag - The tag to save the headHash to
-		 * @param headHash - The head hash to save
-		 */
-		setIteratorHead(tag: string, headHash: Uint8Array): Promise<void>
+  /**
+   * Set header hash of a certain `tag`.
+   * When calling the iterator, the iterator will start running the first child block after the header hash currently stored.
+   * @param tag - The tag to save the headHash to
+   * @param headHash - The head hash to save
+   */
+  setIteratorHead(tag: string, headHash: Uint8Array): Promise<void>
 
-		/**
-		 * Gets total difficulty for a block specified by hash and number
-		 */
-		getTotalDifficulty?(hash: Uint8Array, number?: bigint): Promise<bigint>
+  /**
+   * Gets total difficulty for a block specified by hash and number
+   */
+  getTotalDifficulty?(hash: Uint8Array, number?: bigint): Promise<bigint>
 
-		/**
-		 * Returns the latest full block in the canonical chain.
-		 */
-		getCanonicalHeadBlock(): Promise<Block>
+  /**
+   * Returns the latest full block in the canonical chain.
+   */
+  getCanonicalHeadBlock(): Promise<Block>
 
-		/**
-		 * Optional events emitter
-		 */
-		events?: AsyncEventEmitter<BlockchainEvents>
+  /**
+   * Optional events emitter
+   */
+  events?: AsyncEventEmitter<BlockchainEvents>
 	}
--- packages/blockchain/src/actions/deepCopy.js
+++ packages/blockchain/src/actions/deepCopy.js
@@ -2,14 +2,15 @@ import { createBaseChain } from '../createBaseChain.js'
 
 /**
  * @param {import('../BaseChain.js').BaseChain} baseChain
- * @returns {() => import('../BaseChain.js').BaseChain}
+ * @returns {() => Promise<import('../BaseChain.js').BaseChain>}
  */
-export const deepCopy = (baseChain) => () => {
-	const chain = createBaseChain({
-		common: baseChain.common.copy(),
-	})
-	chain.blocksByTag = new Map(baseChain.blocksByTag.entries())
-	chain.blocks = new Map(baseChain.blocks.entries())
-	chain.blocksByNumber = new Map(baseChain.blocksByNumber.entries())
-	return chain
+export const deepCopy = (baseChain) => async () => {
+  await baseChain.ready()
+  const chain = createBaseChain({
+    common: baseChain.common.copy(),
+  })
+  chain.blocksByTag = new Map(baseChain.blocksByTag.entries())
+  chain.blocks = new Map(baseChain.blocks.entries())
+  chain.blocksByNumber = new Map(baseChain.blocksByNumber.entries())
+  return chain
 }
