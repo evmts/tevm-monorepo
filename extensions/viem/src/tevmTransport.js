e418e029536fbff87444042a63cdbe8945a8c868
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-11 09:17:58 -0700

    :clown_face: Fix: Add longer timeouts to flaky viem tests

--- extensions/viem/src/tevmTransport.js
+++ extensions/viem/src/tevmTransport.js
@@ -2,16 +2,19 @@ import { createTransport } from 'viem'
 
 /**
  * @param {Pick<import('@tevm/memory-client').MemoryClient, 'request'>} tevm The Tevm instance
- * @param {Pick<import('viem').TransportConfig, 'name' | 'key'>} [options]
+ * @param {Pick<import('viem').TransportConfig, 'name' | 'key' | 'timeout' | 'retryCount' | 'retryDelay'>} [options]
  * @returns {import('viem').Transport} The transport function
  */
 export const tevmTransport = ({ request }, options) => {
-	return () => {
-		return createTransport({
-			request: /** @type any*/ (request),
-			type: 'tevm',
-			name: options?.name ?? 'Tevm transport',
-			key: options?.key ?? 'tevm',
-		})
-	}
+  return () => {
+    return createTransport({
+      request: /** @type any*/ (request),
+      type: 'tevm',
+      name: options?.name ?? 'Tevm transport',
+      key: options?.key ?? 'tevm',
+      timeout: options?.timeout ?? 20_000,
+      retryCount: options?.retryCount ?? 3,
+      retryDelay: options?.retryDelay ?? 150,
+    })
+  }
 }
