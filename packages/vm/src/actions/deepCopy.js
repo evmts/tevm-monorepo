231cd0daf5c0d3351f4b4224504d635a6464c7c8
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-11 08:06:40 -0700

    :bug: Fix: bug with not deep copying logging config in evm

--- packages/vm/src/actions/deepCopy.js
+++ packages/vm/src/actions/deepCopy.js
@@ -10,29 +10,33 @@ import { createBaseVm } from '../createBaseVm.js'
  * @returns {DeepCopy}
  */
 export const deepCopy = (baseVm) => async () => {
-	await baseVm.ready()
-	const common = baseVm.common.copy()
-	common.setHardfork(baseVm.common.hardfork())
-	const blockchain = await baseVm.blockchain.deepCopy()
-	if (!('deepCopy' in baseVm.stateManager)) {
-		throw new Error('StateManager does not support deepCopy. Was a Tevm state manager used?')
-	}
-	const stateManager = await baseVm.stateManager.deepCopy()
-	const evmCopy = await createEvm({
-		blockchain,
-		common,
-		stateManager,
-		allowUnlimitedContractSize: baseVm.evm.allowUnlimitedContractSize ?? false,
-		customPrecompiles: /** @type {any} */ (baseVm.evm)._customPrecompiles,
-		// customPredeploys isn't needed because it will be copied along in stateManager.deepCopy
-		// customPredeploys,
-		profiler: Boolean(/** @type {any} */ (baseVm.evm).optsCached?.profiler?.enabled) ?? false,
-	})
-	return createBaseVm({
-		stateManager,
-		blockchain: baseVm.blockchain,
-		activatePrecompiles: true,
-		common,
-		evm: evmCopy,
-	})
+  await baseVm.ready()
+  const common = baseVm.common.copy()
+  common.setHardfork(baseVm.common.hardfork())
+  const blockchain = await baseVm.blockchain.deepCopy()
+  if (!('deepCopy' in baseVm.stateManager)) {
+    throw new Error('StateManager does not support deepCopy. Was a Tevm state manager used?')
+  }
+  const stateManager = await baseVm.stateManager.deepCopy()
+  const evmCopy = await createEvm({
+    blockchain,
+    common,
+    stateManager,
+    allowUnlimitedContractSize: baseVm.evm.allowUnlimitedContractSize ?? false,
+    customPrecompiles: /** @type {any} */ (baseVm.evm)._customPrecompiles,
+    // customPredeploys isn't needed because it will be copied along in stateManager.deepCopy
+    // customPredeploys,
+    profiler: Boolean(/** @type {any} */(baseVm.evm).optsCached?.profiler?.enabled) ?? false,
+  })
+  // we are hacking ethereumjs logger into working with our logger
+  const evmAny = /** @type {any}*/(evmCopy)
+  evmAny.DEBUG = baseVm.evm.DEBUG
+  evmAny._debug = /** @type any*/(baseVm.evm)._debug
+  return createBaseVm({
+    stateManager,
+    blockchain: baseVm.blockchain,
+    activatePrecompiles: true,
+    common,
+    evm: evmCopy,
+  })
 }
