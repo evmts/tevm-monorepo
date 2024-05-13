75179456c7b4aa472c3de99edeed69e7f3fe9825
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-13 15:35:22 -0700

    :recycle: Regenerate

--- packages/precompiles/test/Fs.spec.ts
+++ packages/precompiles/test/Fs.spec.ts
@@ -5,42 +5,42 @@ import { existsSync, rmSync } from 'node:fs'
 import { fsPrecompile } from './FsPrecompile.js'
 
 test('Call precompile from TypeScript', async () => {
-  const client = createMemoryClient({
-    customPrecompiles: [fsPrecompile.precompile()],
-  })
+	const client = createMemoryClient({
+		customPrecompiles: [fsPrecompile.precompile()],
+	})
 
-  const result = await client.tevmContract(fsPrecompile.contract.write.writeFile('test1.txt', 'hello world'))
+	const result = await client.tevmContract(fsPrecompile.contract.write.writeFile('test1.txt', 'hello world'))
 
-  expect(result.errors).toBeUndefined()
+	expect(result.errors).toBeUndefined()
 
-  expect(existsSync('test1.txt')).toBe(true)
-  expect(
-    (
-      await client.tevmContract({
-        ...fsPrecompile.contract.read.readFile('test1.txt'),
-      })
-    ).data,
-  ).toBe('hello world')
+	expect(existsSync('test1.txt')).toBe(true)
+	expect(
+		(
+			await client.tevmContract({
+				...fsPrecompile.contract.read.readFile('test1.txt'),
+			})
+		).data,
+	).toBe('hello world')
 
-  rmSync('test1.txt')
+	rmSync('test1.txt')
 })
 
 test('Call precompile from solidity script', async () => {
-  const { WriteHelloWorld } = await import('./WriteHelloWorld.s.sol')
+	const { WriteHelloWorld } = await import('./WriteHelloWorld.s.sol')
 
-  const client = createMemoryClient({
-    customPrecompiles: [fsPrecompile.precompile()],
-    loggingLevel: 'trace',
-  })
+	const client = createMemoryClient({
+		customPrecompiles: [fsPrecompile.precompile()],
+		loggingLevel: 'trace',
+	})
 
-  const result = await client.tevmScript({
-    ...WriteHelloWorld.write.write(fsPrecompile.contract.address),
-    throwOnFail: false,
-  })
+	const result = await client.tevmScript({
+		...WriteHelloWorld.write.write(fsPrecompile.contract.address),
+		throwOnFail: false,
+	})
 
-  expect(existsSync('test.txt')).toBe(true)
+	expect(existsSync('test.txt')).toBe(true)
 
-  rmSync('test.txt')
+	rmSync('test.txt')
 
-  expect(result.errors).toBeUndefined()
+	expect(result.errors).toBeUndefined()
 })
