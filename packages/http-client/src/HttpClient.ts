eefb0dbf29df8ee199d4e54ba56fb10eb55dfc3a
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-13 15:34:58 -0700

    :wastebasket: Deprecate http client

--- packages/http-client/src/HttpClient.ts
+++ packages/http-client/src/HttpClient.ts
@@ -1,21 +1,15 @@
 import type { TevmClient } from '@tevm/client-types'
 
 /**
- * A remote Tevm client for talking to a Tevm backend over HTTP JSON-RPC
- * Implements the tevm interface so interacting with it is the same api
- * as interacting with a `MemoryTevm` instance directly
- * @see {@link TevmClient}
- * @example
- * ```typescript
- * import { TevmClient, createTevmClient } from "tevm/client";
+ * @deprecated a new http client will be created in a future version. For now it's recomended to use viem
  */
 export type HttpClient = TevmClient & {
-	/**
-	 * The url being used to connect to the remote Tevm backend
-	 */
-	url: string
-	/**
-	 * Name of the client
-	 */
-	name: string
+  /**
+   * The url being used to connect to the remote Tevm backend
+   */
+  url: string
+  /**
+   * Name of the client
+   */
+  name: string
 }
--- packages/http-client/src/HttpClientOptions.ts
+++ packages/http-client/src/HttpClientOptions.ts
@@ -1,13 +1,14 @@
 /**
+ * @deprecated
  * Options for a HttpClient
  */
 export type HttpClientOptions = {
-	/**
-	 * Remote URL to connect to
-	 */
-	readonly url: string
-	/**
-	 * Optional name for the client
-	 */
-	readonly name?: string
+  /**
+   * Remote URL to connect to
+   */
+  readonly url: string
+  /**
+   * Optional name for the client
+   */
+  readonly name?: string
 }
