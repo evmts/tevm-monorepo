d0962f7613ceb1397a5beb3a11599fdb403ab55c
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-13 15:01:45 -0700

    :recycle: Fix: bench test errors

--- test/bench/src/deployment/deployContract.ts
+++ test/bench/src/deployment/deployContract.ts
@@ -16,30 +16,30 @@ const account0 = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
  * This is instructive but I would likely use viem `deployContract` (next example) over this
  */
 export const deployContractWithCall: ContractDeployment = async (initialValue) => {
-	const client = createMemoryClient()
+  const client = createMemoryClient()
 
-	// manually call the constructor function
-	const deployResult = await client.call({
-		data: encodeDeployData({
-			abi: SimpleConstructor.abi,
-			bytecode: SimpleConstructor.bytecode,
-			args: [initialValue],
-		}),
-		createTransaction: true,
-		from: account0,
-	})
+  // manually call the constructor function
+  const deployResult = await client.tevmCall({
+    data: encodeDeployData({
+      abi: SimpleConstructor.abi,
+      bytecode: SimpleConstructor.bytecode,
+      args: [initialValue],
+    }),
+    createTransaction: true,
+    from: account0,
+  })
 
-	if (!deployResult.createdAddress) {
-		throw new Error('Did not create any addresses')
-	}
-	// check that correctly set
-	const { data } = await client.contract({
-		abi: SimpleConstructor.abi,
-		functionName: 'get',
-		args: [],
-		to: deployResult.createdAddress,
-	})
-	return data
+  if (!deployResult.createdAddress) {
+    throw new Error('Did not create any addresses')
+  }
+  // check that correctly set
+  const { data } = await client.tevmContract({
+    abi: SimpleConstructor.abi,
+    functionName: 'get',
+    args: [],
+    to: deployResult.createdAddress,
+  })
+  return data
 }
 
 /**
@@ -47,33 +47,33 @@ export const deployContractWithCall: ContractDeployment = async (initialValue) =
  * which is to simply use viem `deployContract`
  */
 export const deployContractWithViem: ContractDeployment = async (initialValue) => {
-	const tevmClient = createMemoryClient()
-	const walletClient = createWalletClient({
-		// TODO add chain property to tevm instance
-		chain: { ...anvil, id: 900 },
-		// TODO fix this type not matching because of viem versioning issue
-		transport: tevmTransport(tevmClient) as any,
-		account: privateKeyToAccount('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'),
-	})
+  const tevmClient = createMemoryClient()
+  const walletClient = createWalletClient({
+    // TODO add chain property to tevm instance
+    chain: { ...anvil, id: 900 },
+    // TODO fix this type not matching because of viem versioning issue
+    transport: tevmTransport(tevmClient) as any,
+    account: privateKeyToAccount('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'),
+  })
 
-	const hash = await walletClient.deployContract({
-		abi: SimpleConstructor.abi,
-		bytecode: SimpleConstructor.bytecode,
-		args: [initialValue],
-	})
+  const hash = await walletClient.deployContract({
+    abi: SimpleConstructor.abi,
+    bytecode: SimpleConstructor.bytecode,
+    args: [initialValue],
+  })
 
-	const publicClient = createPublicClient({
-		// TODO add chain property to tevm instance
-		chain: { ...anvil, id: 900 },
-		// TODO fix this type not matching because of viem versioning issue
-		transport: tevmTransport(tevmClient) as any,
-	})
+  const publicClient = createPublicClient({
+    // TODO add chain property to tevm instance
+    chain: { ...anvil, id: 900 },
+    // TODO fix this type not matching because of viem versioning issue
+    transport: tevmTransport(tevmClient) as any,
+  })
 
-	const receipt = await publicClient.waitForTransactionReceipt({ hash })
+  const receipt = await publicClient.waitForTransactionReceipt({ hash })
 
-	return publicClient.readContract({
-		abi: SimpleConstructor.abi,
-		functionName: 'get',
-		address: receipt.contractAddress,
-	})
+  return publicClient.readContract({
+    abi: SimpleConstructor.abi,
+    functionName: 'get',
+    address: receipt.contractAddress,
+  })
 }
--- test/bench/src/pureComputation/pureComputation.ts
+++ test/bench/src/pureComputation/pureComputation.ts
@@ -8,13 +8,13 @@ export const tevm = createMemoryClient()
  * Tests fib sequence in solidity
  */
 export const pureComputationSol = async (i: bigint): Promise<bigint> => {
-	return tevm.script(Fibonacci.read.calculate(i)).then((res) => res.data) as Promise<bigint>
+  return tevm.tevmScript(Fibonacci.read.calculate(i)).then((res) => res.data) as Promise<bigint>
 }
 /**
  * Tests fib sequence in ts
  *
  */
 export const pureComputationTs = (i: bigint): bigint => {
-	const fib = new FibTs()
-	return fib.calculate(i)
+  const fib = new FibTs()
+  return fib.calculate(i)
 }
