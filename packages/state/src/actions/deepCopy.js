53a649dfca7c245f490e41fe7c052290b570d8e6
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-14 20:24:57 -0700

    :recycle: Fix: Clean up deep copy

--- packages/state/src/actions/deepCopy.js
+++ packages/state/src/actions/deepCopy.js
@@ -1,6 +1,5 @@
 import { createBaseState } from '../createBaseState.js'
 import { dumpCanonicalGenesis } from './dumpCannonicalGenesis.js'
-import { generateCanonicalGenesis } from './generateCannonicalGenesis.js'
 
 /**
  * Returns a new instance of the ForkStateManager with the same opts and all storage copied over
@@ -18,13 +17,11 @@ export const deepCopy = (baseState) => async () => {
 	}
 	const newState = createBaseState({
 		...baseState.options,
+		genesisState: await dumpCanonicalGenesis(baseState)(),
+		stateRoots: new Map(baseState.stateRoots),
+		currentStateRoot: baseState.getCurrentStateRoot(),
 	})
 	await newState.ready()
-	const currentState = await dumpCanonicalGenesis(baseState)()
-	baseState.logger.debug(currentState, 'Deep copying state...')
-	await generateCanonicalGenesis(newState)(currentState)
-	newState.stateRoots.set(baseState.getCurrentStateRoot(), currentState)
-	newState.setCurrentStateRoot(baseState.getCurrentStateRoot())
 	baseState.logger.debug("Successfully deepCopy'd StateManager")
 	return newState
 }
