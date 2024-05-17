bf5ab0ac2f5c62dcffbce68a0fa5a9befe301c8a
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-16 19:42:04 -0700

    :bug: Implement general block support for eth_getBalance

--- packages/actions/docs/functions/getBalanceHandler.md
+++ packages/actions/docs/functions/getBalanceHandler.md
@@ -6,15 +6,80 @@
 
 # Function: getBalanceHandler()
 
-> **getBalanceHandler**(`options`): `EthGetBalanceHandler`
+> **getBalanceHandler**(`baseClient`): `EthGetBalanceHandler`
 
 ## Parameters
 
-• **options**
+• **baseClient**
 
-• **options.forkUrl**: `undefined` \| `string`
+• **baseClient.extend**
 
-• **options.getVm**
+Extends the base client with additional functionality. This enables optimal code splitting
+and extensibility
+
+• **baseClient.forkUrl?**: `string`
+
+Fork url if the EVM is forked
+
+**Example**
+
+```ts
+const client = createMemoryClient({ forkUrl: 'https://mainnet.infura.io/v3/your-api-key' })
+console.log(client.forkUrl)
+```
+
+• **baseClient.getReceiptsManager**
+
+Interface for querying receipts and historical state
+
+• **baseClient.getTxPool**
+
+Gets the pool of pending transactions to be included in next block
+
+• **baseClient.getVm**
+
+Internal instance of the VM. Can be used for lower level operations.
+Normally not recomended to use unless building libraries or extensions
+on top of Tevm.
+
+• **baseClient.logger**: `Logger`
+
+The logger instance
+
+• **baseClient.miningConfig**: `MiningConfig`
+
+The configuration for mining. Defaults to 'auto'
+- 'auto' will mine a block on every transaction
+- 'interval' will mine a block every `interval` milliseconds
+- 'manual' will not mine a block automatically and requires a manual call to `mineBlock`
+
+• **baseClient.mode**: `"fork"` \| `"normal"`
+
+The mode the current client is running in
+`fork` mode will fetch and cache all state from the block forked from the provided URL
+`normal` mode will not fetch any state and will only run the EVM in memory
+
+**Example**
+
+```ts
+let client = createMemoryClient()
+console.log(client.mode) // 'normal'
+client = createMemoryClient({ forkUrl: 'https://mainnet.infura.io/v3/your-api-key' })
+console.log(client.mode) // 'fork'
+```
+
+• **baseClient.ready**
+
+Returns promise that resulves when the client is ready
+The client is usable without calling this method but may
+have extra latency on the first call from initialization
+
+**Example**
+
+```ts
+const client = createMemoryClient()
+await client.ready()
+```
 
 ## Returns
 
@@ -22,4 +87,4 @@
 
 ## Source
 
-[packages/actions/src/eth/getBalanceHandler.js:24](https://github.com/evmts/tevm-monorepo/blob/main/packages/actions/src/eth/getBalanceHandler.js#L24)
+[packages/actions/src/eth/getBalanceHandler.js:21](https://github.com/evmts/tevm-monorepo/blob/main/packages/actions/src/eth/getBalanceHandler.js#L21)
--- packages/actions/src/eth/getBalanceHandler.spec.ts
+++ packages/actions/src/eth/getBalanceHandler.spec.ts
@@ -1,63 +1,14 @@
-import { describe, expect, it, jest } from 'bun:test'
-import { getAlchemyUrl } from '@tevm/test-utils'
-import { EthjsAddress } from '@tevm/utils'
+import { describe, expect, it } from 'bun:test'
+import { createBaseClient } from '@tevm/base-client'
+import { type Address, EthjsAddress } from '@tevm/utils'
+import { setAccountHandler } from '../tevm/setAccountHandler.js'
 import { getBalanceHandler } from './getBalanceHandler.js'
 
 describe(getBalanceHandler.name, () => {
-	it('should fetch balance from state manager if tag is not defined defaulting the tag to `pending`', async () => {
-		const stateManager = {
-			getAccount: jest.fn(),
-		}
-		stateManager.getAccount.mockResolvedValueOnce({ balance: 420n })
+	it('should fetch balance from state manager if tag is not defined defaulting the tag to `latest`', async () => {
+		const baseClient = createBaseClient()
 		const address = EthjsAddress.zero().toString() as `0x${string}`
-		expect(
-			await getBalanceHandler({
-				getVm: async () => ({ stateManager }) as any,
-			})({ address }),
-		).toEqual(420n)
-		expect(stateManager.getAccount).toHaveBeenCalledWith(EthjsAddress.fromString(address))
-	})
-
-	it('should fetch balance from state manager if tag is `pending`', async () => {
-		const stateManager = {
-			getAccount: jest.fn(),
-		}
-		stateManager.getAccount.mockResolvedValueOnce({ balance: 420n })
-		const address = EthjsAddress.zero().toString() as `0x${string}`
-		expect(
-			await getBalanceHandler({
-				getVm: () => ({ stateManager }) as any,
-			})({ address, blockTag: 'pending' }),
-		).toEqual(420n)
-		expect(stateManager.getAccount).toHaveBeenCalledWith(EthjsAddress.fromString(address))
-	})
-
-	it('should throw an error if tag is not pending and no forkUrl is set', async () => {
-		const stateManager = {
-			getAccount: jest.fn(),
-		}
-		stateManager.getAccount.mockResolvedValueOnce({ balance: 420n })
-		const address = EthjsAddress.zero().toString() as `0x${string}`
-		expect(
-			await getBalanceHandler({
-				getVm: () => ({ stateManager }) as any,
-			})({ address, blockTag: 'latest' }).catch((e) => (e as any)._tag),
-		).toEqual('NoForkUrlSetError')
-	})
-
-	// this passed until free rpc tier endpoint started failing
-	it.todo('should fetch from provider if fetching a historical block', async () => {
-		const stateManager = {
-			getAccount: jest.fn(),
-		}
-		stateManager.getAccount.mockResolvedValueOnce({ balance: 420n })
-		const address = '0xa0b0660b498d0a7ce193dd6632bf7e9126168a3d' as const
-		const blockNumber = 114830382n
-		expect(
-			await getBalanceHandler({
-				getVm: async () => ({ stateManager }) as any,
-				forkUrl: getAlchemyUrl(),
-			})({ address, blockTag: blockNumber }),
-		).toEqual(5536669375141759n)
+		await setAccountHandler(baseClient)({ address: EthjsAddress.zero().toString() as Address, balance: 420n })
+		expect(await getBalanceHandler(baseClient)({ address })).toEqual(420n)
 	})
 })
