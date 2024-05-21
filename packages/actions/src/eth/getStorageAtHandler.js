4244ee28d784662f58da182334be4b89743ef382
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-20 23:08:43 -0700

    :bug: Fix: Bug with last blocks state root accidentally being mutated when mining new blocks

--- packages/actions/src/eth/getStorageAtHandler.js
+++ packages/actions/src/eth/getStorageAtHandler.js
@@ -11,33 +11,38 @@ import { NoForkUrlSetError } from './getBalanceHandler.js'
  */
 export const getStorageAtHandler =
 	({ forkUrl, getVm }) =>
-		async (params) => {
-			const vm = await getVm()
-			const tag = params.blockTag ?? 'pending'
-			if (tag === 'latest') {
-				return bytesToHex(
-					await vm.stateManager.getContractStorage(EthjsAddress.fromString(params.address), hexToBytes(params.position, { size: 32 })),
-				)
-			}
-			if (!forkUrl) {
-				throw new NoForkUrlSetError('No forkurl set. Block tags other than latest for getStorageAt has not yet been implemented')
-			}
-			const fetcher = createJsonRpcFetcher(forkUrl)
-			return fetcher
-				.request({
-					jsonrpc: '2.0',
-					method: 'eth_getStorageAt',
-					params: [params.address, params.position, tag],
-					id: 1,
-				})
-				.then((res) => {
-					if (res.error) {
-						/** @type {any} */
-						const err = new Error(res.error.message)
-						err.name = res.error.code
-						err._tag = res.error.code
-						throw err
-					}
-					return /**@type {import('@tevm/utils').Address}*/ (res.result)
-				})
+	async (params) => {
+		const vm = await getVm()
+		const tag = params.blockTag ?? 'pending'
+		if (tag === 'latest') {
+			return bytesToHex(
+				await vm.stateManager.getContractStorage(
+					EthjsAddress.fromString(params.address),
+					hexToBytes(params.position, { size: 32 }),
+				),
+			)
 		}
+		if (!forkUrl) {
+			throw new NoForkUrlSetError(
+				'No forkurl set. Block tags other than latest for getStorageAt has not yet been implemented',
+			)
+		}
+		const fetcher = createJsonRpcFetcher(forkUrl)
+		return fetcher
+			.request({
+				jsonrpc: '2.0',
+				method: 'eth_getStorageAt',
+				params: [params.address, params.position, tag],
+				id: 1,
+			})
+			.then((res) => {
+				if (res.error) {
+					/** @type {any} */
+					const err = new Error(res.error.message)
+					err.name = res.error.code
+					err._tag = res.error.code
+					throw err
+				}
+				return /**@type {import('@tevm/utils').Address}*/ (res.result)
+			})
+	}
