1b4a310468baf27c83e3ceaef9cb4754e929ddf2
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-05-23 14:53:01 -0700

    :mute: Log warning if nothing set

--- test/test-utils/src/transports.ts
+++ test/test-utils/src/transports.ts
@@ -5,13 +5,20 @@ import { mainnet as viemMainnet, optimism as viemOptimism } from 'viem/chains'
 const mainnetRpcUrls = process.env['TEVM_RPC_URLS_MAINNET']?.split(',') ?? []
 const optimismRpcUrls = process.env['TEVM_RPC_URLS_OPTIMISM']?.split(',') ?? []
 
+if (mainnetRpcUrls.length === 0) {
+	console.warn('TEVM_RPC_URLS_MAINNET is not set')
+}
+if (optimismRpcUrls.length === 0) {
+	console.warn('TEVM_RPC_URLS_OPTIMISM is not set')
+}
+
 const mainnet = loadBalance(
 	mainnetRpcUrls.map((url) => rateLimit(http(url), { browser: false, requestsPerSecond: 25 })),
-)({retryCount: 3, chain: viemMainnet})
+)({ retryCount: 3, chain: viemMainnet })
 
 const optimism = loadBalance(
 	optimismRpcUrls.map((url) => rateLimit(http(url), { browser: false, requestsPerSecond: 25 })),
-)({retryCount: 3, chain: viemOptimism})
+)({ retryCount: 3, chain: viemOptimism })
 
 export const transports = {
 	mainnet,
