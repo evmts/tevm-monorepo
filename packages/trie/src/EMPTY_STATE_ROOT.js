466d09d2945a474c0bf5795dac5729caac0bfbe0
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-06-12 14:53:38 -0700

    :construction_worker: Feat: Productionize trie package

--- packages/trie/src/EMPTY_STATE_ROOT.js
+++ packages/trie/src/EMPTY_STATE_ROOT.js
@@ -1,3 +1,7 @@
+/**
+ * An hardcoded state root that represents an empty trie.
+ * Can be dynamically computed using keccak256([]) or generateStateRoot([])
+ */
 export const EMPTY_STATE_ROOT = Uint8Array.from([
 	86, 232, 31, 23, 27, 204, 85, 166, 255, 131, 69, 230, 146, 192, 248, 110, 91, 72, 224, 27, 153, 108, 173, 192, 1, 98,
 	47, 181, 227, 99, 180, 33,
