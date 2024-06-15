8fa029834697602a5674e6d09803e4e283444842
Will Cory <willcory10@gmail.com>
2024-06-15 16:41:35 -0700

    :recycle: Chore: Update default for tevm/bundler (#1208)
    ## Description
    
    _Concise description of proposed changes_
    
    ## Testing
    
    Explain the quality checks that have been done on the code changes
    
    ## Additional Information
    
    - [ ] I read the [contributing docs](../docs/contributing.md) (if this
    is your first contribution)
    
    Your ENS/address:
    
    ---------
    
    Co-authored-by: William Cory <williamcory@Williams-MacBook-Pro.local>

--- bundler-packages/base-bundler/src/getContractPath.js
+++ bundler-packages/base-bundler/src/getContractPath.js
@@ -19,10 +19,10 @@ export const getContractPath = (basePath) => {
 	// @ts-expect-error checking for global existence of bun
 	const isBun = typeof Bun !== 'undefined'
 	if (isBun) {
-		console.warn(`Unable to autodetect whether tevm or @tevm/contract is installed because of a known bug in bun. Defaulting to @tevm/contract.
-You may need to install @tevm/contract if it is not already installed for use with bun.
+		console.warn(`Unable to autodetect whether tevm or @tevm/contract is installed because of a known bug in bun. Defaulting to tevm.
+You may need to install tevm if it is not already installed for use with bun.
 see https://github.com/oven-sh/bun/issues/8974`)
-		return '@tevm/contract'
+		return 'tevm/contract'
 	}
 	throw new Error(
 		`Could not find tevm/contract or @tevm/contract in ${basePath}!. Please install it with \`npm i @tevm/contract\` \`pnpm i @tevm/contract\` or \`yarn add tevm/contract\``,
