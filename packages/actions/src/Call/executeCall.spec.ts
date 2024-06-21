bab602bfd82a903d2acf1327fe34e05a409a1120
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-06-21 13:04:28 -0700

    :white_check_mark: Fix: broken test

--- packages/actions/src/Call/executeCall.spec.ts
+++ packages/actions/src/Call/executeCall.spec.ts
@@ -1,10 +1,11 @@
 import { describe, expect, it } from 'bun:test'
 import { createBaseClient } from '@tevm/base-client'
-import { ExecutionError, InvalidGasPriceError } from '@tevm/errors'
+import { EvmRevertError, InvalidGasPriceError, RevertError } from '@tevm/errors'
 import { TestERC20 } from '@tevm/test-utils'
 import { EthjsAddress, encodeFunctionData, hexToBytes } from '@tevm/utils'
 import { setAccountHandler } from '../SetAccount/setAccountHandler.js'
 import { executeCall } from './executeCall.js'
+import { parseEther } from 'viem'
 
 const ERC20_ADDRESS = `0x${'3'.repeat(40)}` as const
 const ERC20_BYTECODE = TestERC20.deployedBytecode
@@ -52,6 +53,7 @@ describe('executeCall', () => {
 	it('should handle contract call errors', async () => {
 		const client = createBaseClient()
 		const vm = await client.getVm()
+		const caller = `0x${'23'.repeat(20)}` as const
 		expect(
 			(
 				await setAccountHandler(client)({
@@ -60,32 +62,41 @@ describe('executeCall', () => {
 				})
 			).errors,
 		).toBeUndefined()
-
-		const caller = `0x${'23'.repeat(20)}` as const
+		expect(
+			(
+				await setAccountHandler(client)({
+					address: caller,
+					balance: parseEther('100'),
+				})
+			).errors,
+		).toBeUndefined()
+		const notCaller = `0x${'32'.repeat(20)}` as const
 		const evmInput = {
 			data: hexToBytes(
 				encodeFunctionData({
 					abi: ERC20_ABI,
 					functionName: 'transferFrom',
-					args: [caller, caller, 90n],
+					args: [notCaller, caller, 90n],
 				}),
 			),
 			to: EthjsAddress.fromString(ERC20_ADDRESS),
 			origin: EthjsAddress.fromString(caller),
-			caller: EthjsAddress.fromString(caller),
+			from: EthjsAddress.fromString(caller),
 			gasLimit: 16784800n,
 			block: await vm.blockchain.getCanonicalHeadBlock(),
 		}
-
 		const result = await executeCall(client, evmInput, { createAccessList: true, createTrace: true })
 		if (!('errors' in result)) {
 			throw 'should have errors'
 		}
 		expect(result.errors).toBeDefined()
-		expect(result.errors[0]).toBeInstanceOf(ExecutionError)
-		expect(result.errors[0].name).toBe('ExecutionError')
+		expect(result.errors[0].message).toBe(
+			'revert\n\nDocs: https://tevm.sh/reference/tevm/errors/classes/reverterror/\nDetails: {"error":"revert","errorType":"EvmError"}\nVersion: 1.1.0.next-73',
+		)
+		expect(result.errors[0]).toBeInstanceOf(EvmRevertError)
+		expect(result.errors[0]).toBeInstanceOf(RevertError)
 		expect(result.errors[0].code).toBe(-32000)
-		expect(result.errors[0].message).toMatchSnapshot()
+		expect(result.errors[0].name).toBe('EvmRevertError')
 	})
 
 	it('should handle gas price too low error', async () => {
