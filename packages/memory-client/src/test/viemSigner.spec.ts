8402511118e31e61281816d156740dd721ea2862
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-06-26 17:24:36 -0700

    :recycle: Refactor viemsigner

--- packages/memory-client/src/test/viemSigner.spec.ts
+++ packages/memory-client/src/test/viemSigner.spec.ts
@@ -3,15 +3,43 @@ import { optimism } from '@tevm/common'
 import { SimpleContract } from '@tevm/contract'
 import { walletActions } from 'viem'
 import { privateKeyToAccount } from 'viem/accounts'
+import { deployContract, writeContract } from 'viem/actions'
 import { createMemoryClient } from '../index.js'
 
+// Same accounts anvil and hardhat prefund
 const TEVM_TEST_ACCOUNTS = [
 	'0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
 	'0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
 ] as const
 
 describe('using MemoryClient as viem signer', () => {
-	it('should be able to pass in an account', async () => {
+	it('should be able to pass in an account and use viem wallet api to interact with tevm', async () => {
+		const walletClient = createMemoryClient({
+			common: optimism,
+			account: privateKeyToAccount(TEVM_TEST_ACCOUNTS[1]),
+		})
+
+		const txHash = await deployContract(walletClient, SimpleContract.deploy(2n))
+		await walletClient.tevmMine()
+
+		const receipt = await walletClient.getTransactionReceipt({ hash: txHash })
+
+		if (!receipt.contractAddress) {
+			throw new Error('No address created')
+		}
+		expect(receipt.contractAddress).toEqual('0x5FbDB2315678afecb367f032d93F642f64180aa3')
+
+		const contract = SimpleContract.withAddress(receipt.contractAddress)
+
+		expect(await walletClient.readContract(contract.read.get())).toEqual(2n)
+		expect(await writeContract(walletClient, contract.write.set(420n))).toBe(
+			'0xb9efaaa8a2873f58058a8f426692c7774453e05664f56fbd925d15c063de5e54',
+		)
+		await walletClient.tevmMine()
+		expect(await walletClient.readContract(contract.read.get())).toEqual(420n)
+	})
+
+	it('should be robust wrt nonces', async () => {
 		const client = createMemoryClient({
 			common: optimism,
 			account: privateKeyToAccount(TEVM_TEST_ACCOUNTS[1]),
