e5fb2eb6515b817f794334082d69af6a46f6643f
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-07-01 14:45:16 -0700

    :mute: Console.logs

--- packages/state/src/actions/dumpStorage.js
+++ packages/state/src/actions/dumpStorage.js
@@ -15,23 +15,10 @@ export const dumpStorage = (vm) => (address) => {
 	 * @type {import("@tevm/common").StorageDump}
 	 */
 	const dump = {}
-	console.log(
-		'lrucache',
-		storage._orderedMapCache?.size(),
-		'storageMap',
-		address.toString(),
-		storageMap,
-		'storageKey',
-		storage.get(address, new Uint8Array(32)),
-		'elementByKey',
-		storage._orderedMapCache?.getElementByKey(address.toString().slice(2)),
-	)
 	if (storageMap !== undefined) {
 		for (const slot of storageMap) {
-			console.log('looping slot', slot[0], bytesToHex(slot[1]))
 			dump[slot[0]] = bytesToHex(slot[1])
 		}
 	}
-	console.log('returning dump', dump)
 	return Promise.resolve(dump)
 }
