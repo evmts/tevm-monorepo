655a5a3637e578803d221bbfd59ec424861bdba4
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-07-01 14:40:05 -0700

    :bug: Fix: Bug with stale object references in dumpStorage

--- packages/state/src/actions/dumpStorage.js
+++ packages/state/src/actions/dumpStorage.js
@@ -6,18 +6,32 @@ import { bytesToHex } from 'viem'
  * Both are represented as `0x` prefixed hex strings.
  * @type {import("../state-types/index.js").StateAction<'dumpStorage'>}
  */
-export const dumpStorage =
-	({ caches: { storage } }) =>
-	(address) => {
-		const storageMap = storage.dump(address)
-		/**
-		 * @type {import("@tevm/common").StorageDump}
-		 */
-		const dump = {}
-		if (storageMap !== undefined) {
-			for (const slot of storageMap) {
-				dump[slot[0]] = bytesToHex(slot[1])
-			}
+export const dumpStorage = (vm) => (address) => {
+	const {
+		caches: { storage },
+	} = vm
+	const storageMap = storage.dump(address)
+	/**
+	 * @type {import("@tevm/common").StorageDump}
+	 */
+	const dump = {}
+	console.log(
+		'lrucache',
+		storage._orderedMapCache?.size(),
+		'storageMap',
+		address.toString(),
+		storageMap,
+		'storageKey',
+		storage.get(address, new Uint8Array(32)),
+		'elementByKey',
+		storage._orderedMapCache?.getElementByKey(address.toString().slice(2)),
+	)
+	if (storageMap !== undefined) {
+		for (const slot of storageMap) {
+			console.log('looping slot', slot[0], bytesToHex(slot[1]))
+			dump[slot[0]] = bytesToHex(slot[1])
 		}
-		return Promise.resolve(dump)
 	}
+	console.log('returning dump', dump)
+	return Promise.resolve(dump)
+}
