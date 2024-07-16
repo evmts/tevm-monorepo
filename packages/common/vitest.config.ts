b16e240e5a1bdebca3e92954ef1f06e5b38954ac
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-07-16 14:27:08 -0700

    :recycle: Regenerate

--- packages/common/vitest.config.ts
+++ packages/common/vitest.config.ts
@@ -2,20 +2,20 @@ import { defineConfig } from 'vitest/config'
 
 // https://vitest.dev/config/ - for docs
 export default defineConfig({
-				test: {
-								include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-								environment: 'node',
-								coverage: {
-												include: ['src/**/*.js'],
-												provider: 'v8',
-												reporter: ['text', 'json-summary', 'json'],
-												thresholds: {
-																autoUpdate: true,
-																functions: 100,
-																lines: 99.14,
-																statements: 99.14,
-																branches: 81.73,
-												},
-								},
-				},
+	test: {
+		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+		environment: 'node',
+		coverage: {
+			include: ['src/**/*.js'],
+			provider: 'v8',
+			reporter: ['text', 'json-summary', 'json'],
+			thresholds: {
+				autoUpdate: true,
+				functions: 100,
+				lines: 99.14,
+				statements: 99.14,
+				branches: 81.73,
+			},
+		},
+	},
 })
