d04f5490a6afa8f1295cd632474888a8bcecae6d
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-07-29 18:05:10 -0700

    :white_check_mark: Hack test into working

--- bundler-packages/tevm-run/src/tevm-run.spec.ts
+++ bundler-packages/tevm-run/src/tevm-run.spec.ts
@@ -6,7 +6,7 @@ it('should run a cli', async () => {
 		(await $`./src/tevm-run.js ./example/example.ts`)
 			.text()
 			.split('\n')
-			.map((line) => line.replace(process.cwd(), ''))
+			.map((line) => line.replace(process.cwd(), '').trim())
 			.join('\n'),
 	).toBe(
 		'[tevm-run] bun run --bun --config=/bunfig.toml --install=fallback ./example/example.ts \nsuccess\naddress:  0x5FbDB2315678afecb367f032d93F642f64180aa3\nSimpleContract.get():  20n\n',
