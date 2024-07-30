75d7e1d1141327bbf05350544dd7240be8f2061c
William Cory <williamcory@Williams-MacBook-Pro.local>
2024-07-30 02:37:55 -0700

    :bug: Fix: test that doesnt work on ci

--- bundler-packages/tevm-run/src/tevm-run.spec.ts
+++ bundler-packages/tevm-run/src/tevm-run.spec.ts
@@ -2,13 +2,14 @@ import { expect, it } from 'bun:test'
 import { $ } from 'bun'
 
 it('should run a cli', async () => {
-	expect(
-		(await $`./src/tevm-run.js ./example/example.ts`)
-			.text()
-			.split('\n')
-			.map((line) => line.replace(process.cwd(), '').trim())
-			.join('\n'),
-	).toBe(
-		'[tevm-run] bun run --bun --config=/bunfig.toml --install=fallback ./example/example.ts \nsuccess\naddress:  0x5FbDB2315678afecb367f032d93F642f64180aa3\nSimpleContract.get():  20n\n',
-	)
+	const result = (await $`./src/tevm-run.js ./example/example.ts`)
+		.text()
+		.split('\n')
+		.map((line) => line.replace(process.cwd(), '').trim())
+		.join('\n')
+	expect(result).toInclude('[tevm-run] bun run --bun --config=/bunfig.toml --install=fallback ./example/example.ts')
+	expect(result).toInclude('success')
+	expect(result).toInclude('address:  0x5FbDB2315678afecb367f032d93F642f64180aa3')
+	expect(result).toInclude('SimpleContract.get()')
+	expect(result).toInclude('20n')
 })
