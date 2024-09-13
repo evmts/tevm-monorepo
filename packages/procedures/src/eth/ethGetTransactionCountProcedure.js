99fe3332a3c3354d2d96bf0ac095686159cc87b1
William Cory <williamcory@Williams-MacBook-Pro-2.local>
2024-09-13 11:54:20 -0700

    :white_check_mark: Test: Add eth_getTransactionCount tests

--- packages/procedures/src/eth/ethGetTransactionCountProcedure.js
+++ packages/procedures/src/eth/ethGetTransactionCountProcedure.js
@@ -1,17 +1,18 @@
 import { createAddress } from '@tevm/address'
-import { bytesToHex, getAddress, hexToBigInt, hexToBytes, numberToHex } from '@tevm/utils'
+import { ForkError, InternalEvmError } from '@tevm/errors'
+import { hexToBigInt, hexToBytes, numberToHex } from '@tevm/utils'
 
 /**
  * Request handler for eth_getFilterLogs JSON-RPC requests.
- * @param {import('@tevm/node').TevmNode} client
+ * @param {import('@tevm/node').TevmNode} node
  * @returns {import('./EthProcedure.js').EthGetTransactionCountJsonRpcProcedure}
  */
-export const ethGetTransactionCountProcedure = (client) => {
+export const ethGetTransactionCountProcedure = (node) => {
 	return async (request) => {
 		const [address, tag] = request.params
 
 		const block = await (async () => {
-			const vm = await client.getVm()
+			const vm = await node.getVm()
 			if (tag.startsWith('0x') && tag.length === 66) {
 				return vm.blockchain.getBlock(hexToBytes(/** @type {import('@tevm/utils').Hex}*/ (tag)))
 			}
@@ -42,22 +43,63 @@ export const ethGetTransactionCountProcedure = (client) => {
 		const pendingCount =
 			tag === 'pending'
 				? await (async () => {
-						const txPool = await client.getTxPool()
+						const txPool = await node.getTxPool()
 						const pendingTx = await txPool.getBySenderAddress(createAddress(address))
 						return BigInt(pendingTx.length)
 					})()
 				: 0n
 
 		const includedCount = await (async () => {
-			const vm = await client.getVm()
-			// TODO we can optimize this by not deep copying once we are more confident it's safe
-			const root = vm.stateManager._baseState.stateRoots.get(bytesToHex(block.header.stateRoot))
-			if (!root) {
-				// todo we might want to throw an error hre
-				return 0n
+			const vm = await node.getVm()
+			if (!(await vm.stateManager.hasStateRoot(block.header.stateRoot))) {
+				return undefined
 			}
-			return root[getAddress(address)]?.nonce ?? 0n
+			const stateCopy = await vm.stateManager.deepCopy()
+			stateCopy.setStateRoot(block.header.stateRoot)
+			const account = await stateCopy.getAccount(createAddress(address))
+			return account?.nonce ?? 0n
 		})()
+
+		if (includedCount === undefined && node.forkTransport) {
+			try {
+				/**
+				 * @type {import('@tevm/utils').Hex}
+				 */
+				const result = await node.forkTransport.request(request)
+				return {
+					...(request.id ? { id: request.id } : {}),
+					method: request.method,
+					jsonrpc: request.jsonrpc,
+					result: numberToHex(hexToBigInt(result) + pendingCount),
+				}
+			} catch (e) {
+				const err = new ForkError('Unable to resolve eth_getTransactionCount with fork', {
+					cause: /** @type {any}*/ (e),
+				})
+				return {
+					...(request.id ? { id: request.id } : {}),
+					method: request.method,
+					jsonrpc: request.jsonrpc,
+					error: {
+						code: err.code,
+						message: err.message,
+					},
+				}
+			}
+		}
+		if (includedCount === undefined) {
+			const err = new InternalEvmError(`No state root found for block tag ${tag} in eth_getTransactionCountProcedure`)
+			node.logger.error(err)
+			return {
+				...(request.id ? { id: request.id } : {}),
+				method: request.method,
+				jsonrpc: request.jsonrpc,
+				error: {
+					code: err.code,
+					message: err.message,
+				},
+			}
+		}
 
 		return {
 			...(request.id ? { id: request.id } : {}),
