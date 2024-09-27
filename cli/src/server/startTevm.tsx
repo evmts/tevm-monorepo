f29b74a4d6485042c7bcef08c31cedfd91d762ee
William Cory <williamcory@Williams-MacBook-Pro-2.local>
2024-09-27 01:30:44 -0700

    :bug: Fix: lint

--- cli/src/server/startTevm.tsx
+++ cli/src/server/startTevm.tsx
@@ -1,12 +1,14 @@
 import { chains } from './chains.js'
 import { options as optionsSchema } from './options.js'
 import { http } from '@tevm/jsonrpc'
-import { createMemoryClient, type MemoryClient } from '@tevm/memory-client'
+import { type MemoryClient, createMemoryClient } from '@tevm/memory-client'
 import { createServer } from '@tevm/server'
-import { z } from 'zod'
 import { type Server } from 'node:http'
+import { z } from 'zod'
 
-export const startTevm = async (options: z.infer<typeof optionsSchema>): Promise<{ transport: MemoryClient, server: Server }> => {
+export const startTevm = async (
+	options: z.infer<typeof optionsSchema>,
+): Promise<{ transport: MemoryClient; server: Server }> => {
 	const chain =
 		options.preset !== undefined ? chains[options.preset] : undefined
 
