1f09a408ee8b8d5922d10cd22fb63e9b946a5bf8
William Cory <willcory10@gmail.com>
2024-10-13 10:36:50 -0700

    :bug: Fix: add jsonAbiAsConst to validator

--- bundler-packages/config/src/config/validateUserConfig.js
+++ bundler-packages/config/src/config/validateUserConfig.js
@@ -62,6 +62,7 @@ const SCompilerConfig = Struct({
 	remappings: optional(Union(Record({ key: SString, value: SString }), Undefined)),
 	debug: optional(Union(SBoolean, Undefined)),
 	cacheDir: optional(Union(SString, Undefined)),
+	jsonAbiAsConst: optional(Union(SString, SArray(SString), Undefined)), // Updated property
 })
 
 /**
