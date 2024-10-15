4987fb0087064b84dcedd9e4460fe51c9023e6fa
William Cory <willcory10@gmail.com>
2024-10-15 11:48:33 -0700

    :zap: Fix: Remove bun warning from base-bundler

--- bundler-packages/base-bundler/src/getContractPath.js
+++ bundler-packages/base-bundler/src/getContractPath.js
@@ -16,12 +16,9 @@ export const getContractPath = (basePath) => {
 			return contractPackage
 		} catch (e) {}
 	}
-	// @ts-expect-error checking for global existence of bun
-	const isBun = typeof Bun !== 'undefined'
-	if (isBun) {
-		return 'tevm/contract'
-	}
-	throw new Error(
-		`Could not find tevm/contract or @tevm/contract in ${basePath}!. Please install it with \`npm i @tevm/contract\` \`pnpm i @tevm/contract\` or \`yarn add tevm/contract\``,
+	console.warn(
+		`Could not find tevm/contract or @tevm/contract in ${basePath}!. Please install it with \`npm i @tevm/contract\` \`pnpm i @tevm/contract\` or \`yarn add tevm/contract\`
+Falling back to attempting tevm/contract`,
 	)
+	return 'tevm/contract'
 }
