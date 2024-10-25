b98b932c0f2fbf2e28f552ebf788b4405a78526e
William Cory <willcory10@gmail.com>
2024-10-24 20:32:57 -0700

    :white_check_mark: Test: Update them

--- packages/memory-client/src/test/viem/call.spec.ts
+++ packages/memory-client/src/test/viem/call.spec.ts
@@ -19,7 +19,12 @@ beforeEach(async () => {
 	mc = createMemoryClient({
 		common: mainnet,
 		fork: {
-			transport: transports.mainnet,
+			transport: {
+				request: (request) => {
+					console.log(request)
+					return transports.mainnet.request(request)
+				},
+			},
 			blockTag: forkBlock,
 		},
 	})
@@ -45,7 +50,7 @@ beforeEach(async () => {
 const account = privateKeyToAccount('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80')
 
 describe('call', () => {
-	it.todo(
+	it(
 		'should work',
 		async () => {
 			expect(
