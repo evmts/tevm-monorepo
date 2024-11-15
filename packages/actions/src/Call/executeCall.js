2245f6827c06731f7ac90301cbd13153fe4ce52d
William Cory <willcory10@gmail.com>
2024-11-15 13:29:44 +0700

    :zap: Feat: Optimize executeCall

--- packages/actions/src/Call/executeCall.js
+++ packages/actions/src/Call/executeCall.js
@@ -1,8 +1,9 @@
 import { runTx } from '@tevm/vm'
-import { bytesToHex } from 'viem'
+import { bytesToHex, hexToBytes, numberToHex } from 'viem'
 import { evmInputToImpersonatedTx } from '../internal/evmInputToImpersonatedTx.js'
 import { runCallWithTrace } from '../internal/runCallWithTrace.js'
 import { handleRunTxError } from './handleEvmError.js'
+import { createAddress } from '@tevm/address'
 
 /**
  * The error returned by executeCall
@@ -36,6 +37,61 @@ export const executeCall = async (client, evmInput, params) => {
 	 */
 	let accessList = undefined
 	const vm = await client.getVm()
+		// as an optimization we want to attempt to fetch all storage up front
+		; (async () => {
+			if (!client.forkTransport) return
+			const blockTag = await client.getVm()
+				.then(vm => vm.blockchain.blocksByTag.get('forked'))
+				.then(block => block !== undefined ? numberToHex(block.header.number) : 'latest')
+			// TODO this is wrong we should use forkTransport.send but it doesn't exist yet
+			/**
+			 * @type {import('../eth/EthJsonRpcResponse.js').EthCreateAccessListJsonRpcResponse}
+			 */
+			const accessListRes = await client.forkTransport?.request({
+				method: 'eth_createAccessList',
+				params: [
+					{
+						to: evmInput.to,
+						gas: evmInput.gasLimit,
+						// gasPrice: evmInput.gasPrice, not necessary to set this
+						value: evmInput.value,
+						data: evmInput.data,
+					},
+					blockTag,
+				],
+			})
+			if (accessListRes.error) {
+				client.logger.error('Unable to get an access list', accessListRes.error)
+			}
+			for (const contract of accessListRes.result?.accessList ?? []) {
+				for (const storageKey of contract.storageKeys) {
+					const getStorage = () => vm.stateManager._baseState.caches.storage.get(createAddress(contract.address), hexToBytes(storageKey, { size: 32 }))
+					if (getStorage()?.length) {
+						return
+					}
+					/**
+					 * @type {import('../eth/EthJsonRpcResponse.js').EthCreateAccessListJsonRpcResponse}
+					 */
+					client.forkTransport.request({
+						method: 'eth_getStorageAt',
+						params: [contract.address, storageKey, blockTag]
+					}).then(async storage => {
+						const vm = await client.getVm()
+						// THIS MUST BE SYNC TO INITIALLY SET STORAGE OR RACE CONDITIONS WILL EXIST!!!
+						// This can be cleaned up with a state lock later
+						if (getStorage()?.length) {
+							return
+						}
+						vm.stateManager._baseState.caches.storage
+							.put(
+								createAddress(contract.address),
+								hexToBytes(storageKey, { size: 32 }),
+								hexToBytes(/** @type {import('viem').Hex}*/(storage))
+							)
+					})
+				}
+			}
+		})()
 	try {
 		const tx = await evmInputToImpersonatedTx({
 			...client,
