3ecfa42940d1c19d782d6d665afb4c5fd709b0bb
Your Name <willcory10@gmail.com>
2025-02-22 14:27:04 -0800

    :memo: Docs: Update vocs.config

--- docs/node/vocs.config.ts
+++ docs/node/vocs.config.ts
@@ -12,4 +12,24 @@ export default defineConfig({
       link: '/example',
     },
   ],
+  editLink: {
+    pattern: 'https://github.com/evmts/tevm-monorepo/edit/docs/node/docs/pages/:path'
+  },
+  socials: [
+    {
+      icon: 'github',
+      link: 'https://github.com/evmts/tevm-monorepo',
+      label: 'Github'
+    },
+    {
+      icon: 'telegram',
+      link: 'https://t.me/+ANThR9bHDLAwMjUx',
+      label: 'Telegram'
+    },
+    {
+      icon: 'x',
+      link: 'https://x.com/tevmtools',
+      label: 'Twitter'
+    },
+  ],
 })
