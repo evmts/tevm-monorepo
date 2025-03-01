f7a444e85d47021e3effd816ba36255acf928415
Your Name <willcory10@gmail.com>
2025-03-01 12:37:41 -0700

    :bug: Fix: type errors

--- bundler-packages/solc/src/solc.js
+++ bundler-packages/solc/src/solc.js
@@ -10,6 +10,7 @@ export const fileLevelOption = ''
  * @type {import("./solcTypes.js").Releases}
  */
 export const releases = {
+	'0.8.28': 'v0.8.28+commit.c33e5a8c.js',
 	'0.8.27': 'v0.8.27+commit.4a35a09.js',
 	'0.8.26': 'v0.8.26+commit.8a97fa7a.js',
 	'0.8.25': 'v0.8.25+commit.b61c2a91.js',
