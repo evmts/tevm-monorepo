d222fe2e528c35fa129a3c08bf2bd05bccfe34b2
Your Name <willcory10@gmail.com>
2025-03-07 11:16:06 -0800

    :bug: Another fix

--- packages/actions/src/eth/ethGetTransactionCountProcedure.spec.ts
+++ packages/actions/src/eth/ethGetTransactionCountProcedure.spec.ts
@@ -5,6 +5,7 @@ import { numberToHex, parseEther } from '@tevm/utils'
 import { describe, expect, it } from 'vitest'
 import { callHandler } from '../Call/callHandler.js'
 import { ethGetTransactionCountProcedure } from './ethGetTransactionCountProcedure.js'
+import { setAccountHandler } from '../SetAccount/setAccountHandler.js'
 
 const address = '0xb5d85CBf7cB3EE0D56b3bB207D5Fc4B82f43F511' as const
 
@@ -24,13 +25,13 @@ describe(ethGetTransactionCountProcedure.name, () => {
 				params: [address, 'latest'],
 			}),
 		).toMatchInlineSnapshot(`
-			{
-			  "id": 1,
-			  "jsonrpc": "2.0",
-			  "method": "eth_getTransactionCount",
-			  "result": "0x8e96b4",
-			}
-		`)
+{
+  "id": 1,
+  "jsonrpc": "2.0",
+  "method": "eth_getTransactionCount",
+  "result": "0xa83701",
+}
+`)
 	})
 	it('should work with past block tags', async () => {
 		const node = createTevmNode({
@@ -44,16 +45,16 @@ describe(ethGetTransactionCountProcedure.name, () => {
 				jsonrpc: '2.0',
 				id: 1,
 				method: 'eth_getTransactionCount',
-				params: [address, numberToHex(20700000n)],
+				params: [address, numberToHex(21996939n)],
 			}),
 		).toMatchInlineSnapshot(`
-			{
-			  "id": 1,
-			  "jsonrpc": "2.0",
-			  "method": "eth_getTransactionCount",
-			  "result": "0x8df90f",
-			}
-		`)
+{
+  "id": 1,
+  "jsonrpc": "2.0",
+  "method": "eth_getTransactionCount",
+  "result": "0xa836d8",
+}
+`)
 	})
 	it('should work with pending tx', async () => {
 		const node = createTevmNode({
@@ -62,9 +63,13 @@ describe(ethGetTransactionCountProcedure.name, () => {
 				blockTag: 21996939n,
 			},
 		})
+		await setAccountHandler(node)({
+			address,
+			balance: parseEther('25')
+		})
 		await callHandler(node)({
 			from: address,
-			to: createAddress(5).toString(),
+			to: createAddress(500).toString(),
 			value: parseEther('0.1'),
 			createTransaction: true,
 		})
@@ -76,13 +81,13 @@ describe(ethGetTransactionCountProcedure.name, () => {
 				params: [address, 'latest'],
 			}),
 		).toMatchInlineSnapshot(`
-			{
-			  "id": 1,
-			  "jsonrpc": "2.0",
-			  "method": "eth_getTransactionCount",
-			  "result": "0x8e96b4",
-			}
-		`)
+{
+  "id": 1,
+  "jsonrpc": "2.0",
+  "method": "eth_getTransactionCount",
+  "result": "0xa836d8",
+}
+`)
 		expect(
 			await ethGetTransactionCountProcedure(node)({
 				jsonrpc: '2.0',
@@ -91,12 +96,12 @@ describe(ethGetTransactionCountProcedure.name, () => {
 				params: [address, 'pending'],
 			}),
 		).toMatchInlineSnapshot(`
-			{
-			  "id": 1,
-			  "jsonrpc": "2.0",
-			  "method": "eth_getTransactionCount",
-			  "result": "0x8e96b5",
-			}
-		`)
+{
+  "id": 1,
+  "jsonrpc": "2.0",
+  "method": "eth_getTransactionCount",
+  "result": "0xa836d9",
+}
+`)
 	})
 })
