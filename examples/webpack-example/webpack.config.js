32ce50d169203975ce6ff9ee8a92a3436c303577
Your Name <willcory10@gmail.com>
2025-03-12 19:27:20 -0700

    :package: Feat(examples): add webpack, rollup, and rspack example projects
      This commit adds three new example projects showcasing Tevm integration with different bundlers:
    
    :package: Chore: Add moore examples

---
+++ examples/webpack-example/webpack.config.js
@@ -0,0 +1,45 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { tevmPlugin } from '@tevm/webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    tevmPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.sol'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
