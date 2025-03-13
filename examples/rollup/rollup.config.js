4891ed1b42375743a8dceccbb8a768b3db391a02
Your Name <willcory10@gmail.com>
2025-03-12 19:18:11 -0700

    ✨ feat: Implement gas mining support for transaction creation

---
+++ examples/rollup/rollup.config.js
@@ -0,0 +1,51 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import css from 'rollup-plugin-css-only';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import { tevmPlugin } from '@tevm/rollup-plugin';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'src/main.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'public/build/bundle.js'
+  },
+  plugins: [
+    // Enable importing .sol files directly
+    tevmPlugin(),
+    
+    // Parse node_modules
+    nodeResolve({
+      browser: true,
+      dedupe: ['viem']
+    }),
+    
+    // Convert CommonJS modules to ES6
+    commonjs(),
+    
+    // Extract CSS into a separate file
+    css({ output: 'bundle.css' }),
+    
+    // If in development mode, serve the app
+    !production && serve({
+      open: false,
+      contentBase: 'public',
+      port: 8080,
+    }),
+    
+    // Enable live reloading in development mode
+    !production && livereload('public'),
+    
+    // If in production mode, minify
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
+};
