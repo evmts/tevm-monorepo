62fdd276f66c99c3e8ee815bbf95d221800a2833
Your Name <willcory10@gmail.com>
2025-03-17 04:56:41 -0700

    fix: fixes

--- packages/server/vitest.config.ts
+++ packages/server/vitest.config.ts
@@ -2,20 +2,20 @@ import { defineConfig } from 'vitest/config'
 
 // https://vitest.dev/config/ - for docs
 export default defineConfig({
-				test: {
-								include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-								environment: 'node',
-								coverage: {
-												include: ['src/**/*.js'],
-												provider: 'v8',
-												reporter: ['text', 'json-summary', 'json'],
-												thresholds: {
-																autoUpdate: true,
-																lines: 100,
-																functions: 100,
-																branches: 98.88, // Temporarily reduced from 100 to 98
-																statements: 100,
-												},
-								},
-				},
+	test: {
+		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+		environment: 'node',
+		coverage: {
+			include: ['src/**/*.js'],
+			provider: 'v8',
+			reporter: ['text', 'json-summary', 'json'],
+			thresholds: {
+				autoUpdate: true,
+				lines: 100,
+				functions: 100,
+				branches: 98.88, // Temporarily reduced from 100 to 98
+				statements: 100,
+			},
+		},
+	},
 })
