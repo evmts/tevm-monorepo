1321c17108ef9f952f8c93b99d3e064c3b3e417a
William Cory <willcory10@gmail.com>
2025-03-26 06:02:05 -0700

    fix: lint

--- test/bench/src/server.spec.ts
+++ test/bench/src/server.spec.ts
@@ -1,18 +1,18 @@
-import { createMemoryClient, http } from "tevm";
-import { createServer } from "tevm/server";
+import { http, createMemoryClient } from 'tevm'
+import { createServer } from 'tevm/server'
 
-const PORT = 8545;
+const PORT = 8545
 
-const client = createMemoryClient();
-const server = createServer(client);
+const client = createMemoryClient()
+const server = createServer(client)
 
 server.listen(PORT, () => {
-  console.log("server started making a request");
+	console.log('server started making a request')
 
-  http(`http://localhost:${PORT}`)({})
-    .request({
-      method: "eth_chainId",
-    })
-    .then(console.log)
-    .catch(console.error);
-});
+	http(`http://localhost:${PORT}`)({})
+		.request({
+			method: 'eth_chainId',
+		})
+		.then(console.log)
+		.catch(console.error)
+})
