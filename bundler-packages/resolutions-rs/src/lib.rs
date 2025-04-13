d142f31c45df74f07bc28cdb84fe72f60ca70a36
William Cory <willcory10@gmail.com>
2025-04-12 22:55:17 -0700

    feat: implement more of the resolutions package

--- bundler-packages/resolutions-rs/src/lib.rs
+++ bundler-packages/resolutions-rs/src/lib.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use std::path::{Path, PathBuf};
 use std::{collections::HashMap, io};
 
 // #[macro_use]
@@ -30,15 +31,38 @@ struct ResolvedImport {
     updated: String,
 }
 
-pub fn resolve_imports(
-    absolutePath: &str,
+fn resolve_import_path(
+    absolute_path: &str,
+    import_path: &str,
+    remappings: HashMap<&str, &str>,
+    libs: Vec<&str>,
+) -> PathBuf {
+    let dirpath = match Path::new(absolute_path).parent() {
+        Some(dirpath) => dirpath,
+        None => panic!("expected dirpath"),
+    };
+    for (val, key) in remappings {
+        if import_path.starts_with(key) {
+            return dirpath.join(import_path.replace(key, val));
+        }
+    }
+    if import_path.starts_with('.') {
+        return dirpath.join(import_path);
+    }
+    panic!("TODO implement node resolution: return node_resolve(import_path, {basedir: absolutePath, paths: libs})");
+}
+
+fn resolve_imports(
+    absolute_path: &str,
     code: &str,
     remappings: HashMap<&str, &str>,
     libs: Vec<&str>,
 ) -> Result<Vec<ResolvedImport>, io::Error> {
     let import_regex = Regex::new(r#"^\s?import\s+[^'"]*['"](.*)['"]\s*"#).unwrap();
 
-    for caps in import_regex.captures_iter(&code) {}
+    for caps in import_regex.captures_iter(&code) {
+        if let Some(import_path) = caps.get(0) {};
+    }
 
     Err(io::Error::new(io::ErrorKind::Other, "placeholder"))
 }
