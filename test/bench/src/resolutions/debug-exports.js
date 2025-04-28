db1f21ddf730acd6e17402aef1e1c61b29838816
William Cory <willcory10@gmail.com>
2025-04-28 09:03:29 -0700

    remove test changes

--- test/bench/src/resolutions/debug-exports.js
+++ test/bench/src/resolutions/debug-exports.js
@@ -1,18 +1,18 @@
-const resolutionsRs = require('@tevm/resolutions-rs')
+const resolutionsRs = require('@tevm/resolutions-rs');
 
-console.log('Available exports:', Object.keys(resolutionsRs))
+console.log('Available exports:', Object.keys(resolutionsRs));
 
 // Check if the native module is loaded correctly
-console.log('Native binding details:')
+console.log('Native binding details:');
 try {
-	const nativeBinding = require('../../../bundler-packages/resolutions-rs/tevm_resolutions_rs.darwin-arm64.node')
-	console.log('Native binding exports:', Object.keys(nativeBinding))
+  const nativeBinding = require('../../../bundler-packages/resolutions-rs/tevm_resolutions_rs.darwin-arm64.node');
+  console.log('Native binding exports:', Object.keys(nativeBinding));
 } catch (e) {
-	console.error('Error loading native binding:', e)
+  console.error('Error loading native binding:', e);
 }
 
 // Test calling the exports
-console.log('\nTesting exports:')
+console.log('\nTesting exports:');
 for (const key of Object.keys(resolutionsRs)) {
-	console.log(`- ${key} type:`, typeof resolutionsRs[key])
+  console.log(`- ${key} type:`, typeof resolutionsRs[key]);
 }
