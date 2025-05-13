7c91f44ebf44de8319a5c011f61e8fdcf4fed151
William Cory <willcory10@gmail.com>
2025-05-13 04:47:46 -0700

    errors

--- src/Evm/evm.zig
+++ src/Evm/evm.zig
@@ -54,6 +54,37 @@ const ExecuteResult = struct {
     blobGasUsed: ?u256,
 };
 
+const ExecutionError = error{
+    OUT_OF_GAS,
+    CODESTORE_OUT_OF_GAS,
+    CODESIZE_EXCEEDS_MAXIMUM,
+    STACK_UNDERFLOW,
+    STACK_OVERFLOW,
+    INVALID_JUMP,
+    INVALID_OPCODE,
+    OUT_OF_RANGE,
+    REVERT,
+    STATIC_STATE_CHANGE,
+    INTERNAL_ERROR,
+    CREATE_COLLISION,
+    STOP,
+    REFUND_EXHAUSTED,
+    VALUE_OVERFLOW,
+    INSUFFICIENT_BALANCE,
+    INVALID_BYTECODE_RESULT,
+    INITCODE_SIZE_VIOLATION,
+    INVALID_INPUT_LENGTH,
+    INVALID_EOF_FORMAT,
+    BLS_12_381_INVALID_INPUT_LENGTH,
+    BLS_12_381_POINT_NOT_ON_CURVE,
+    BLS_12_381_INPUT_EMPTY,
+    BLS_12_381_FP_NOT_IN_FIELD,
+    BN254_FP_NOT_IN_FIELD,
+    INVALID_COMMITMENT,
+    INVALID_INPUTS,
+    INVALID_PROOF,
+};
+
 pub const EVM = struct {
     allocator: std.mem.Allocator,
 
@@ -65,7 +96,7 @@ pub const EVM = struct {
 
     pub fn execute(
         _: ExecuteParams,
-    ) !ExecuteResult {
+    ) ExecutionError!ExecuteResult {
         return ExecuteResult{
             .executionGasUsed = 0,
             .returnValue = .{},
