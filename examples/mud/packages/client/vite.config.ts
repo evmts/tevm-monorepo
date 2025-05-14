6c15831146c5ecd21ac89728e203862aa58bdb8d
0xpolarzero <0xpolarzero@gmail.com>
2025-05-14 08:55:15 +0200

    init: mud react example in examples

--- examples/mud/packages/client/vite.config.ts
+++ examples/mud/packages/client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { mud } from "vite-plugin-mud";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), mud({ worldsFile: "../contracts/worlds.json" })],
@@ -8,5 +9,10 @@ export default defineConfig({
     target: "es2022",
     minify: true,
     sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      "@tevm/mud": path.resolve(__dirname, "../../../../bundler-packages/mud/src/index.ts"),
+    },
   },
 });
