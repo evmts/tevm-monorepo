7f30acd71cef4fa620ac52d258cde6570d5ca255
William Cory <willcory10@gmail.com>
2025-05-18 10:49:14 -0700

    feat: Mostly connect it

--- app/src-tauri/build.rs
+++ app/src-tauri/build.rs
@@ -13,21 +13,35 @@ fn connect_zig() {
     let zig_out_path = repo_root_path.join("zig-out/lib");
     let out_dir_env = env::var("OUT_DIR").expect("OUT_DIR not set");
     let zig_build_status = Command::new("zig")
-        .args(&["build", "-O", "ReleaseSafe", "--target", &target])
+        .args(&["build", "-Doptimize=ReleaseSafe"])
         .current_dir(repo_root_path)
         .status()
         .expect("Failed to run zig compiler");
     assert!(zig_build_status.success(), "Zig compilation failed");
 
     let lib_name = if target.contains("windows") {
-        "libzigevm.lib"
+        "zigevm.lib"
     } else {
         "libzigevm.a"
     };
+    let link_name = if lib_name.starts_with("lib") {
+        &lib_name[3..lib_name.len() - 2]
+    } else {
+        &lib_name[..lib_name.len() - 4]
+    };
 
     let lib_path = zig_out_path.join(lib_name);
-    copy(lib_path, format!("{}/{}", out_dir_env, lib_name)).expect("Failed to move binary");
+    copy(&lib_path, format!("{}/{}", out_dir_env, lib_name)).expect("Failed to move binary");
 
     println!("cargo:rustc-link-search={}", out_dir_env);
-    println!("cargo:rustc-link-lib=static=libzigevm");
+    println!("cargo:rustc-link-lib=static={}", link_name);
+
+    println!(
+        "cargo:rerun-if-changed={}",
+        repo_root_path.join("src").display()
+    );
+    println!(
+        "cargo:rerun-if-changed={}",
+        repo_root_path.join("build.zig").display()
+    );
 }
