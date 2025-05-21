17ae67ba029969230cd7e7a52985cc082d2c1c81
William Cory <willcory10@gmail.com>
2025-05-21 02:12:40 -0700

    feat: Scaffold out operator

--- src/Evm/interpreter.zig
+++ src/Evm/interpreter.zig
@@ -1,9 +1,11 @@
 const std = @import("std");
 const _contract = @import("Contract.zig");
 const _frame = @import("Frame.zig");
+const _opcodes = @import("Opcodes.zig");
 
 const Contract = _contract.Contract;
 const Frame = _frame.Frame;
+const Opcode = _opcodes.Opcode;
 
 const Allocator = std.mem.Allocator;
 const print = std.debug.print;
@@ -11,6 +13,23 @@ const print = std.debug.print;
 pub const InterpreterError = error{
     /// Not enough gas to continue execution
     OutOfGas,
+};
+
+pub const OpcodeExecutor = struct {
+    const Self = @This();
+
+    // we are treating this like this because in future this should be dynamically dispatching via a jump table and pointer
+    opcode: Opcode,
+    pub fn create(opcode: Opcode) Self {
+        return Self{ .opcode = opcode };
+    }
+
+    // TODO: Perf improvement we should use a jump table though
+    pub fn execute(self: Self, op: Opcode, evm: Evm, frame: Frame) InterpreterError!void {
+        _ = evm;
+        _ = frame;
+        switch (op) {}
+    }
 };
 
 pub const Evm = struct {
@@ -30,11 +49,18 @@ pub const Evm = struct {
     pub fn run(self: *Self, contract: *Contract, input: []const u8) InterpreterError![]const u8 {
         print("Starting execution", .{});
 
+        self.evm.depth += 1;
+        defer self.evm.depth -= 1;
+
         var frame = try Frame.create(self.allocator, contract);
         defer frame.destroy();
 
         while (true) {
-            const op_code = contract.getOp(frame.pc);
+            // TODO: Perf improvement. Rather than getting opcode from bytecode then getting operation from opcode we can just replace the bytecode with direct pointers to operation
+            const op = contract.getOp(frame.pc);
+            print("Executing opcode: pc={d}, op=0x{x:0>2} ({s})", .{ frame.pc, op, op.getName() });
+
+            _ = operate(frame, self);
         }
     }
 };
