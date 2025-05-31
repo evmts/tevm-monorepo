e6e6f994fdb06fa60931fc43fa83a5b0a724698b
William Cory <willcory10@gmail.com>
2025-05-31 02:50:13 -0700

    more changes wip

--- src/ui/src/components/evm-debugger/utils.ts
+++ src/ui/src/components/evm-debugger/utils.ts
@@ -1,79 +1,79 @@
-import { EvmState } from "./types";
+import { EvmState } from './types'
 
 export async function loadBytecode(bytecodeHex: string): Promise<void> {
 	try {
-		console.log("load_bytecode", { bytecodeHex });
+		console.log('load_bytecode', { bytecodeHex })
 	} catch (err) {
-		throw new Error(`Failed to load bytecode: ${err}`);
+		throw new Error(`Failed to load bytecode: ${err}`)
 	}
 }
 
 export async function resetEvm(): Promise<EvmState> {
 	try {
-		console.log("reset_evm");
-		return await getEvmState();
+		console.log('reset_evm')
+		return await getEvmState()
 	} catch (err) {
-		throw new Error(`Failed to reset EVM: ${err}`);
+		throw new Error(`Failed to reset EVM: ${err}`)
 	}
 }
 
 export async function stepEvm(): Promise<EvmState> {
 	try {
-		console.log("step_evm");
+		console.log('step_evm')
 		return {
 			pc: 2,
 			logs: [],
 			depth: 0,
 			stack: [],
-			memory: "0x0",
-			opcode: "0x0",
+			memory: '0x0',
+			opcode: '0x0',
 			gasLeft: 420,
 			storage: {},
-			returnData: "0x0",
-		};
+			returnData: '0x0',
+		}
 	} catch (err) {
-		throw new Error(`Failed to step: ${err}`);
+		throw new Error(`Failed to step: ${err}`)
 	}
 }
 
 export async function toggleRunPause(): Promise<EvmState> {
 	try {
-		console.log("toggle_run_pause");
+		console.log('toggle_run_pause')
 		return {
 			pc: 2,
 			logs: [],
 			depth: 0,
 			stack: [],
-			memory: "0x0",
-			opcode: "0x0",
+			memory: '0x0',
+			opcode: '0x0',
 			gasLeft: 420,
 			storage: {},
-			returnData: "0x0",
-		};
+			returnData: '0x0',
+		}
 	} catch (err) {
-		throw new Error(`Failed to toggle run/pause: ${err}`);
+		throw new Error(`Failed to toggle run/pause: ${err}`)
 	}
 }
 
 export async function getEvmState(): Promise<EvmState> {
 	try {
-		console.log("get_evm_state");
+		console.log('get_evm_state')
 		return {
 			pc: 2,
 			logs: [],
 			depth: 0,
 			stack: [],
-			memory: "0x0",
-			opcode: "0x0",
+			memory: '0x0',
+			opcode: '0x0',
 			gasLeft: 420,
 			storage: {},
-			returnData: "0x0",
-		};
+			returnData: '0x0',
+		}
 	} catch (err) {
-		throw new Error(`Failed to get state: ${err}`);
+		throw new Error(`Failed to get state: ${err}`)
 	}
 }
 
 export const copyToClipboard = (text: string): void => {
-	navigator.clipboard.writeText(text);
-};
+	navigator.clipboard.writeText(text)
+}
--- src/ui/ui.zig
+++ src/ui/ui.zig
@@ -1,11 +1,116 @@
+const std = @import("std");
 const webui = @import("webui.zig");
 
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const allocator = gpa.allocator();
+
+pub const Asset = struct {
+    const Self = @This();
+
+    path: []const u8,
+    content: []const u8,
+    mime_type: []const u8,
+    response: []u8,
+
+    pub fn init(
+        path: []const u8,
+        content: []const u8,
+        mime_type: []const u8,
+    ) Self {
+        return Self{
+            .path = path,
+            .content = content,
+            .mime_type = mime_type,
+            .response = try std.fmt.allocPrint(allocator, "HTTP/1.1 200 OK\r\n" ++
+                "Content-Type: {s}\r\n" ++
+                "Content-Length: {d}\r\n" ++
+                "\r\n" ++
+                "{s}", .{ mime_type, content.len, content }),
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        allocator.free(self.response);
+    }
+
+    pub const assets = [_]Asset{
+        Asset.init(
+            "/index.html",
+            "dist/index.html",
+            "text/html",
+        ),
+        Asset.init(
+            "/vite.svg",
+            "dist/vite.svg",
+            "image/svg+xml",
+        ),
+        Asset.init(
+            "/assets/index-g0-s2jNV.js",
+            "dist/assets/index-g0-s2jNV.js",
+            "application/javascript",
+        ),
+        Asset.init(
+            "/assets/index-BG_JvFUn.css",
+            "dist/assets/index-BG_JvFUn.css",
+            "text/css",
+        ),
+        Asset.init(
+            "/assets/logo-BKhbptE1.svg",
+            "dist/assets/logo-BKhbptE1.svg",
+            "image/svg+xml",
+        ),
+        Asset.init(
+            "/tauri.svg",
+            "dist/tauri.svg",
+            "image/svg+xml",
+        ),
+    };
+
+    pub fn getAsset(filename: []const u8) ?Asset {
+        for (assets) |asset| {
+            if (std.mem.eql(u8, asset.path, filename)) {
+                return asset;
+            }
+        }
+        return null;
+    }
+};
+
+const HandlerError = error{
+    FILE_NOT_FOUND,
+};
+
 pub fn main() !void {
-    var window = webui.newWindow();
+    defer _ = gpa.deinit();
+    var app = App.init();
+    defer App.deinit();
 
-    try window.show("<html><head><script src=\"/webui.js\"></script></head> Hello World ! </html>");
-
-    webui.wait();
-
-    // intentionally don't cleanup memory to close faster
+    try app.run();
 }
+
+const App = struct {
+    const Self = @This();
+
+    window: webui,
+
+    pub fn init() Self {
+        const window = webui.newWindow();
+        window.setRuntime(.Bun);
+        return Self{ .window = window };
+    }
+
+    pub fn deinit() void {
+        webui.clean();
+    }
+
+    pub fn handler(filename: []const u8) ?[]const u8 {
+        const asset = Asset.getAsset(filename) orelse unreachable;
+        return asset.response;
+    }
+
+    pub fn run(self: *Self) !void {
+        self.window.setFileHandler(App.handler);
+        try self.window.show("index.html");
+        webui.wait();
+    }
+};
