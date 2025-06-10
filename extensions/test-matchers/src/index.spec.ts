1398c4cd147840947e1972be545c46d63a520ad7
William Cory <willcory10@gmail.com>
2025-06-10 13:45:19 -0700

    fix: lint

--- extensions/test-matchers/src/index.spec.ts
+++ extensions/test-matchers/src/index.spec.ts
@@ -1,5 +1,5 @@
-import { expect, test } from "vitest";
+import { expect, test } from 'vitest'
 
-test("placeholder", () => {
-  expect(true).toBeTruthy();
-});
+test('placeholder', () => {
+	expect(true).toBeTruthy()
+})
