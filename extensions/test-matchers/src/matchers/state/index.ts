854ac991441b80b81b0844fbc61ba0e3d86c212a
0xpolarzero <0xpolarzero@gmail.com>
2025-06-16 19:28:09 +0200

    refactor: accept a tevm node

--- extensions/test-matchers/src/matchers/state/index.ts
+++ extensions/test-matchers/src/matchers/state/index.ts
@@ -1,8 +1,9 @@
 import type { Client } from 'viem'
 import { toBeInitializedAccount } from './toBeInitializedAccount.js'
+import type { TevmNode } from '@tevm/node'
 
 export { toBeInitializedAccount }
 
 export interface StateMatchers {
-	toBeInitializedAccount(client: Client): Promise<void>
+	toBeInitializedAccount(client: Client | TevmNode): Promise<void>
 }
