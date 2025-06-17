8f085b285bd821423ffbae5b3f1b100236c25088
0xpolarzero <0xpolarzero@gmail.com>
2025-06-17 19:45:40 +0200

    test: more test matchers in tests

--- packages/actions/src/internal/runCallWithPrestateTrace.spec.ts
+++ packages/actions/src/internal/runCallWithPrestateTrace.spec.ts
@@ -3,7 +3,6 @@ import { createTevmNode } from '@tevm/node'
 import { TestERC20 } from '@tevm/test-utils'
 import { encodeFunctionData, hexToBytes, parseEther } from '@tevm/utils'
 import { beforeEach, describe, expect, it } from 'vitest'
-import { getAccountHandler } from '../GetAccount/getAccountHandler.js'
 import { setAccountHandler } from '../SetAccount/setAccountHandler.js'
 import { dealHandler } from '../anvil/index.js'
 import { runCallWithPrestateTrace } from './runCallWithPrestateTrace.js'
@@ -31,12 +30,8 @@ describe('runCallWithPrestateTrace', () => {
 			).errors,
 		).toBeUndefined()
 
-		expect(
-			await getAccountHandler(client)({
-				address: ERC20_ADDRESS,
-			}),
-		).toMatchObject({
-			address: ERC20_ADDRESS,
+		// @ts-expect-error: Monorepo type conflict: TevmNode from source (/src) conflicts with the matcher's type from compiled output (/dist).
+		await expect(ERC20_ADDRESS).toHaveState(client, {
 			deployedBytecode: ERC20_BYTECODE,
 		})
 
