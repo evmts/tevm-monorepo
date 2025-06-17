c352f51a3d2af7b20c395cd5d3c52773fcb6f8a2
0xpolarzero <0xpolarzero@gmail.com>
2025-06-17 19:08:54 +0200

    feat: toHaveStorageAt matcher

--- extensions/test-matchers/src/matchers/state/index.ts
+++ extensions/test-matchers/src/matchers/state/index.ts
@@ -2,11 +2,13 @@ import type { TevmNode } from '@tevm/node'
 import type { Client } from 'viem'
 import { toBeInitializedAccount } from './toBeInitializedAccount.js'
 import { toHaveState } from './toHaveState.js'
-import type { ExpectedState } from './types.js'
+import { toHaveStorageAt } from './toHaveStorageAt.js'
+import type { ExpectedState, ExpectedStorage } from './types.js'
 
-export { toBeInitializedAccount, toHaveState }
+export { toBeInitializedAccount, toHaveState, toHaveStorageAt }
 
 export interface StateMatchers {
 	toBeInitializedAccount(client: Client | TevmNode): Promise<void>
 	toHaveState(client: Client | TevmNode, expectedState: ExpectedState): Promise<void>
+	toHaveStorageAt(client: Client | TevmNode, expectedStorage: ExpectedStorage): Promise<void>
 }
