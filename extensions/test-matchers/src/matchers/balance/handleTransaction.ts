a3ce29d56f09cf14d976a124f824a29c799ef2bf
0xpolarzero <0xpolarzero@gmail.com>
2025-06-18 19:54:50 +0200

    refactor: use prestate trace for balance change

--- extensions/test-matchers/src/matchers/balance/handleTransaction.ts
+++ extensions/test-matchers/src/matchers/balance/handleTransaction.ts
@@ -1,9 +1,9 @@
-import { ethGetTransactionReceiptHandler, getBalanceHandler } from '@tevm/actions'
+import { type PrestateTraceResult, debugTraceTransactionJsonRpcProcedure } from '@tevm/actions'
 import { type TevmNode, createTevmNode } from '@tevm/node'
-import { type Address, type BlockTag, type Client, type Hex, isHex } from 'viem'
+import { type Address, type Client, type Hex, isHex } from 'viem'
 import { waitForTransactionReceipt } from 'viem/actions'
 import type { ContainsTransactionAny } from '../../common/types.js'
-import type { BalanceChange, HandleTransactionResult } from './types.js'
+import type { HandleTransactionResult } from './types.js'
 
 /**
  * Handles a transaction to extract balance changes
@@ -20,69 +20,53 @@ export const handleTransaction = async (
 
 	const node = 'request' in client ? createTevmNode({ fork: { transport: client } }) : client
 
-	// If it's already a transaction receipt
-	if (typeof res === 'object' && 'status' in res) {
-		return {
-			getBalanceChange: (address: Address) => getBalanceChange(node)(address, res.blockNumber - 1n, res.blockNumber),
-		}
-	}
+	const txHash =
+		// If it's a transaction receipt
+		typeof res === 'object' && 'transactionHash' in res
+			? res.transactionHash
+			: // If it's a call result
+				typeof res === 'object' && 'txHash' in res
+				? res.txHash
+				: // If it's already a transaction hash
+					typeof res === 'string' && isHex(res)
+					? res
+					: undefined
 
-	let txHash: Hex | undefined
-
-	// If it's a call result, grab the tx hash
-	if (typeof res === 'object' && 'txHash' in res) {
-		if (res.txHash === undefined)
-			throw new Error(
-				'Transaction hash is undefined, you need to pass `addToBlockchain: true` or `addToMempool: true` to include the transaction in a block',
-			)
-		txHash = res.txHash
-	}
-
-	// If it's a transaction hash just assign it
-	if (typeof res === 'string' && isHex(res)) {
-		txHash = res
-	}
-
-	// At this point if we don't have a tx hash, we don't know what was passed
-	if (!txHash)
+	if (txHash === undefined) {
 		throw new Error(
 			'Transaction hash is undefined, you need to pass a transaction hash, receipt or call result, or a promise that resolves to one of those',
 		)
-
-	if ('request' in client) {
-		// If a client was passed, we assume the tx will get mined consumer side, so we just want to wait it and grab the receipt
-		const txReceipt = await waitForTransactionReceipt(client, { hash: txHash })
-		return {
-			getBalanceChange: (address: Address) =>
-				getBalanceChange(node)(address, txReceipt.blockNumber - 1n, txReceipt.blockNumber),
-		}
 	}
 
-	// Otherwise we'll find out from the node directly
-	const txReceipt = await ethGetTransactionReceiptHandler(node)({ hash: txHash })
-	if (!txReceipt) throw new Error('Transaction receipt not found')
+	// If a client was passed, we assume the tx will get mined consumer side, so we just need to wait for it
+	if ('request' in client) {
+		await waitForTransactionReceipt(client, { hash: txHash })
+	}
+
+	// Now we can replay the transaction with a prestate tracer to figure out the balance changes
+	const { getBalanceChange } = await getDiffMethodsFromPrestateTrace(node, txHash)
 	return {
-		getBalanceChange: (address: Address) =>
-			getBalanceChange(node)(address, txReceipt.blockNumber - 1n, txReceipt.blockNumber),
+		getBalanceChange,
 	}
 }
 
-// Create balance change getter function
-const getBalanceChange =
-	(node: TevmNode) =>
-	async (
-		address: Address,
-		beforeBlockTag: bigint | BlockTag,
-		afterBlockTag: bigint | BlockTag,
-	): Promise<BalanceChange> => {
-		const getBalance = getBalanceHandler(node)
-		const balanceBefore = await getBalance({ address, blockTag: beforeBlockTag })
-		const balanceAfter = await getBalance({ address, blockTag: afterBlockTag })
+const getDiffMethodsFromPrestateTrace = async (node: TevmNode, txHash: Hex) => {
+	const res = await debugTraceTransactionJsonRpcProcedure(node)({
+		jsonrpc: '2.0',
+		method: 'debug_traceTransaction',
+		params: [{ transactionHash: txHash, tracer: 'prestateTracer', tracerConfig: { diffMode: true } }],
+		id: 1,
+	})
 
-		return {
-			address,
-			balanceBefore,
-			balanceAfter,
-			balanceChange: balanceAfter - balanceBefore,
-		}
+	const prestateTrace = res.result as PrestateTraceResult<true>
+
+	return {
+		getBalanceChange: (address: Address) => {
+			const pre = prestateTrace.pre[address.toLowerCase() as Address]
+			const post = prestateTrace.post[address.toLowerCase() as Address]
+			if (!pre || !post) return 0n
+
+			return BigInt(post.balance ?? 0n) - BigInt(pre.balance ?? 0n)
+		},
 	}
+}
