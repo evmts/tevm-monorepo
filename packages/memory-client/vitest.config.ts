99b11461d0e842dcfb16f6f696eb8096482b1296
0xpolarzero <0xpolarzero@gmail.com>
2025-06-19 20:02:49 +0200

    fix: incorrect setupFiles

--- packages/memory-client/vitest.config.ts
+++ packages/memory-client/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 // https://vitest.dev/config/ - for docs
 export default defineConfig({
 	test: {
-		setupFiles: ['../../extensions/test-matchers/src/index.ts'],
+		setupFiles: ['@tevm/test-matchers'],
 		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 		environment: 'node',
 		coverage: {
