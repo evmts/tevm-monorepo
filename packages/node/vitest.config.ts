e9026110fc3e51b4c4928f8b7f981ecc321b2d11
0xpolarzero <0xpolarzero@gmail.com>
2025-06-20 16:24:14 +0200

    chore: remove test-matchers from unused packages

--- packages/node/vitest.config.ts
+++ packages/node/vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config'
 // https://vitest.dev/config/ - for docs
 export default defineConfig({
 	test: {
-		setupFiles: ['@tevm/test-matchers'],
 		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 		environment: 'node',
 		coverage: {
