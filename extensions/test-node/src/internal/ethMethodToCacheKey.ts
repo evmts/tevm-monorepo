f1f7e85f2b9340d8fdadf78e3abc33294f0feebb
0xpolarzero <0xpolarzero@gmail.com>
2025-06-24 13:00:07 +0200

    lint: test-node extension

--- extensions/test-node/src/internal/ethMethodToCacheKey.ts
+++ extensions/test-node/src/internal/ethMethodToCacheKey.ts
@@ -1,9 +1,9 @@
-import type { EthJsonRpcRequest } from "@tevm/actions"
+import type { EthJsonRpcRequest } from '@tevm/actions'
 
 // Turn a request (that can be cached, we ignore non cacheable methods) into a cache key that depends on block height
 // i.e. we don't pass the full request as req.id for instance would make every request unique
 export const ethMethodToCacheKey: Partial<{
 	[TMethod in EthJsonRpcRequest['method']]: (request: Extract<EthJsonRpcRequest, { method: TMethod }>) => string
 }> = {
-  eth_getBlockByNumber: (req) =>  JSON.stringify([req.jsonrpc, req.method, req.params[0].toLowerCase(), req.params[1]])
+	eth_getBlockByNumber: (req) => JSON.stringify([req.jsonrpc, req.method, req.params[0].toLowerCase(), req.params[1]]),
 }
