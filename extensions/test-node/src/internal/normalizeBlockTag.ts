c7357bd8eaf4f8e4b50c728646af8ba7a5fff05f
0xpolarzero <0xpolarzero@gmail.com>
2025-06-26 20:33:58 +0200

    wip: all methods

--- extensions/test-node/src/internal/normalizeBlockTag.ts
+++ extensions/test-node/src/internal/normalizeBlockTag.ts
@@ -1,7 +1,15 @@
-import type { BlockTag } from '@tevm/actions'
-import { type Hex, isHex } from 'viem'
+import { type BlockTag, type Hex, isHex, type RpcBlockIdentifier } from 'viem'
 
-export const normalizeBlockTag = (tag: BlockTag | Hex | undefined) => {
-	if (!tag || !isHex(tag)) throw new Error('Invalid block tag') // this should never happen as it would have been filtered out by isCachedJsonRpcMethod
-	return tag.toLowerCase()
+export const normalizeBlockTag = (tag: BlockTag | Hex | RpcBlockIdentifier | undefined) => {
+	if (typeof tag === 'object' && 'blockHash' in tag && tag.blockHash !== undefined) {
+		return tag.blockHash.toLowerCase()
+	}
+	if (typeof tag === 'object' && 'blockNumber' in tag && tag.blockNumber !== undefined) {
+		return tag.blockNumber.toLowerCase()
+	}
+	if (typeof tag === 'string' && isHex(tag)) {
+		return tag.toLowerCase()
+	}
+
+	throw new Error('Invalid block tag') // this should never happen as it would have been filtered out by isCachedJsonRpcMethod
 }
