626bfc762dbd9d3c58f8c5d774e5cef25b6b7bb8
0xpolarzero <0xpolarzero@gmail.com>
2025-06-27 20:58:06 +0200

    lint: test-node

--- extensions/test-node/src/internal/isCachedMethod.ts
+++ extensions/test-node/src/internal/isCachedMethod.ts
@@ -1,8 +1,8 @@
-import { isHex, type EIP1193RequestFn } from 'viem'
+import { type EIP1193RequestFn, isHex } from 'viem'
 
 // This only tells polly if this should be cached or not.
 // Actually caching/retrieving from cache depending on the parameters is done separately.
-export const isCachedMethod = ({ method, params: _params}: Parameters<EIP1193RequestFn>[0]) => {
+export const isCachedMethod = ({ method, params: _params }: Parameters<EIP1193RequestFn>[0]) => {
 	if (_params && !Array.isArray(_params)) return false
 	const params = _params as unknown[]
 
