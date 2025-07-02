3625698111490cf499606871f99d5417c9baa9c3
0xpolarzero <0xpolarzero@gmail.com>
2025-07-02 10:40:28 +0200

    chore: export new client

--- extensions/test-node/src/index.ts
+++ extensions/test-node/src/index.ts
@@ -1,5 +1,12 @@
 // Export the new simplified API
 export { createTestSnapshotClient } from './createTestSnapshotClient.js'
+export { createTestSnapshotNode } from './createTestSnapshotNode.js'
 
 // Re-export types for convenience
-export type { TestSnapshotClient, TestSnapshotClientOptions, TestOptions } from './types.js'
+export type {
+	TestSnapshotClient,
+	TestSnapshotClientOptions,
+	TestSnapshotNode,
+	TestSnapshotNodeOptions,
+	TestOptions,
+} from './types.js'
