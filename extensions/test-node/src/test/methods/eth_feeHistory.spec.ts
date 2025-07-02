73ecc56add7e3473a3ed4d2f5900211e31e2ca1a
0xpolarzero <0xpolarzero@gmail.com>
2025-07-02 12:43:22 +0200

    fix: new api

--- extensions/test-node/src/test/methods/eth_feeHistory.spec.ts
+++ extensions/test-node/src/test/methods/eth_feeHistory.spec.ts
@@ -5,22 +5,22 @@ import { client } from '../vitest.setup.js'
 
 describe('eth_feeHistory', () => {
 	it('should create a cache entry with a static newest block number', async () => {
-		await client.tevm.transport.tevm.forkTransport?.request({
+		await client.transport.tevm.forkTransport?.request({
 			method: 'eth_feeHistory',
 			params: ['0x1', BLOCK_NUMBER, [1, 2, 3]],
 		})
 
-		await client.save()
+		await client.saveSnapshots()
 		assertMethodCached('eth_feeHistory', (params) => params[1] === BLOCK_NUMBER)
 	})
 
 	it('should NOT create a cache entry with a dynamic newest block tag', async () => {
-		await client.tevm.transport.tevm.forkTransport?.request({
+		await client.transport.tevm.forkTransport?.request({
 			method: 'eth_feeHistory',
 			params: ['0x1', 'latest', [1, 2, 3]],
 		})
 
-		await client.save()
+		await client.saveSnapshots()
 		assertMethodNotCached('eth_feeHistory', (params) => params[1] === 'latest')
 	})
 })
--- extensions/test-node/src/test/methods/eth_getLogs.spec.ts
+++ extensions/test-node/src/test/methods/eth_getLogs.spec.ts
@@ -6,23 +6,23 @@ import { client } from '../vitest.setup.js'
 
 describe('eth_getLogs', () => {
 	it('should create a cache entry with a static block numbers', async () => {
-		await client.tevm.transport.tevm.forkTransport?.request({
+		await client.transport.tevm.forkTransport?.request({
 			method: 'eth_getLogs',
 			params: [{ fromBlock: BLOCK_NUMBER, toBlock: BLOCK_NUMBER }],
 		})
 
-		await client.save()
+		await client.saveSnapshots()
 		assertMethodCached('eth_getLogs', (params) => params[0] === BLOCK_NUMBER && params[1] === BLOCK_NUMBER)
 	})
 
 	it('should NOT create a cache entry with dynamic block tags', async () => {
-		const latestBlock = (await client.tevm.transport.tevm.forkTransport?.request({ method: 'eth_blockNumber' })) as Hex
-		await client.tevm.transport.tevm.forkTransport?.request({
+		const latestBlock = (await client.transport.tevm.forkTransport?.request({ method: 'eth_blockNumber' })) as Hex
+		await client.transport.tevm.forkTransport?.request({
 			method: 'eth_getLogs',
 			params: [{ fromBlock: numberToHex(BigInt(latestBlock) - 1n), toBlock: 'latest' }],
 		})
 
-		await client.save()
+		await client.saveSnapshots()
 		assertMethodNotCached(
 			'eth_getLogs',
 			(params) => params[0] === numberToHex(BigInt(latestBlock) - 1n) && params[1] === 'latest',
