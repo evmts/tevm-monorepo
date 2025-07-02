27c373dba40f8bf75ec154f3d792ec91e3e66511
0xpolarzero <0xpolarzero@gmail.com>
2025-07-02 18:36:36 +0200

    lint: test-matchers

--- extensions/test-matchers/src/matchers/balance/getDiffMethodsFromPrestateTrace.ts
+++ extensions/test-matchers/src/matchers/balance/getDiffMethodsFromPrestateTrace.ts
@@ -1,11 +1,11 @@
 import { type PrestateTraceResult, debugTraceTransactionJsonRpcProcedure } from '@tevm/actions'
 import { type TevmNode } from '@tevm/node'
 import { type Address, type Client } from 'viem'
+import { handleTransaction } from '../../common/handleTransaction.js'
+import type { ContainsTransactionAny } from '../../common/types.js'
 import { getBalanceChange } from './getBalanceChange.js'
 import { getTokenBalanceChange } from './getTokenBalanceChange.js'
 import type { HandleTransactionResult } from './types.js'
-import type { ContainsTransactionAny } from '../../common/types.js'
-import { handleTransaction } from '../../common/handleTransaction.js'
 
 /**
  * Replay a transaction with a prestate tracer to figure out the balance changes
@@ -13,7 +13,10 @@ import { handleTransaction } from '../../common/handleTransaction.js'
  * @param txHash - The transaction hash
  * @returns Promise with balance change getter functions
  */
-export const getDiffMethodsFromPrestateTrace = async (client: Client | TevmNode, tx: ContainsTransactionAny | Promise<ContainsTransactionAny>): Promise<HandleTransactionResult> => {
+export const getDiffMethodsFromPrestateTrace = async (
+	client: Client | TevmNode,
+	tx: ContainsTransactionAny | Promise<ContainsTransactionAny>,
+): Promise<HandleTransactionResult> => {
 	const { node, txHash } = await handleTransaction(client, tx)
 
 	const res = await debugTraceTransactionJsonRpcProcedure(node)({
