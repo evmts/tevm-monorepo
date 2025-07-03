e0e19e5c19befd47db31d99ccf080ac8de372c65
0xpolarzero <0xpolarzero@gmail.com>
2025-07-03 09:49:04 +0200

    feat: advanced contract to test call traces

--- packages/contract/src/contract-lib/index.ts
+++ packages/contract/src/contract-lib/index.ts
@@ -1,4 +1,5 @@
 export { SimpleContract } from './SimpleContract.s.sol.js'
+export { AdvancedContract } from './AdvancedContract.s.sol.js'
 export { ErrorContract } from './ErrorContract.s.sol.js'
 export { ERC20 } from './ERC20.js'
 export { ERC721 } from './ERC721.js'
