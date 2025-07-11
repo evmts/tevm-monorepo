0800540ece3195e12a1b82407ac2c23d8c096407
0xpolarzero <0xpolarzero@gmail.com>
2025-07-11 13:40:10 +0200

    fix: applyUpdates

--- bundler-packages/mud/src/internal/mud/applyUpdates.ts
+++ bundler-packages/mud/src/internal/mud/applyUpdates.ts
@@ -31,23 +31,8 @@ export function applyStashUpdates({ stash, updates }: ApplyUpdatesArgs): void {
 		if (stash.get().config[table.namespaceLabel]?.[table.label] == null) {
 			registerTable({ stash, table })
 		}
-		const tableState = ((stash._.state.records[table.namespaceLabel] ??= {})[table.label] ??= {})
-		const encodedKey = encodeKey({ table, key })
-		const prevRecord = tableState[encodedKey]
 
-		// create new record, preserving field order
-		const nextRecord =
-			value == null
-				? undefined
-				: Object.fromEntries(
-						Object.entries(table.schema).map(([fieldName, { type }]) => [
-							fieldName,
-							key[fieldName] ?? // Use provided key fields
-								value[fieldName] ?? // Or provided value fields
-								prevRecord?.[fieldName] ?? // Keep existing non-overridden fields
-								schemaAbiTypeToDefaultValue[type], // Default values for new fields
-						]),
-					)
+		const { tableState, encodedKey, nextRecord } = getUpdatedRecords(stash, { table, key, value })
 
 		// apply update to state
 		if (nextRecord != null) {
@@ -70,17 +55,16 @@ export function notifyStashSubscribers({ stash, updates: _updates }: ApplyUpdate
 
 	// Group updates by namespace and table for notification
 	for (const { table, key, value } of _updates) {
-		const tableState = stash._.state.records[table.namespaceLabel]?.[table.label]
-		const encodedKey = encodeKey({ table, key })
-		const prevRecord = tableState?.[encodedKey]
-		const currentRecord = value == null ? undefined : tableState?.[encodedKey]
-
 		const tableUpdates = ((pendingUpdates[table.namespaceLabel] ??= {})[table.label] ??= [])
+
+		const { prevRecord, nextRecord } = getUpdatedRecords(stash, { table, key, value })
+
+		// push update to table updates
 		tableUpdates.push({
 			table,
 			key,
 			previous: prevRecord,
-			current: currentRecord,
+			current: nextRecord,
 		})
 	}
 
@@ -96,4 +80,28 @@ export function notifyStashSubscribers({ stash, updates: _updates }: ApplyUpdate
 		.map((namespaceUpdates) => Object.values(namespaceUpdates))
 		.flat(2)
 	stash._.storeSubscribers.forEach((subscriber) => subscriber({ type: 'records', updates }))
+}
+
+function getUpdatedRecords(stash: Stash, update: PendingStashUpdate) {
+	const { table, key, value } = update
+
+	const tableState = ((stash._.state.records[table.namespaceLabel] ??= {})[table.label] ??= {})
+	const encodedKey = encodeKey({ table, key })
+	const prevRecord = tableState[encodedKey]
+
+	// create new record, preserving field order
+	const nextRecord =
+		value == null
+			? undefined
+			: Object.fromEntries(
+					Object.entries(table.schema).map(([fieldName, { type }]) => [
+						fieldName,
+						key[fieldName] ?? // Use provided key fields
+							value[fieldName] ?? // Or provided value fields
+							prevRecord?.[fieldName] ?? // Keep existing non-overridden fields
+							schemaAbiTypeToDefaultValue[type], // Default values for new fields
+					]),
+				)
+
+	return { tableState, encodedKey, prevRecord, nextRecord }
 }
