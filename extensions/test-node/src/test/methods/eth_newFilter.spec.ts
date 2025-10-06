decdf61a95397f37841c5f4a040940db93bcc707
0xpolarzero <0xpolarzero@gmail.com>
2025-10-06 12:23:32 +0200

    fix: lower filter window for alchemy

--- extensions/test-node/src/test/methods/eth_newFilter.spec.ts
+++ extensions/test-node/src/test/methods/eth_newFilter.spec.ts
@@ -1,13 +1,15 @@
+import { type Hex, numberToHex } from 'viem'
 import { describe, it } from 'vitest'
-import { BLOCK_NUMBER } from '../constants.js'
 import { assertMethodNotCached } from '../snapshot-utils.js'
 import { client } from '../vitest.setup.js'
 
 describe('eth_newFilter', () => {
 	it('should NOT create a cache entry', async () => {
+		const latestBlock = await client.transport.tevm.forkTransport?.request({ method: 'eth_blockNumber' })
+		if (!latestBlock) throw new Error('Latest block not found')
 		await client.transport.tevm.forkTransport?.request({
 			method: 'eth_newFilter',
-			params: [{ fromBlock: BLOCK_NUMBER, toBlock: 'latest' }],
+			params: [{ fromBlock: numberToHex(BigInt(latestBlock as Hex) - 1n), toBlock: 'latest' }],
 		})
 
 		await client.saveSnapshots()
