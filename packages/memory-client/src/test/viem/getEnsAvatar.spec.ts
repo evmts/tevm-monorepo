9c87c48d2c79a7b8c05d72690c57bf4c7118194b
0xpolarzero <0xpolarzero@gmail.com>
2025-10-08 11:43:28 +0200

    fix: concurrent caches

--- packages/memory-client/src/test/viem/getEnsAvatar.spec.ts
+++ packages/memory-client/src/test/viem/getEnsAvatar.spec.ts
@@ -1,5 +1,5 @@
 import { mainnet } from '@tevm/common'
-import { createCachedMainnetTransport } from '@tevm/test-utils'
+import { /* createCachedMainnetTransport, */ transports } from '@tevm/test-utils'
 import { loadKZG } from 'kzg-wasm'
 import { describe, expect, it } from 'vitest'
 import { createMemoryClient } from '../../createMemoryClient.js'
@@ -7,11 +7,12 @@ import { createMemoryClient } from '../../createMemoryClient.js'
 describe('getEnsAvatar', async () => {
 	it('should work', { timeout: 40_000 }, async () => {
 		const kzg = await loadKZG()
-		const cachedTransport = createCachedMainnetTransport()
+		// TODO: we can't use a cached transport here otherwise some other test will reuse it because of createMemoryClient cache, see TODO issue in extensions/test-node/src/snapshot/createCachedTransport.ts
+		// const cachedTransport = createCachedMainnetTransport()
 		const mainnetClient = createMemoryClient({
 			common: Object.assign({ kzg }, mainnet),
 			fork: {
-				transport: cachedTransport,
+				transport: transports.mainnet,
 				blockTag: 23483670n,
 			},
 		})
